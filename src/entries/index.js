e3befca49925c078a32942d6332e07a095196169
kobezzza <kobezzza@gmail.com>
2017-07-07 14:03:06 +0300

    Some fixes

---
+++ src/entries/index.js
@@ -0,0 +1,11 @@
+/*!
+ * TravelChat Client
+ * https://github.com/Travelbanda/TravelChat
+ *
+ * Released under the FSFUL license
+ * https://github.com/Travelbanda/TravelChat/blob/master/LICENSE
+ */
+
+import '../core';
+import '../global/g-def';
+import '../global/g-hint';
