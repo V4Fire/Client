d9cc7893e33f5e02322d0ae50635f4ca6905fa06
kobezzza <kobezzza@gmail.com>
2017-07-10 17:28:43 +0300

    Gulp refactoring

--- build/static.gulp.js
+++ build/static.gulp.js
@@ -8,168 +8,169 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-const
-	gulp = require('gulp'),
-	plumber = require('gulp-plumber');
-
-gulp.task('favicons', (cb) => {
+module.exports = function (gulp = require('gulp')) {
 	const
-		favicons = require('gulp-favicons'),
-		image = require('gulp-image'),
-		async = require('async');
+		plumber = require('gulp-plumber');
 
-	/* eslint-disable camelcase */
+	gulp.task('favicons', (cb) => {
+		const
+			favicons = require('gulp-favicons'),
+			image = require('gulp-image'),
+			async = require('async');
 
-	async.series([
-		(cb) => gulp.src('./assets/logo.svg')
+		/* eslint-disable camelcase */
+
+		async.series([
+			(cb) => gulp.src('./assets/logo.svg')
+				.pipe(plumber())
+				.pipe(favicons({
+					appName: 'V4Fire',
+					start_url: '.',
+					background: '#3D7D73',
+					path: '../../assets/favicons/',
+					display: 'standalone',
+					orientation: 'portrait',
+					version: 1.0,
+					logging: false,
+					html: 'favicons.html',
+					pipeHTML: true,
+					replace: true
+				}))
+
+				.pipe(gulp.dest('./assets/favicons'))
+				.on('end', cb),
+
+			(cb) => gulp.src('./assets/favicons/*')
+				.pipe(plumber())
+				.pipe(image({
+					pngquant: true,
+					concurrent: 10
+				}))
+
+				.pipe(gulp.dest('./assets/favicons'))
+				.on('end', cb)
+
+		], cb);
+
+		/* eslint-enable camelcase */
+	});
+
+	gulp.task('html', (cb) => {
+		const
+			htmlmin = require('gulp-htmlmin'),
+			replace = require('gulp-replace');
+
+		gulp.src('./dist/packages/**/*.html')
 			.pipe(plumber())
-			.pipe(favicons({
-				appName: 'V4Fire',
-				start_url: '.',
-				background: '#3D7D73',
-				path: '../../assets/favicons/',
-				display: 'standalone',
-				orientation: 'portrait',
-				version: 1.0,
-				logging: false,
-				html: 'favicons.html',
-				pipeHTML: true,
-				replace: true
+			.pipe(htmlmin({
+				useShortDoctype: true,
+				conservativeCollapse: true,
+				removeAttributeQuotes: true
 			}))
 
-			.pipe(gulp.dest('./assets/favicons'))
-			.on('end', cb),
-
-		(cb) => gulp.src('./assets/favicons/*')
-			.pipe(plumber())
-			.pipe(image({
-				pngquant: true,
-				concurrent: 10
-			}))
-
-			.pipe(gulp.dest('./assets/favicons'))
-			.on('end', cb)
-
-	], cb);
-
-	/* eslint-enable camelcase */
-});
-
-gulp.task('html', (cb) => {
-	const
-		htmlmin = require('gulp-htmlmin'),
-		replace = require('gulp-replace');
-
-	gulp.src('./dist/packages/**/*.html')
-		.pipe(plumber())
-		.pipe(htmlmin({
-			useShortDoctype: true,
-			conservativeCollapse: true,
-			removeAttributeQuotes: true
-		}))
-
-		.pipe(gulp.dest('./dist/packages'))
-		.on('end', cb);
-});
-
-gulp.task('css', (cb) => {
-	const
-		csso = require('gulp-csso'),
-		async = require('async');
-
-	function f(path, cb) {
-		return gulp.src([`${path}/**/*.css`, `!${path}/**/*.min.css`])
-			.pipe(plumber())
-			.pipe(csso())
-			.pipe(gulp.dest(path))
+			.pipe(gulp.dest('./dist/packages'))
 			.on('end', cb);
-	}
+	});
 
-	async.parallel([
-		(cb) => f('./dist/packages/lib', cb),
-		(cb) => f('./assets', cb),
-	], cb);
-});
+	gulp.task('css', (cb) => {
+		const
+			csso = require('gulp-csso'),
+			async = require('async');
 
-gulp.task('js', (cb) => {
-	const
-		uglify = require('gulp-uglify'),
-		async = require('async');
+		function f(path, cb) {
+			return gulp.src([`${path}/**/*.css`, `!${path}/**/*.min.css`])
+				.pipe(plumber())
+				.pipe(csso())
+				.pipe(gulp.dest(path))
+				.on('end', cb);
+		}
 
-	/* eslint-disable camelcase */
+		async.parallel([
+			(cb) => f('./dist/packages/lib', cb),
+			(cb) => f('./assets', cb),
+		], cb);
+	});
 
-	function f(path, cb) {
-		return gulp.src([`${path}/**/*.js`, `!${path}/**/*.min.js`])
-			.pipe(plumber())
-			.pipe(uglify({
-				compress: {
-					warnings: false,
-					keep_fnames: true
-				},
+	gulp.task('js', (cb) => {
+		const
+			uglify = require('gulp-uglify'),
+			async = require('async');
 
-				output: {
-					comments: false
-				},
+		/* eslint-disable camelcase */
 
-				mangle: false
-			}))
+		function f(path, cb) {
+			return gulp.src([`${path}/**/*.js`, `!${path}/**/*.min.js`])
+				.pipe(plumber())
+				.pipe(uglify({
+					compress: {
+						warnings: false,
+						keep_fnames: true
+					},
 
-			.pipe(gulp.dest(path))
-			.on('end', cb);
-	}
+					output: {
+						comments: false
+					},
 
-	/* eslint-enable camelcase */
+					mangle: false
+				}))
 
-	async.parallel([
-		(cb) => f('./dist/packages/lib', cb),
-		(cb) => f('./assets', cb),
-	], cb);
-});
+				.pipe(gulp.dest(path))
+				.on('end', cb);
+		}
 
-gulp.task('image', (cb) => {
-	const
-		image = require('gulp-image'),
-		async = require('async');
+		/* eslint-enable camelcase */
 
-	function f(path, cb) {
-		const isArr = Array.isArray(path);
-		return gulp.src([`${isArr ? path[0] : path}/**/*.@(png|svg)`].concat(isArr ? path[1] || [] : []))
-			.pipe(plumber())
-			.pipe(image({
-				pngquant: true,
-				svgo: true,
-				concurrent: 10
-			}))
+		async.parallel([
+			(cb) => f('./dist/packages/lib', cb),
+			(cb) => f('./assets', cb),
+		], cb);
+	});
 
-			.pipe(gulp.dest(isArr ? path[0] : path))
-			.on('end', cb);
-	}
+	gulp.task('image', (cb) => {
+		const
+			image = require('gulp-image'),
+			async = require('async');
 
-	async.parallel([
-		(cb) => f('./dist/packages', cb),
-		(cb) => f(['./assets', `!assets/favicons/**`], cb),
-	], cb);
-});
+		function f(path, cb) {
+			const isArr = Array.isArray(path);
+			return gulp.src([`${isArr ? path[0] : path}/**/*.@(png|svg)`].concat(isArr ? path[1] || [] : []))
+				.pipe(plumber())
+				.pipe(image({
+					pngquant: true,
+					svgo: true,
+					concurrent: 10
+				}))
 
-gulp.task('gzip', ['image', 'html', 'css', 'js'], (cb) => {
-	const
-		async = require('async'),
-		gzip = require('gulp-gzip');
+				.pipe(gulp.dest(isArr ? path[0] : path))
+				.on('end', cb);
+		}
 
-	function f(path, cb) {
-		return gulp.src([`${path}/**/*`, `!${path}/**/*.gz`])
-			.pipe(plumber())
-			.pipe(gzip({
-				threshold: '1kb',
-				gzipOptions: {level: 9}
-			}))
+		async.parallel([
+			(cb) => f('./dist/packages', cb),
+			(cb) => f(['./assets', `!assets/favicons/**`], cb),
+		], cb);
+	});
 
-			.pipe(gulp.dest(path))
-			.on('end', cb);
-	}
+	gulp.task('gzip', ['image', 'html', 'css', 'js'], (cb) => {
+		const
+			async = require('async'),
+			gzip = require('gulp-gzip');
 
-	async.parallel([
-		(cb) => f('./dist/packages', cb),
-		(cb) => f('./assets', cb),
-	], cb);
-});
+		function f(path, cb) {
+			return gulp.src([`${path}/**/*`, `!${path}/**/*.gz`])
+				.pipe(plumber())
+				.pipe(gzip({
+					threshold: '1kb',
+					gzipOptions: {level: 9}
+				}))
+
+				.pipe(gulp.dest(path))
+				.on('end', cb);
+		}
+
+		async.parallel([
+			(cb) => f('./dist/packages', cb),
+			(cb) => f('./assets', cb),
+		], cb);
+	});
+};
--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,34 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-const {env, argv} = process;
-env.NODE_ENV = env.NODE_ENV || 'standalone';
+module.exports = function (gulp = require('gulp')) {
+	require('@v4fire/core/gulpfile')(gulp);
+	require('./build/static.gulp')(gulp);
 
-const
-	gulp = require('gulp'),
-	plumber = require('gulp-plumber'),
-	cached = require('gulp-cached');
+	const
+		runWebpack = 'node node_modules/parallel-webpack/bin/run.js',
+		args = process.argv.slice(3).join(' ');
 
-require('@v4fire/core/gulpfile')(gulp);
-require('./build/static.gulp');
+	gulp.task('cleanClient', (cb) => {
+		const del = require('del');
+		del('./dist/packages').then(() => cb(), cb);
+	});
 
-const
-	runWebpack = 'node node_modules/parallel-webpack/bin/run.js',
-	args = argv.slice(3).join(' ');
+	gulp.task('client', (cb) => {
+		const run = require('gulp-run');
+		run(`${runWebpack} -- ${args}`, {verbosity: 3})
+			.exec()
+			.on('finish', cb);
+	});
 
-gulp.task('cleanClient', (cb) => {
-	const del = require('del');
-	del('./dist/packages').then(() => cb(), cb);
-});
+	gulp.task('watchClient', () => {
+		const run = require('gulp-run');
+		run(`${runWebpack} --watch -- ${args}`, {verbosity: 3}).exec();
+	});
 
-gulp.task('client', (cb) => {
-	const run = require('gulp-run');
-	run(`${runWebpack} -- ${args}`, {verbosity: 3})
-		.exec()
-		.on('finish', cb);
-});
+	gulp.task('watch', ['watchClient']);
+	gulp.task('default', ['head', 'cleanClient', 'client']);
+	gulp.task('prod', ['setProd', 'default']);
+};
 
-gulp.task('watchClient', () => {
-	const run = require('gulp-run');
-	run(`${runWebpack} --watch -- ${args}`, {verbosity: 3}).exec();
-});
-
-gulp.task('watch', ['watchClient']);
-gulp.task('default', ['head', 'cleanClient', 'client']);
-gulp.task('prod', ['setProd', 'default']);
+module.exports();
