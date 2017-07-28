2406d67fd23c430be50343f05b28b94fba7a0065
kobezzza <kobezzza@gmail.com>
2017-07-28 17:24:16 +0300

    :wrench:

--- src/config/index.js
+++ src/config/index.js
@@ -8,4 +8,4 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-module.exports = Object.assign({}, require(`@v4fire/core/src/config`), CONFIG);
+module.exports = Object.assign({}, require('@v4fire/core/src/config'), CONFIG);
