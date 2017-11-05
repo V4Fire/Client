552d9f9ab7d1f74361c3edefe598db2a8e9965b0
kobezzza <kobezzza@gmail.com>
2017-11-05 16:55:37 +0300

    IMoved config to new API

--- config/default.js
+++ config/default.js
@@ -9,58 +9,48 @@
  */
 
 const
-	$C = require('collection.js'),
 	path = require('path'),
-	defConfig = require('@v4fire/core/config/default');
+	config = require('@v4fire/core/config/default');
 
-const config = module.exports = $C.extend(defConfig.extend, Object.create(defConfig), {
-	src: {
-		client: [path.join(__dirname, '../src')].concat(defConfig.src.client)
-	},
+module.exports = config.createConfig({dirs: [__dirname, 'client']}, {
+	__proto__: config,
 
-	globals: {
-		'process.env': {
-			NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-		}
-	},
-
-	externals: {
-		'collection.js': '$C',
-		'eventemitter2': 'EventEmitter2',
-		'localforage': 'localforage',
-		'urijs': 'URI',
-		'sugar': 'Sugar',
-		'vue': 'Vue',
-		'chart.js': 'Chart',
-		'ion-sound': 'ion'
-	},
-
-	monic: {
-		styl: {
-			flags: {
-				'+:*': true
+	monic() {
+		return {
+			styl: {
+				flags: {
+					'+:*': true
+				}
 			}
-		}
+		};
+	},
+
+	favicons() {
+		const
+			config = require('config');
+
+		return {
+			appName: config.appName(),
+			path: path.join(config.src.assets(), 'favicons'),
+			background: '#FFF',
+			display: 'standalone',
+			orientation: 'portrait',
+			version: 1.0,
+			logging: false
+		};
+	},
+
+	snakeskin() {
+		return {
+			client: this.extend(super.snakeskin(), {
+				adapter: 'ss2vue',
+				adapterOptions: {transpiler: true},
+				tagFilter: 'vueComp',
+				tagNameFilter: 'vueTag',
+				bemFilter: 'bem2vue'
+			}),
+
+			server: super.snakeskin()
+		};
 	}
 });
-
-config.favicons = {
-	appName: config.appName,
-	background: '#3D7D73',
-	path: '../../assets/favicons/',
-	display: 'standalone',
-	orientation: 'portrait',
-	version: 1.0,
-	logging: false
-};
-
-config.snakeskin = {
-	client: $C.extend(true, {}, defConfig.snakeskin, {
-		adapter: 'ss2vue',
-		tagFilter: 'vueComp',
-		tagNameFilter: 'vueTag',
-		bemFilter: 'bem2vue'
-	}),
-
-	server: $C.extend(true, {}, defConfig.snakeskin)
-};
