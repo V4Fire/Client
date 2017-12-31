9ad12c8a1313bb6fa36154e800b3ea4e254a1943
kobezzza <kobezzza@gmail.com>
2017-12-31 16:02:41 +0300

    Refactoring

--- build/snakeskin.webpack.js
+++ build/snakeskin.webpack.js
@@ -9,10 +9,6 @@
  */
 
 const
-	{String} = require('sugar'),
-	{validators} = require('@pzlr/build-core');
-
-const
 	$C = require('collection.js'),
 	ss = require('snakeskin'),
 	uuid = require('uuid');
@@ -22,266 +18,259 @@ const
 	path = require('path'),
 	glob = require('glob');
 
-/**
- * Initializes Snakeskin
- * @param {Array<string>} folders - list of block folders
- */
-module.exports = function ({folders}) {
-	folders = folders.slice().reverse();
+const
+	{String} = require('sugar'),
+	{validators} = require('@pzlr/build-core'),
+	{src} = require('config');
+
+const
+	folders = src.client.slice().reverse(),
+	components = `/**/@(${validators.blockTypeList.join('|')})-*.@(ts|js)`;
+
+const
+	files = $C(folders).reduce((arr, el) => arr.concat(glob.sync(path.join(el, components))), []).reverse(),
+	blocksTree = {};
+
+$C(files).forEach((el) => {
+	const
+		file = fs.readFileSync(el, {encoding: 'utf-8'});
+
+	if (!/^export default class (.*?) extends (.*?) {/m.test(file)) {
+		return;
+	}
 
 	const
-		blocksTree = {},
-		components = `/**/@(${validators.blockTypeList.join('|')})-*.@(ts|js)`,
-		files = $C(folders).reduce((arr, el) => arr.concat(glob.sync(path.join(el, components))), []).reverse();
+		component = RegExp.$1,
+		parent = RegExp.$2;
 
-	$C(files).forEach((el) => {
+	const obj = blocksTree[component] = blocksTree[component] || {
+		props: {},
+		functional: /@component\s*\(\s*{\s*functional\s*:\s*true\s*}\s*\)/.test(file),
+		parent
+	};
+
+	const
+		propRgxp = /^(\t@(?:field|abstract)[\s\S]+?\)*\n+)?\t([\w$]+)\s*:\s*[ \w|&$?()[\]{}<>'"`:.]+?\s*(?:=|;$)/gm;
+
+	let s;
+	while ((s = propRgxp.exec(file))) {
+		if (!s[1]) {
+			obj.props[s[2]] = true;
+		}
+	}
+});
+
+$C(blocksTree).forEach((el, key, data) => {
+	if (el.parent && data[el.parent]) {
+		Object.setPrototypeOf(el.props, data[el.parent].props);
+	}
+});
+
+const bind = {
+	bind: [
+		(o) => o.getVar('$attrs'),
+		'typeof rootTag !== "undefined" ? rootTag : undefined'
+	]
+};
+
+ss.importFilters({
+	vueComp,
+	vueTag: ss.setFilterParams(vueTag, bind),
+	bem2vue: ss.setFilterParams(bem2vue, bind),
+	b
+});
+
+function b(url) {
+	const
+		hasMagic = glob.hasMagic(url),
+		end = /\.e?ss$/.test(url) ? '' : '/',
+		ends = [];
+
+	if (end) {
 		const
-			file = fs.readFileSync(el, {encoding: 'utf-8'});
+			basename = path.basename(url);
 
-		if (!/^export default class (.*?) extends (.*?) {/m.test(file)) {
+		if (!glob.hasMagic(basename)) {
+			ends.push(`${basename}.ss`);
+		}
+
+		if (!validators.blockName(basename)) {
+			ends.push('main.ss', 'index.ss');
+		}
+
+	} else {
+		ends.push('');
+	}
+
+	for (let i = 0; i < folders.length; i++) {
+		for (let j = 0; j < ends.length; j++) {
+			const
+				fullPath = path.join(folders[i], url, ends[j] || '');
+
+			if (hasMagic ? Boolean(glob.sync(fullPath).length) : fs.existsSync(fullPath)) {
+				return fullPath;
+			}
+		}
+	}
+
+	return url + end;
+}
+
+function vueComp({name, attrs}) {
+	$C(attrs).forEach((el, key) => {
+		if (!/^(:|@|v-)/.test(key)) {
 			return;
 		}
 
 		const
-			component = RegExp.$1,
-			parent = RegExp.$2;
+			tmp = String.dasherize(key);
 
-		const obj = blocksTree[component] = blocksTree[component] || {
-			props: {},
-			functional: /@component\s*\(\s*{\s*functional\s*:\s*true\s*}\s*\)/.test(file),
-			parent
-		};
-
-		const
-			propRgxp = /^(\t@(?:field|abstract)[\s\S]+?\)*\n+)?\t([\w$]+)\s*:\s*[ \w|&$?()[\]{}<>'"`:.]+?\s*(?:=|;$)/gm;
-
-		let s;
-		while ((s = propRgxp.exec(file))) {
-			if (!s[1]) {
-				obj.props[s[2]] = true;
-			}
+		if (tmp !== key) {
+			delete attrs[key];
+			attrs[tmp] = el;
 		}
 	});
 
-	$C(blocksTree).forEach((el, key, data) => {
-		if (el.parent && data[el.parent]) {
-			Object.setPrototypeOf(el.props, data[el.parent].props);
-		}
-	});
-
-	const bind = {
-		bind: [
-			(o) => o.getVar('$attrs'),
-			'typeof rootTag !== "undefined" ? rootTag : undefined'
-		]
-	};
-
-	ss.importFilters({
-		vueComp,
-		vueTag: ss.setFilterParams(vueTag, bind),
-		bem2vue: ss.setFilterParams(bem2vue, bind),
-		b
-	});
+	let
+		isSync = attrs['v-sync'];
 
 	const
-		exists = {};
+		isAsync = attrs['v-async'],
+		isAsyncBack = attrs['v-async-back'];
 
-	function b(url) {
-		const
-			hasMagic = glob.hasMagic(url),
-			end = /\.e?ss$/.test(url) ? '' : '/',
-			ends = [];
-
-		if (end) {
-			const
-				basename = path.basename(url);
-
-			if (!glob.hasMagic(basename)) {
-				ends.push(`${basename}.ss`);
-			}
-
-			if (!validators.blockName(basename)) {
-				ends.push('main.ss', 'index.ss');
-			}
-
-		} else {
-			ends.push('');
+	if (attrs['v-for']) {
+		if (/^\s*([\w$]+)(\s+(?:in|of)\s+.*)$/.test(attrs['v-for'].join(' '))) {
+			attrs['v-for'] = [`(${RegExp.$1}, i)${RegExp.$2}`];
 		}
-
-		for (let i = 0; i < folders.length; i++) {
-			for (let j = 0; j < ends.length; j++) {
-				const
-					fullPath = path.join(folders[i], url, ends[j] || '');
-
-				if (fullPath in exists === false) {
-					exists[fullPath] = hasMagic ? Boolean(glob.sync(fullPath).length) : fs.existsSync(fullPath);
-				}
-
-				if (exists[fullPath]) {
-					return fullPath;
-				}
-			}
-		}
-
-		return url + end;
 	}
 
-	function vueComp({name, attrs}) {
+	delete attrs['v-sync'];
+	delete attrs['v-async'];
+	delete attrs['v-async-back'];
+
+	$C(attrs).forEach((el, key) => {
+		if (key.slice(0, 2) === ':-') {
+			attrs[`:data-${key.slice(2)}`] = el;
+			delete attrs[key];
+
+		} else if (key === ':key') {
+			const
+				parts = el.join('').split(/\s*,\s*/),
+				val = attrs[key] = parts.slice(-1);
+
+			$C(parts.slice(0, -1)).forEach((key) => {
+				if (key.slice(0, 2) === ':-') {
+					attrs[`:data-${key.slice(2)}`] = val;
+
+				} else {
+					attrs[key] = val;
+				}
+			});
+		}
+	});
+
+	if (name === 'component' || validators.blockName(name)) {
+		let componentName;
+		if (attrs[':instance-of']) {
+			componentName = attrs[':instance-of'][0];
+			delete attrs[':instance-of'];
+
+		} else {
+			componentName = name === 'component' ? 'iBlock' : String.camelize(name, false);
+		}
+
+		const
+			c = blocksTree[componentName];
+
+		if (!isAsync && !isAsyncBack && c && c.functional) {
+			isSync = true;
+		}
+
 		$C(attrs).forEach((el, key) => {
-			if (!/^(:|@|v-)/.test(key)) {
+			if (key[0] !== ':') {
 				return;
 			}
 
 			const
-				tmp = String.dasherize(key);
+				base = key.slice(1),
+				prop = `${base}Prop`;
 
-			if (tmp !== key) {
+			if (c && !c.props[base] && c.props[prop]) {
+				attrs[`:${String.dasherize(prop)}`] = el;
 				delete attrs[key];
-				attrs[tmp] = el;
 			}
 		});
 
-		let
-			isSync = attrs['v-sync'];
+		if (!attrs[':mods-prop']) {
+			attrs[':mods-prop'] = ['provideMods()'];
+		}
+	}
 
+	if (!isSync && !attrs.ref && !attrs[':ref'] && (isAsync || isAsyncBack)) {
 		const
-			isAsync = attrs['v-async'],
-			isAsyncBack = attrs['v-async-back'];
+			id = `'${uuid()}' + i`,
+			p = isAsyncBack ? 'Back' : '',
+			key = attrs['v-else'] ? 'v-else-if' : attrs['v-else-if'] ? 'v-else-if' : 'v-if';
 
-		if (attrs['v-for']) {
-			if (/^\s*([\w$]+)(\s+(?:in|of)\s+.*)$/.test(attrs['v-for'].join(' '))) {
-				attrs['v-for'] = [`(${RegExp.$1}, i)${RegExp.$2}`];
-			}
-		}
+		attrs[key] = attachVIf(
+			(attrs[key] || []).concat(`async${p}Components[regAsync${p}Component(${id}, '${name}')]`),
+			isAsyncBack ? '||' : '&&'
+		);
 
-		delete attrs['v-sync'];
-		delete attrs['v-async'];
-		delete attrs['v-async-back'];
+		delete attrs['v-else'];
+	}
+}
 
-		$C(attrs).forEach((el, key) => {
-			if (key.slice(0, 2) === ':-') {
-				attrs[`:data-${key.slice(2)}`] = el;
-				delete attrs[key];
+function vueTag(tag, attrs, rootTag) {
+	if (/^a:void$/.test(tag)) {
+		attrs.href = ['javascript:void(0)'];
+		tag = 'a';
 
-			} else if (key === ':key') {
-				const
-					parts = el.join('').split(/\s*,\s*/),
-					val = attrs[key] = parts.slice(-1);
+	} else if (/^button:a$/.test(tag)) {
+		attrs.type = ['button'];
+		attrs.class = attachClass((attrs.class || []).concat('a'));
+		tag = 'button';
 
-				$C(parts.slice(0, -1)).forEach((key) => {
-					if (key.slice(0, 2) === ':-') {
-						attrs[`:data-${key.slice(2)}`] = val;
-
-					} else {
-						attrs[key] = val;
-					}
-				});
-			}
-		});
-
-		if (name === 'component' || validators.blockName(name)) {
-			let componentName;
-			if (attrs[':instance-of']) {
-				componentName = attrs[':instance-of'][0];
-				delete attrs[':instance-of'];
-
-			} else {
-				componentName = name === 'component' ? 'iBlock' : String.camelize(name, false);
-			}
-
-			const
-				c = blocksTree[componentName];
-
-			if (!isAsync && !isAsyncBack && c && c.functional) {
-				isSync = true;
-			}
-
-			$C(attrs).forEach((el, key) => {
-				if (key[0] !== ':') {
-					return;
-				}
-
-				const
-					base = key.slice(1),
-					prop = `${base}Prop`;
-
-				if (c && !c.props[base] && c.props[prop]) {
-					attrs[`:${String.dasherize(prop)}`] = el;
-					delete attrs[key];
-				}
-			});
-
-			if (!attrs[':mods-prop']) {
-				attrs[':mods-prop'] = ['provideMods()'];
-			}
-		}
-
-		if (!isSync && !attrs.ref && !attrs[':ref'] && (isAsync || isAsyncBack)) {
-			const
-				id = `'${uuid()}' + i`,
-				p = isAsyncBack ? 'Back' : '',
-				key = attrs['v-else'] ? 'v-else-if' : attrs['v-else-if'] ? 'v-else-if' : 'v-if';
-
-			attrs[key] = attachVIf(
-				(attrs[key] || []).concat(`async${p}Components[regAsync${p}Component(${id}, '${name}')]`),
-				isAsyncBack ? '||' : '&&'
-			);
-
-			delete attrs['v-else'];
-		}
+	} else if (tag === '_') {
+		tag = rootTag;
 	}
 
-	function vueTag(tag, attrs, rootTag) {
-		if (/^a:void$/.test(tag)) {
-			attrs.href = ['javascript:void(0)'];
-			tag = 'a';
+	return tag;
+}
 
-		} else if (/^button:a$/.test(tag)) {
-			attrs.type = ['button'];
-			attrs.class = attachClass((attrs.class || []).concat('a'));
-			tag = 'button';
+function attachVIf(arr, op) {
+	const
+		join = arr.join;
 
-		} else if (tag === '_') {
-			tag = rootTag;
+	arr.join = function () {
+		return join.call(this, op);
+	};
+
+	return arr;
+}
+
+function bem2vue(block, attrs, rootTag, val) {
+	const
+		tmp = attrs[':class'] = attrs[':class'] || [];
+
+	if (!$C(tmp).includes('blockId')) {
+		attrs[':class'] = attachClass(tmp.concat('blockId', `classes['${val.replace(/^_+/, '')}']`));
+	}
+
+	return block + val;
+}
+
+function attachClass(arr) {
+	const
+		join = arr.join;
+
+	arr.join = function () {
+		if (this.length < 2) {
+			return join.call(this);
 		}
 
-		return tag;
-	}
+		return `[${join.call(this, ',')}]`;
+	};
 
-	function attachVIf(arr, op) {
-		const
-			join = arr.join;
-
-		arr.join = function () {
-			return join.call(this, op);
-		};
-
-		return arr;
-	}
-
-	function bem2vue(block, attrs, rootTag, val) {
-		const
-			tmp = attrs[':class'] = attrs[':class'] || [];
-
-		if (!$C(tmp).includes('blockId')) {
-			attrs[':class'] = attachClass(tmp.concat('blockId', `classes['${val.replace(/^_+/, '')}']`));
-		}
-
-		return block + val;
-	}
-
-	function attachClass(arr) {
-		const
-			join = arr.join;
-
-		arr.join = function () {
-			if (this.length < 2) {
-				return join.call(this);
-			}
-
-			return `[${join.call(this, ',')}]`;
-		};
-
-		return arr;
-	}
-};
+	return arr;
+}
