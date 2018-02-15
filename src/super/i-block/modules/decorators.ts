a0090a87ecf3114e2b121470ba15fa8609355685
kobezzza <kobezzza@gmail.com>
2018-02-15 18:10:06 +0300

    Added wait decorator

--- src/super/i-block/modules/decorators.ts
+++ src/super/i-block/modules/decorators.ts
@@ -6,7 +6,9 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import iBlock from 'super/i-block/i-block';
+import iBlock, { statuses } from 'super/i-block/i-block';
+import { AsyncOpts } from 'core/async';
+
 import {
 
 	field as fieldDecorator,
@@ -34,3 +36,92 @@ export const field = fieldDecorator as (params?: InitFieldFn | ComponentField) =
  * @override
  */
 export const system = systemDecorator as (params?: InitFieldFn | ComponentField) => Function;
+
+/**
+ * Decorates a method or a function for using with the specified init status
+ *
+ * @see Async.wait
+ * @decorator
+ * @param status
+ * @param [params] - additional parameters:
+ *   *) [params.fn] - callback function
+ *   *) [params.defer] - if true, then the function will always return a promise
+ */
+export function wait(
+	status: number | string,
+	params: AsyncOpts & {fn?: Function; defer?: boolean | number} | Function
+
+) {
+	let {
+		join,
+		label,
+		group,
+		defer,
+		fn
+	} = typeof params !== 'function' ? params : <Dictionary>{};
+
+	if (Object.isString(status)) {
+		status = statuses[status];
+	}
+
+	let
+		handler = <Function>(fn || params);
+
+	/** @this {iBlock} */
+	function wrapper() {
+		const
+			args = arguments;
+
+		if (join === undefined) {
+			join = handler.length ? 'replace' : true;
+		}
+
+		const
+			{async: $a, block: $b} = this,
+			p = {join, label, group};
+
+		function reject(err) {
+			if (err.type !== 'clear') {
+				throw err;
+			}
+		}
+
+		if ($b) {
+			if (status > 0 && $b.status < 0) {
+				return;
+			}
+
+			if ($b.status >= status) {
+				if (defer) {
+					return $a.promise(
+						(async () => {
+							await $a.nextTick();
+							return handler.apply(this, args);
+						})(),
+
+						p
+					).catch(reject);
+				}
+
+				return handler.apply(this, args);
+			}
+		}
+
+		return $a.promise(
+			new Promise((resolve) =>
+				this.localEvent.once(`block.status.${statuses[status]}`, () => resolve(handler.apply(this, args)))
+			),
+
+			p
+		).catch(reject);
+	}
+
+	if (Object.isFunction(handler)) {
+		return wrapper;
+	}
+
+	return (target, key, descriptors) => {
+		handler = descriptors.value;
+		descriptors.value = wrapper;
+	};
+}
