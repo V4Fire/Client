96230e6d63f736c658f57be0d8af9eb193b2a3e9
kobezzza <kobezzza@gmail.com>
2018-02-26 17:36:55 +0300

    Added i18n

--- src/super/i-page/i-page.ts
+++ src/super/i-page/i-page.ts
@@ -6,7 +6,43 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import iData, { component } from 'super/i-data/i-data';
+import iData, { component, field, system, watch } from 'super/i-data/i-data';
+import { setLang, lang } from 'core/i18n';
+export * from 'super/i-data/i-data';
 
 @component()
-export default class iPage<T extends Dictionary = Dictionary> extends iData<T> {}
+export default class iPage<T extends Dictionary = Dictionary> extends iData<T> {
+	/**
+	 * Link to i18n function
+	 */
+	@system()
+	protected readonly i18n: typeof i18n = i18n;
+
+	/**
+	 * System language
+	 */
+	get lang(): string {
+		return this.langStore;
+	}
+
+	/**
+	 * Sets a new system language
+	 */
+	set lang(value: string) {
+		setLang(this.langStore = value);
+	}
+
+	/**
+	 * System language store
+	 */
+	@field()
+	protected langStore: string = lang;
+
+	/**
+	 * Synchronization for the langStore field
+	 */
+	@watch('langStore')
+	protected syncLangWatcher(): void {
+		this.$forceUpdate();
+	}
+}
