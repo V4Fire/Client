a89275416884f6720e84e6e95794649ba12f9254
kobezzza <kobezzza@gmail.com>
2018-03-02 17:38:36 +0300

    :wrench:

--- src/form/b-input/b-input.ts
+++ src/form/b-input/b-input.ts
@@ -11,7 +11,7 @@
 import $C = require('collection.js');
 import symbolGenerator from 'core/symbol';
 import KeyCodes from 'core/keyCodes';
-import BlockValidators from './modules/validators';
+import BlockValidators from 'form/b-input/modules/validators';
 import iInput, {
 
 	component,
@@ -25,6 +25,8 @@ import iInput, {
 	PARENT
 
 } from 'super/i-input/i-input';
+
+export * from 'super/i-input/i-input';
 
 export const
 	$$ = symbolGenerator();
