36283eddc96e1d3d94de309864942ac10bffa80f
kobezzza <kobezzza@gmail.com>
2018-03-19 13:31:35 +0300

    Moved to TS

---
+++ src/base/b-up/b-up.ts
@@ -0,0 +1,41 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import symbolGenerator from 'core/symbol';
+import iBlock, { component, ModsDecl } from 'super/i-block/i-block';
+export * from 'super/i-block/i-block';
+
+export const
+	$$ = symbolGenerator();
+
+@component()
+export default class bUp extends iBlock {
+	/** @inheritDoc */
+	static mods: ModsDecl = {
+		hidden: [
+			['true'],
+			'false'
+		]
+	};
+
+	/** @override */
+	protected async mounted(): Promise<void> {
+		await super.mounted();
+
+		const
+			{async: $a} = this;
+
+		$a.on(document, 'scroll', () => this.setMod('hidden', !(pageYOffset > innerHeight / 3)), {
+			label: $$.scroll
+		});
+
+		$a.on(this.$el, 'click', () => window.scrollTo(0, 0), {
+			label: $$.up
+		});
+	}
+}
