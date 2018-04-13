a9d058a72ea50c2dd19da79cf79d8ea60baf73bf
kobezzza <kobezzza@gmail.com>
2018-04-13 16:37:56 +0300

    Refactoring

--- src/form/b-textarea/b-textarea.ts
+++ src/form/b-textarea/b-textarea.ts
@@ -8,7 +8,7 @@
 
 import symbolGenerator from 'core/symbol';
 import bScrollInline from 'base/b-scroll/b-scroll-inline/b-scroll-inline';
-import bInput, { component, prop, system, p, wait, watch, ModsDecl } from 'form/b-input/b-input';
+import bInput, { component, prop, system, p, wait, watch, ModsDecl, VueElement } from 'form/b-input/b-input';
 export * from 'form/b-input/b-input';
 
 export const
@@ -86,7 +86,7 @@ export default class bTextarea<T extends Dictionary = Dictionary> extends bInput
 	// @ts-ignore
 	protected readonly $refs!: {
 		superWrapper: HTMLElement;
-		scroll: bScrollInline;
+		scroll: VueElement<bScrollInline>;
 		input: HTMLTextAreaElement;
 	};
 
@@ -96,7 +96,7 @@ export default class bTextarea<T extends Dictionary = Dictionary> extends bInput
 	@wait('ready', {label: $$.calcHeight, defer: true})
 	async calcHeight(): Promise<number | void> {
 		const
-			{input, scroll} = this.$refs,
+			{input, scroll} = this.refs,
 			{length} = this.value;
 
 		if (input.scrollHeight <= input.clientHeight) {
@@ -135,7 +135,7 @@ export default class bTextarea<T extends Dictionary = Dictionary> extends bInput
 	@wait('ready', {label: $$.minimize, defer: true})
 	protected async minimize(): Promise<number> {
 		const
-			{input, scroll} = this.$refs;
+			{input, scroll} = this.refs;
 
 		const
 			val = this.value,
