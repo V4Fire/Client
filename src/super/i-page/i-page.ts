727512859050acb1b66518cbcead8d6b9eb06beb
kobezzza <kobezzza@gmail.com>
2018-05-10 19:16:09 +0300

    Added setRootMod and removeRootMod

--- src/super/i-page/i-page.ts
+++ src/super/i-page/i-page.ts
@@ -50,6 +50,60 @@ export default class iPage<
 	protected langStore: string = lang;
 
 	/**
+	 * Sets a modifier to the root element
+	 *
+	 * @param name
+	 * @param value
+	 */
+	setRootMod(name: string, value: any): boolean {
+		if (value === undefined) {
+			return false;
+		}
+
+		const
+			root = document.documentElement,
+			cl = root.classList;
+
+		name = name.camelize(false);
+		value = String(value).camelize(false);
+
+		const
+			mod = `${name}_${value}`;
+
+		if (cl.contains(mod)) {
+			return false;
+		}
+
+		root.className = root.className.replace(new RegExp(`(?:^|\\s+)${name}_[^\\s]+`), '');
+		cl.add(mod);
+		return true;
+	}
+
+	/**
+	 * Removes a modifier from the root element
+	 *
+	 * @param name
+	 * @param value
+	 */
+	removeRootMod(name: string, value?: any): boolean {
+		const
+			root = document.documentElement;
+
+		name = name.camelize(false);
+		value = value !== undefined ? String(value).camelize(false) : undefined;
+
+		let
+			res = false;
+
+		root.className = root.className.replace(new RegExp(`(?:^|\\s+)${name}_([^\\s]+)`), (str, v) => {
+			res = value === undefined || v === value;
+			return res ? '' : str;
+		});
+
+		return res;
+	}
+
+	/**
 	 * Synchronization for the langStore field
 	 */
 	@watch('langStore')
