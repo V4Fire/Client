d548b2dbd877ca406d5bec53e797fb54ca5f4188
kobezzza <kobezzza@gmail.com>
2018-05-20 20:14:46 +0300

    Fixed $normalParent for non functional components

--- src/core/component/component.ts
+++ src/core/component/component.ts
@@ -73,7 +73,14 @@ export function getComponent(
 			const
 				ctx = <any>this;
 
-			ctx.$normalParent = ctx.$parent;
+			let
+				p = ctx.$parent;
+
+			while (p && p.isFunctional) {
+				p = p.$parent;
+			}
+
+			ctx.$normalParent = p;
 			ctx.$state = state;
 			ctx.$async = new Async(this);
 			ctx.instance = instance;
