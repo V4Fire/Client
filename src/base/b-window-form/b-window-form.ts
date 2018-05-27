0138f8045fa973b3a139a27cdb6d38867bcd55a1
kobezzza <kobezzza@gmail.com>
2018-05-27 19:12:28 +0300

    :wrench:

--- src/base/b-window-form/b-window-form.ts
+++ src/base/b-window-form/b-window-form.ts
@@ -105,7 +105,7 @@ export default class bWindowForm<T extends Dictionary = Dictionary> extends bWin
 			if (await this.reset() || this.db) {
 				this.formTmp = {};
 				this.id = undefined;
-				this.db = null;
+				this.db = undefined;
 			}
 		}
 
