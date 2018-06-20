23b7664589d34d44789538e1556b709bd565fbd3
kobezzza <kobezzza@gmail.com>
2018-06-20 17:38:44 +0300

    Refactoring

--- src/core/const/browser.ts
+++ src/core/const/browser.ts
@@ -29,6 +29,59 @@ export function match(pattern: RegExp | string): [string, number[] | null] | boo
 	return res ? [res[1], res[2] ? res[2].split('.').map(parseInt) : null] : false;
 }
 
+/**
+ * Returns true if navigator.userAgent matches the specified parameters
+ *
+ * @param platform - browser platform
+ * @param [operation] - operation type (>, >=, etc.)
+ * @param [version] - browser version
+ */
+export function test(platform: string, operation?: Operations, version?: string): boolean {
+	const
+		val = this[platform];
+
+	if (!val) {
+		return false;
+	}
+
+	if (!operation || !version) {
+		return true;
+	}
+
+	if (!val[1]) {
+		return false;
+	}
+
+	const
+		v1 = val[1],
+		v2 = version.split('.').map(parseInt);
+
+	const
+		gt = v1[0] > v2[0] || v1[1] > v2[1] || v1[2] > v2[2],
+		lt = v1[0] < v2[0] || v1[1] < v2[1] || v1[2] < v2[2],
+		eq = v1.join() === v2.join();
+
+	switch (operation) {
+		case '>':
+			return gt;
+
+		case '>=':
+			return gt || eq;
+
+		case '<':
+			return lt;
+
+		case '<=':
+			return lt || eq;
+
+		case '==':
+			return eq;
+
+		case '^=':
+			return v1[0] === v2[0];
+	}
+}
+
 export const is = {
 	Firefox: match('Firefox'),
 	Android: match('Android'),
@@ -38,62 +91,9 @@ export const is = {
 	WindowsMobile: match('IEMobile'),
 
 	/**
-	 * Returns true if navigator.userAgent matches the specified parameters
-	 *
-	 * @param platform - browser platform
-	 * @param [operation] - operation type (>, >=, etc.)
-	 * @param [version] - browser version
-	 */
-	test(platform: string, operation?: Operations, version?: string): boolean {
-		const
-			val = this[platform];
-
-		if (!val) {
-			return false;
-		}
-
-		if (!operation || !version) {
-			return true;
-		}
-
-		if (!val[1]) {
-			return false;
-		}
-
-		const
-			v1 = val[1],
-			v2 = version.split('.').map(parseInt);
-
-		const
-			gt = v1[0] > v2[0] || v1[1] > v2[1] || v1[2] > v2[2],
-			lt = v1[0] < v2[0] || v1[1] < v2[1] || v1[2] < v2[2],
-			eq = v1.join() === v2.join();
-
-		switch (operation) {
-			case '>':
-				return gt;
-
-			case '>=':
-				return gt || eq;
-
-			case '<':
-				return lt;
-
-			case '<=':
-				return lt || eq;
-
-			case '==':
-				return eq;
-
-			case '^=':
-				return v1[0] === v2[0];
-		}
-	},
-
-	/**
 	 * Version of the current mobile browser or false
 	 */
 	get mobile(): string[] | boolean {
-		return this.android || this.blackberry || this.ios || this.operamini || this.windowsmobile || false;
+		return this.Android || this.BlackBerry || this.iOS || this.OperaMini || this.WindowsMobile || false;
 	}
 };
