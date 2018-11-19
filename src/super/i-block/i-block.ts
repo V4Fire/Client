71d79c40ff161427fb0d3b202f62e70adca880ce
bonkalol <bogdan@edadeal.ru>
2018-11-19 17:58:09 +0300

    Fix context being lost

--- src/super/i-block/i-block.ts
+++ src/super/i-block/i-block.ts
@@ -3470,10 +3470,10 @@ export default class iBlock extends ComponentInterface<iBlock, iStaticPage> {
 			}
 
 			const
-				fn = $a[`mute-${names[key]}`.camelize(false)];
+				fn = this.async[`mute-${names[key]}`.camelize(false)];
 
 			if (Object.isFunction(fn)) {
-				fn();
+				fn.call($a);
 			}
 		}
 
