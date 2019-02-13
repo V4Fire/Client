a220b4a47c63e9521c14870d4074495dd29dad41
kobezzza <kobezzza@gmail.com>
2019-02-13 16:23:56 +0300

    Fixed modifiers with functional components

--- src/super/i-block/modules/block.ts
+++ src/super/i-block/modules/block.ts
@@ -201,17 +201,27 @@ export default class Block {
 		value = String(value).dasherize();
 
 		const
-			{mods, node} = this,
+			{mods, node} = this;
+
+		const
+			initSetMod = reason === 'initSetMod',
+			funcInit = initSetMod && this.component.isFunctional,
 			prev = this.getMod(name);
 
 		if (prev !== value) {
-			this.removeMod(name, undefined, 'setMod');
+			if (!funcInit) {
+				this.removeMod(name, undefined, 'setMod');
+			}
 
 			if (mods) {
 				mods[name] = <string>value;
 			}
 
-			if (reason !== 'initSetMod' && node) {
+			if (node && (!initSetMod || funcInit)) {
+				if (funcInit) {
+					this.removeMod(name, undefined, 'initSetMod');
+				}
+
 				node.classList.add(this.getFullBlockName(name, value));
 			}
 
@@ -244,7 +254,7 @@ export default class Block {
 
 		const
 			{mods, node} = this,
-			current = this.getMod(name);
+			current = this.getMod(name, reason === 'initSetMod');
 
 		if (current !== undefined && (value === undefined || current === value)) {
 			if (mods) {
@@ -272,13 +282,15 @@ export default class Block {
 
 	/**
 	 * Returns a value of the specified block modifier
+	 *
 	 * @param mod
+	 * @param [strict] - if true, then the modifier value will always taken from a dom node
 	 */
-	getMod(mod: string): CanUndef<string> {
+	getMod(mod: string, strict?: boolean): CanUndef<string> {
 		const
 			{mods, node} = this;
 
-		if (mods) {
+		if (mods && !strict) {
 			return mods[mod.camelize(false)];
 		}
 
