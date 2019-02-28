2553f32ddf3e96f410e974debc021ddde31b148f
kobezzza <kobezzza@gmail.com>
2019-02-28 16:53:43 +0300

    Refactoring

--- src/base/b-image/b-image.ts
+++ src/base/b-image/b-image.ts
@@ -7,55 +7,34 @@
  */
 
 import symbolGenerator from 'core/symbol';
-import iMessage, { component, prop, wait, hook } from 'super/i-message/i-message';
+
+import iProgress from 'traits/i-progress/i-progress';
+import iVisible from 'traits/i-visible/i-visible';
+
+import iMessage, { component, prop, wait, hook, ModsDecl } from 'super/i-message/i-message';
 export * from 'super/i-message/i-message';
 
 export const
 	$$ = symbolGenerator();
 
 @component({functional: true})
-export default class bImage extends iMessage {
+export default class bImage extends iMessage implements iProgress, iVisible {
 	/**
 	 * Target image src
 	 */
 	@prop({type: String, watch: {fn: 'initOverlay', immediate: true}})
 	readonly src!: string;
 
+	/** @inheritDoc */
+	static readonly mods: ModsDecl = {
+		...iProgress.mods,
+		...iVisible.mods
+	};
+
 	/** @override */
 	protected readonly $refs!: {
 		img: HTMLImageElement;
 	};
-
-	/**
-	 * Handler: image loaded
-	 *
-	 * @param img
-	 * @emits load
-	 */
-	protected onImageLoaded(img: HTMLImageElement): void {
-		this.setMod('progress', false);
-		this.setMod('showError', false);
-
-		this.$refs.img.insertAdjacentElement('afterbegin', img);
-		this.emit('load');
-	}
-
-	/**
-	 * Handler: image load error
-	 *
-	 * @param err
-	 * @emits loadError
-	 */
-	protected onImageError(err: Error): void {
-		this.setMod('progress', false);
-		this.setMod('showError', true);
-		this.emit('loadError', err);
-	}
-
-	/** @override */
-	protected initCloseHelpers(): void {
-		return;
-	}
 
 	/**
 	 * Initializes an image loading process
@@ -86,12 +65,44 @@ export default class bImage extends iMessage {
 	 */
 	@hook('beforeDestroy')
 	protected destroyImage(): void {
-		const {img} = this.$refs;
+		const
+			{img} = this.$refs;
 
 		if (img.innerHTML) {
 			this.tmp[this.src] = img.innerHTML;
 		}
 
 		img.innerHTML = '';
+	}
+
+	/** @override */
+	protected initModEvents(): void {
+		super.initModEvents();
+		iVisible.initModEvents(this);
+	}
+
+	/**
+	 * Handler: image loaded
+	 *
+	 * @param img
+	 * @emits load
+	 */
+	protected onImageLoaded(img: HTMLImageElement): void {
+		this.setMod('progress', false);
+		this.setMod('showError', false);
+		this.$refs.img.insertAdjacentElement('afterbegin', img);
+		this.emit('load');
+	}
+
+	/**
+	 * Handler: image load error
+	 *
+	 * @param err
+	 * @emits loadError
+	 */
+	protected onImageError(err: Error): void {
+		this.setMod('progress', false);
+		this.setMod('showError', true);
+		this.emit('loadError', err);
 	}
 }
