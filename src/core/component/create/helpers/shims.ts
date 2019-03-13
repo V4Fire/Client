d76097e482b17f96733a4879da952afcb4aec6e0
kobezzza <kobezzza@gmail.com>
2019-03-13 13:50:51 +0300

    Removed elAccessor

--- src/core/component/create/helpers/shims.ts
+++ src/core/component/create/helpers/shims.ts
@@ -6,40 +6,7 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import { ComponentInterface, ComponentElement } from 'core/component/interface';
 import { EventEmitter2 as EventEmitter } from 'eventemitter2';
-
-/**
- * Adds an $el accessor to the specified component
- *
- * @param elId - element unique id
- * @param ctx - component context
- */
-export function addElAccessor(elId: symbol, ctx: ComponentInterface): void {
-	let
-		staticEl;
-
-	Object.defineProperty(ctx, '$el', {
-		set(val: Element): void {
-			staticEl = val;
-		},
-
-		get(): CanUndef<ComponentElement<any>> {
-			if (staticEl) {
-				return staticEl;
-			}
-
-			const
-				el = <Element>ctx[elId];
-
-			if (el && el.closest('html')) {
-				return el;
-			}
-
-			return (ctx[elId] = document.querySelector(`.i-block-helper.${ctx.componentId}`) || undefined);
-		}
-	});
-}
 
 /**
  * Adds the component event API to the specified component
