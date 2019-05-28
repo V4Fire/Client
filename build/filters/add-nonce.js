2dfa8823aa22011e90768240f06c31987b5ccbfb
Valera <v-chupurnov@edadeal.ru>
2019-05-28 14:19:40 +0300

    Remove new line

--- build/filters/add-nonce.js
+++ build/filters/add-nonce.js
@@ -11,7 +11,6 @@
 const
 	tagReg = /^(['"])<(link|script)([^>]*)>/;
 
-
 exports.addNonce = function (tag) {
 	if (tagReg.test(tag)) {
 		return tag.replace(tagReg, `$1<$2$3$1 + (typeof GLOBAL_NONCE === 'string' ? ' nonce="' + GLOBAL_NONCE + '"') + $1>`);
