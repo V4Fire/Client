5a529422067e9777d05dc23724a585bea126f1ee
bonkalol <bonkalol@list.ru>
2019-07-03 10:53:49 +0300

    WIP

--- src/super/i-block/modules/animate.ts
+++ src/super/i-block/modules/animate.ts
@@ -10,10 +10,34 @@ import iBlock from 'super/i-block/i-block';
 import Block from 'super/i-block/modules/block';
 import Async, { AsyncOpts } from 'core/async';
 
+export type PropertyValue = string | number | number[];
+
+export interface Properties extends Dictionary<PropertyValue> {
+	time?: number;
+}
+
+export type NonAnimatedProperties =
+	'time' |
+	'transition' |
+	'display' |
+	'pointerEvents';
+
+export type AnimatedProperties = Omit<Dictionary<PropertyValue>, NonAnimatedProperties>;
+
 /**
  * Base class from Animation API
  */
 export default class Animate {
+	/**
+	 * Properties that cannot be animated
+	 */
+	static nonAnimatedProps: NonAnimatedProperties[] = [
+		'transition',
+		'display',
+		'time',
+		'pointerEvents'
+	];
+
 	/**
 	 * Link to component async module
 	 */
@@ -62,33 +86,58 @@ export default class Animate {
 	 * Start an animation
 	 *
 	 * @param props
+	 *   *) time - transition duration in ms
 	 * @param [el] - reference to element or element name
 	 * @param [asyncOpts]
 	 */
-	run(
-		props: Dictionary<string | number>,
-		el: HTMLElement | string = this.$el,
-		asyncOpts: AsyncOpts = {}
-	): Promise<Animate> {
-		const
-			{block: $b} = this;
-
+	run(props: Properties, el: HTMLElement | string = this.$el, asyncOpts: AsyncOpts = {}): Promise<Animate> {
 		asyncOpts = {
 			group: '[[ANIMATE]]',
 			label: String(Math.random()),
 			...asyncOpts
 		};
 
-		el = el instanceof HTMLElement ? el : <HTMLElement>$b.element(el) || this.$el;
+		const
+			{async: $a, block: $b} = this,
+			loopback = $a.promise<Animate>(new Promise((r) => r(this.animate)), asyncOpts);
 
+		el = el instanceof HTMLElement ? el : <HTMLElement>$b.element(el);
+
+		if (!(el instanceof HTMLElement)) {
+			return loopback;
+		}
+
+		if (!el.style.transition && !props.transition && !props.time) {
+			return loopback;
+		}
+
+		const
+			rejected = Object.reject(props, Animate.nonAnimatedProps.concat('time')),
+			keys = Object.keys(rejected),
+			baseTime = props.time && props.time / 1000;
+
+		Object.assign(el.style, rejected);
+	}
+
+	/**
+	 * Generates a promise which waiting for all transition end
+	 */
+	protected awaiter(el: HTMLElement, props: string[]): Promise<Animate> {
 		const
 			{async: $a} = this;
 
-		if (!el || (!el.style.transition && !props.transition && !props.time)) {
-			return $a.promise(new Promise((r) => r(this.animate)), asyncOpts);
-		}
+		let
+			animatedFields = 0;
 
-		Object.assign(el.style, props);
-		return $a.promisifyOnce(el, 'transitionend', asyncOpts).then(() => this.animate);
+		return $a.promise<Animate>(new Promise((r, rej) => {
+			$a.on(<HTMLElement>el, 'transitionend', (e) => {
+				animatedFields++;
+
+				if (animatedFields === props.length) {
+					r(this.animate);
+				}
+
+			}, this.async);
+		}));
 	}
 }
