f6eb6dbbb4089f087da6482d746089eebcf90f77
bonkalol <bonkalol@list.ru>
2019-07-22 00:47:13 +0300

    Working on step builder

--- src/super/i-block/modules/transition.ts
+++ src/super/i-block/modules/transition.ts
@@ -14,13 +14,13 @@ import Async from 'core/async';
 
 export type StyleValuePrimitive = string | number;
 export type StyleValueAsFn = (...args: any[]) => StyleValuePrimitive;
-export type StyleValuesAsPromise = Promise<StyleValuePrimitive>;
+export type StyleValuesAsPromise = Promise<StyleValuePrimitive | StyleValueAsFn>;
 export type StyleValue = StyleValuePrimitive | StyleValueAsFn | StyleValuesAsPromise;
 export type StyleDictionary = Dictionary<StyleValue>;
 
 export type TransitionMode = 'sequence' | 'parallel';
 export type VisibilityMode = 'visible' | 'hidden';
-export type TransitionMethod = 'run' | 'visible' | 'hidden';
+export type TransitionMethod = 'step' | 'visible' | 'hidden';
 
 export const NON_ANIMATED_PROPERTIES = {
 	transition: true,
@@ -41,10 +41,6 @@ export enum TRANSITION_STATES {
 	end = 2
 }
 
-export interface GPUParams {
-	overrideTransform?: boolean;
-}
-
 export interface TransitionCtx {
 	ctx: iBlock;
 	label: Label;
@@ -54,22 +50,14 @@ export interface TransitionCtx {
 export interface TransitionInfo {
 	state: TRANSITION_STATES;
 	props: StyleDictionary;
-	node: HTMLElement;
+	nodes: HTMLElement;
+	targets: Targets;
 	initialProps: Dictionary;
-	// mode: TransitionMode;
-	// parent: Transition;
-	// method: TransitionMethod;
-	duration?: number;
-	delay?: number;
-	promise?: Promise<unknown>;
+	inheritedProps: StyleDictionaryAndParams;
+	duration: number;
+	delay: number;
+	promise: Promise<unknown>;
 	resolver(res: Function, rej: Function): unknown;
-}
-
-export interface TransitionParams {
-	node: HTMLElement;
-	props: StyleDictionary;
-	duration?: number;
-	delay?: number;
 }
 
 export interface TransitionReverseOptions {
@@ -79,11 +67,33 @@ export interface TransitionReverseOptions {
 
 export interface TransitionCreateParams {
 	useGPU?: boolean;
+	inheritProps?: boolean;
+	reusable?: boolean;
 }
 
-export type TransitionStack = Array<TransitionInfo | Transition>;
-export type TransitionDirection = 'forward' | 'revers';
+export type Fn<T> = () => T;
+export type StyleDictionaryParam = number | CanArray<number> | Fn<CanArray<number>>;
+
+export interface TransitionParams {
+	offset?: StyleDictionaryParam;
+	delay?: StyleDictionaryParam;
+	duration?: StyleDictionaryParam;
+	each?: boolean;
+	visible?: CanUndef<boolean>;
+}
+
+export interface TransitionStep {
+	stack: TransitionInfo[];
+	mode: TransitionMode;
+	state: TRANSITION_STATES;
+}
+
+export type StyleDictionaryAndParams = TransitionParams & StyleDictionary;
+export type TransitionStack = TransitionInfo[];
+export type TransitionDirection = 'forward' | 'backwards';
 export type Target = HTMLElement | string;
+export type Targets = CanArray<Target>;
+export type TargetsAsArray = Array<Target>;
 export type NonAnimatedProperties = typeof NON_ANIMATED_PROPERTIES;
 
 /**
@@ -120,21 +130,7 @@ export class Transition {
 	 * True, if transitions finished
 	 */
 	get isFulfilled(): boolean {
-		return this.stack.every((info: Transition | TransitionInfo) => {
-			if (info instanceof Transition) {
-				return info.isFulfilled;
-			}
-
-			return info.state === TRANSITION_STATES.end;
-		});
-	}
-
-	/**
-	 * Next transition
-	 */
-	get next(): CanUndef<TransitionInfo> {
-		//..
-		return;
+		return this.steps.every((info: TransitionStep) => info.state === TRANSITION_STATES.end);
 	}
 
 	/**
@@ -183,9 +179,9 @@ export class Transition {
 	protected params: TransitionCreateParams;
 
 	/**
-	 * Stack of transitions
+	 * Steps of transitions
 	 */
-	protected stack: TransitionStack = [];
+	protected steps: TransitionStep[] = [];
 
 	/**
 	 * List of subscribers
@@ -227,7 +223,7 @@ export class Transition {
 	 * Last transition from stack
 	 */
 	protected get last(): Transition | TransitionInfo {
-		return this.stack[this.stack.length - 1];
+		return this.step[this.step.length - 1];
 	}
 
 	/**
@@ -246,94 +242,28 @@ export class Transition {
 	}
 
 	/**
-	 * Runs a transition
+	 * Register a new transition step
 	 *
-	 * @param el
+	 * @param elements
 	 * @param props
-	 * @param duration - transition time in seconds
-	 * @param [delay] - delay before transition start in seconds
+	 * @param params
 	 */
-	run(el: Target, props: StyleDictionary, duration: number, delay?: number): Transition {
-		const
-			{async: $a} = this,
-			node = this.getNode(el);
-
-		if (!node) {
-			return this;
+	step(elements: Targets, props: CanArray<StyleDictionaryAndParams>, params: TransitionParams): Transition {
+		if (!Object.isArray(elements)) {
+			elements = (<TargetsAsArray>[]).concat(elements);
 		}
 
-		const
-			nonAnimatedProperties = Object.select(props, NON_ANIMATED_PROPERTIES),
-			animatedProperties = Object.reject(props, NON_ANIMATED_PROPERTIES),
+		for (let i = 0; i < elements.length; i++) {
+			//...
+		}
 
-			uniqId = Math.random(),
-			keys = Object.keys(animatedProperties);
-
-		const resolver = (resolve) => {
-			let doneCounter = 0;
-			Object.assign(node.style, nonAnimatedProperties);
-
-			new Promise((res, rej) => {
-				$a.requestAnimationFrame(() => {
-					// Сравнить значения с текущими значениями элемента через dummy
-					res();
-				}, {group: TRANSITION_GROUP});
-
-			}).then(() => {
-				$a.on(node, 'transitionend', (e: TransitionEvent) => {
-					if (e.target !== node || !animatedProperties[e.propertyName]) {
-						return;
-					}
-
-					if (doneCounter++ === keys.length) {
-						$a.off({label: uniqId, group: TRANSITION_GROUP});
-						resolve();
-					}
-
-				}, {label: uniqId, group: TRANSITION_GROUP});
-
-			}).catch((err) => {
-				stderr(err);
-				resolve();
-
-			});
-
-		};
-
-		this.append({node, props}, resolver);
 		return this;
 	}
 
 	/**
-	 * Makes an element visible
-	 *
-	 * @param el
-	 * @param [props]
-	 */
-	visible(el: Target, props?: StyleDictionary): Transition {
-		return this.toggleVisibility(el, 'visible', props);
-	}
-
-	/**
-	 * Makes an element hidden
-	 *
-	 * @param el
-	 * @param [props]
-	 */
-	hidden(el: Target, props?: StyleDictionary): Transition {
-		return this.toggleVisibility(el, 'hidden', props);
-	}
-
-	/**
 	 * Repeats transition specified times
-	 *
-	 * @param el
-	 * @param props
-	 * @param duration
-	 * @param [delay]
-	 * @param [repeat]
 	 */
-	repeat(el: Target, props: StyleDictionary, duration: number, delay?: number, repeat?: number): Transition {
+	repeat(): Transition {
 		return this;
 	}
 
@@ -352,191 +282,78 @@ export class Transition {
 	}
 
 	/**
-	 * Creates a new sequence timeline
-	 *
-	 * @param createFn
-	 * @returns Transition – current transition timeline
-	 */
-	sequence(createFn: (ctx: Transition) => Transition): Transition {
-		this.create(createFn, 'sequence');
-		return this;
-	}
-
-	/**
-	 * Creates a parallel
-	 *
-	 * @param createFn
-	 * @returns Transition – current transition timeline
-	 */
-	parallel(createFn: (ctx: Transition) => Transition): Transition {
-		this.create(createFn, 'parallel');
-		return this;
-	}
-
-	/**
 	 * @param resolve
 	 * @param reject
 	 */
 	then(resolve: Function): Transition {
-		if (this.isFulfilled) {
-			Promise.resolve().then(() => resolve(this));
-			return this;
-		}
-
-		const
-			transition = this.last;
-
-		// Возможно стоит биндить каждый then к определенной transition
-		// Точно стоит
-
-		this.subscribers.push(resolve);
 		return this;
 	}
 
 	/**
-	 * Creates a new transition timeline in current timeline
-	 *
-	 * @param createFn
-	 * @param mode
+	 * Runs a specified step
+	 * @param step
 	 */
-	protected create(createFn: (ctx: Transition) => unknown, mode: TransitionMode): Transition {
-		const
-			transition = new Transition(this.component, this.label, mode);
-
-		this.stack.push(transition);
-		createFn(transition);
-		return transition;
+	protected run(step: TransitionInfo): void {
+		return;
 	}
 
 	/**
-	 * Change elements visibility
+	 * Makes an element hidden
 	 *
 	 * @param el
-	 * @param mode
 	 * @param [props]
 	 */
-	protected toggleVisibility(el: Target, mode: VisibilityMode, props?: StyleDictionary): Transition {
-		props = {
-			...(mode === 'visible' ? this.visibleProps : this.hiddenProps),
-			...props
-		};
-
-		const
-			{async: $a} = this,
-			node = this.getNode(el);
-
-		if (!node) {
-			return this;
-		}
-
-		const resolver = (resolve) => {
-			Object.assign(node.style, props);
-
-			$a.requestAnimationFrame(() => {
-				resolve();
-
-			}, {group: TRANSITION_GROUP});
-		};
-
-		this.append({node, props}, resolver);
+	protected hide(el: Target, props?: StyleDictionary): Transition {
 		return this;
 	}
 
 	/**
-	 * Initializes an animation
+	 * Makes an element visible
+	 *
+	 * @param el
+	 * @param [props]
 	 */
-	protected play(): void {
-		if (this.isFulfilled && this.resolver) {
-			this.resolver();
-			return;
-		}
-
-		const
-			{async: $a, mode} = this;
-
-		if (mode === 'sequence') {
-			// const
-			// 	{next} = this;
-
-			// if (!next) {
-			// 	return;
-			// }
-
-			// next.promise = $a.promise(new Promise(next.resolver)).catch(stderr);
-			// next.promise.then(this.play);
-
-		} else {
-			// const
-			// 	{stack} = this;
-
-			// for (let i = 0; i < stack.length; i++) {
-			// 	const
-			// 		transition = stack[i];
-
-			// 	if (transition.promise) {
-			// 		continue;
-			// 	}
-
-			// 	transition.promise = $a.promise(new Promise(transition.resolver)).catch(stderr);
-			// }
-		}
+	protected visible(el: Target, props?: StyleDictionary): Transition {
+		return this;
 	}
 
 	/**
-	 * Appends transition into stack
-	 * @param info
+	 * Returns a DOM nodes by selector
+	 * @param node
 	 */
-	protected append(info: TransitionParams, resolver: (resolve: Function, reject: Function) => unknown): TransitionInfo {
-		if (!this.stack.length) {
-			this.state = TRANSITION_STATES.run;
-		}
-
-		const transition: TransitionInfo = {
-			...info,
-			resolver,
-			state: TRANSITION_STATES.initial,
-			initialProps: Object.select(info.node.style, info.props)
-		};
-
-		this.stack.push(transition);
-		this.play();
-
-		return transition;
+	protected getNodes(selector: string): HTMLElement[] {
+		return [].slice.call(this.block.elements(selector));
 	}
 
 	/**
 	 * Returns a DOM node by selector
-	 * @param node
+	 * @param selector
 	 */
-	protected getNode(node: HTMLElement | string): CanUndef<HTMLElement> {
-		return node instanceof HTMLElement ? node : this.block.element(node);
+	protected getNode(selector: string): CanUndef<HTMLElement> {
+		return this.block.element(selector);
 	}
 
 	/**
-	 * Generates a will-change and transform properties
+	 * Returns a last step from transition stack
+	 */
+	protected getLast(): CanUndef<TransitionInfo> {
+		return this.stack[this.stack.length - 1];
+	}
+
+	/**
+	 * Generates a will-change property
 	 *
 	 * @param props
 	 * @param [params]
 	 */
-	protected generateGPU(props: StyleDictionary, params: GPUParams = {}): StyleDictionary {
+	protected generateGPU(props: StyleDictionary): StyleDictionary {
 		const
 			animateProps = Object.reject(props, NON_ANIMATED_PROPERTIES),
 			keys = Object.keys(animateProps),
 			styles: StyleDictionary = {};
 
-		if (params.overrideTransform) {
-			styles.transform = 'transform3d(0, 0, 0)';
-		}
-
 		styles.willChange = keys.reduce((acc, prop) => `${acc}${acc === '' ? '' : ','}${prop}`, '');
 		return styles;
-	}
-
-	/**
-	 * Handler: transition is finished
-	 */
-	protected onDone(): void {
-		// ..
 	}
 }
 
