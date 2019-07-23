b7daf59726c5678de9b9f7b5708de101ccd44647
kobezzza <kobezzza@gmail.com>
2019-07-23 18:35:27 +0300

    Removed raven-js

--- config/production.js
+++ config/production.js
@@ -12,9 +12,5 @@ const
 	config = include('config/default');
 
 module.exports = config.createConfig({dirs: [__dirname], mod: '@super/config/production'}, {
-	__proto__: config,
-
-	externals: {
-		'raven-js': 'Raven'
-	}
+	__proto__: config
 });
