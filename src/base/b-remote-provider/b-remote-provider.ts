a3da819d11204edd43258d36a9eaec8840343a0b
kobezzza <kobezzza@gmail.com>
2019-09-07 14:39:16 +0300

    Added content getter

--- src/base/b-remote-provider/b-remote-provider.ts
+++ src/base/b-remote-provider/b-remote-provider.ts
@@ -7,7 +7,7 @@
  */
 
 import symbolGenerator from 'core/symbol';
-import iData, { component, prop, system, RequestError } from 'super/i-data/i-data';
+import iData, { component, prop, RequestError } from 'super/i-data/i-data';
 export * from 'super/i-data/i-data';
 
 export const
@@ -26,6 +26,13 @@ export default class bRemoteProvider<T extends object = Dictionary> extends iDat
 	 */
 	@prop({type: String, required: false})
 	readonly fieldProp?: string;
+
+	/**
+	 * Link to the component content nodes
+	 */
+	get content(): CanPromise<HTMLCollection> {
+		return this.waitStatus('loading', () => this.$el.children);
+	}
 
 	/** @override */
 	set db(value: CanUndef<T>) {
