2a89b29146eb3304cbdd92baafd41d12a3d31f19
bonkalol <bonkalol@list.ru>
2019-09-17 16:29:01 +0300

    Fix types

--- src/core/abt/index.ts
+++ src/core/abt/index.ts
@@ -8,6 +8,8 @@
 
 import adapter from 'core/abt/engines';
 import state from 'core/component/state';
+
+import { ExperimentsSet } from 'core/abt/interface';
 import { EventEmitter2 as EventEmitter } from 'eventemitter2';
 
 export const
@@ -19,10 +21,10 @@ export const
  */
 export default async function saveABT(opts: unknown): Promise<void> {
 	let
-		config = adapter(opts);
+		config = <CanPromise<ExperimentsSet | void>>adapter(opts);
 
 	if (Object.isPromise(config)) {
-		config = await config.catch(stderr) || [];
+		config = await config.catch(stderr);
 	}
 
 	if (Object.isArray(config)) {
