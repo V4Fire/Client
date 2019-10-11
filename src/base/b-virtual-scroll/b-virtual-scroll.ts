d7993ef2bcc3e2aa2bc7ec661d4cb2da62d8d3a6
bonkalol <bonkalol@list.ru>
2019-10-11 06:31:10 +0300

    Update cache height

--- src/base/b-virtual-scroll/b-virtual-scroll.ts
+++ src/base/b-virtual-scroll/b-virtual-scroll.ts
@@ -110,7 +110,7 @@ export default class bVirtualScroll extends iData<RemoteData> {
 	 * Number of cached VNodes
 	 */
 	@prop({type: Number, validator: isNatural})
-	readonly cacheSize: number = 200;
+	readonly cacheSize: number = 400;
 
 	/**
 	 * Number of components will be destroyed on cache drop
