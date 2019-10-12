ef53418adfae55e01f51e86ecde43671b949c3d7
kobezzza <kobezzza@gmail.com>
2019-10-12 12:11:16 +0300

    :art:

--- src/form/b-icon-button/b-icon-button.ts
+++ src/form/b-icon-button/b-icon-button.ts
@@ -6,20 +6,8 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import bButton, { component, prop } from 'form/b-button/b-button';
-
+import bButton, { component } from 'form/b-button/b-button';
 export * from 'form/b-button/b-button';
 
-@component({
-	flyweight: true,
-	functional: {
-		dataProvider: undefined,
-		href: undefined
-	}
-})
-
-export default class bIconButton<T extends object = Dictionary> extends bButton<T> {
-	/** @override */
-	@prop({type: String})
-	readonly icon?: string;
-}
+@component({flyweight: true})
+export default class bIconButton<T extends object = Dictionary> extends bButton<T> {}
