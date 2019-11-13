b17bc0c7d82139d38deaa96175ce11514979e705
dmitrybrovka <dmitrybrovka@yandex.ru>
2019-11-13 17:45:45 +0300

    Move render filter from template to .ts

--- src/base/b-matryoshka/b-matryoshka.ts
+++ src/base/b-matryoshka/b-matryoshka.ts
@@ -98,14 +98,18 @@ export default class bMatryoshka extends iBlock {
 
 	/**
 	 * Recursively render filter
-	 * @param id
+	 * @param el
 	 */
-	protected listFilter(id: string): boolean {
-		if (this.top.hook !== 'mounted') {
-			return false;
-		}
+	protected renderFilter(el: Doll): boolean {
+		const filter = (id) => {
+			if (this.top.hook !== 'mounted') {
+				return false;
+			}
 
-		return this.getFoldedMod(id) === 'false';
+			return this.getFoldedMod(id) === 'false';
+		};
+
+		return el.parentId ? filter(el.parentId) : true;
 	}
 
 	/**
