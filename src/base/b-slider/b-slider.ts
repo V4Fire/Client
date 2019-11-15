6927347172aab1b827c6a4923fb5775cb9a26d69
kobezzza <kobezzza@gmail.com>
2019-11-15 15:10:21 +0300

    Fixed OptionsProps type

--- src/base/b-slider/b-slider.ts
+++ src/base/b-slider/b-slider.ts
@@ -20,7 +20,7 @@ export interface SlideRect extends ClientRect {
  * 1  - Next
  */
 export type SlideDirection = -1 | 0 | 1;
-export type OptionProps = ((el: unknown, i: number) => Dictionary) | Dictionary;
+export type OptionProps = ((el: unknown, i: number, key?: string) => Dictionary) | Dictionary;
 export type OptionsIterator<T = bSlider> = (options: unknown[], ctx: T) => unknown[];
 
 export const
