60e768fa0f11df0c5c0be341631575850665edad
kobezzza <kobezzza@gmail.com>
2020-07-17 12:56:26 +0300

    Added README.md; Fixed ESlint warnings

---
+++ src/pages/p-v4-components-demo/b-v4-component-demo/README.md
@@ -0,0 +1,3 @@
+# pages/p-v4-components-demo/b-v4-component-demo
+
+This module provides a component to inspect another components.
--- src/pages/p-v4-components-demo/p-v4-components-demo.ts
+++ src/pages/p-v4-components-demo/p-v4-components-demo.ts
@@ -6,12 +6,22 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-console.time('Initializing');
+/**
+ * [[include:pages/p-v4-components-demo/README.md]]
+ * @packageDocumentation
+ */
 
 import iStaticPage, { component, system } from 'super/i-static-page/i-static-page';
 import { testRoutes } from 'pages/p-v4-components-demo/const';
+
 export * from 'super/i-static-page/i-static-page';
 
+console.time('Initializing');
+
+/**
+ * Page with component demo-s.
+ * Basically it uses with component tests.
+ */
 @component({root: true})
 export default class pV4ComponentsDemo extends iStaticPage {
 	/**
