c0bdfa848d4dab3016446e241d3f03957112fa96
kobezzza <kobezzza@gmail.com>
2020-07-22 09:29:06 +0300

    Added the const file

---
+++ src/super/i-static-page/modules/const.js
@@ -0,0 +1,19 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+/**
+ * Cache of files
+ * @type {!Object}
+ */
+exports.files = Object.create(null);
+
+/**
+ * Cache of directories
+ * @type {null}
+ */
+exports.folders = Object.create(null);
