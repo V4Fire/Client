fee99dd6aa41713ce1bce1879761c09087384e79
bonkalol <bonkalol@list.ru>
2020-07-30 00:53:12 +0300

    :art:

--- src/base/b-slider/index.js
+++ src/base/b-slider/index.js
@@ -1,2 +1,10 @@
-package('b-dummy')
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+package('b-slider')
 	.extends('i-data');
