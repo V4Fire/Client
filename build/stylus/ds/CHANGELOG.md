95d17b1b25cc00da1b76e165568cb9ad27158a13
dmitrybrovka <dmitrybrovka@yandex.ru>
2020-08-09 18:13:56 +0300

    :house: Stylus DS plugins group module was added

---
+++ build/stylus/ds/CHANGELOG.md
@@ -0,0 +1,16 @@
+Changelog
+=========
+
+> **Tags:**
+> - :boom:       [Breaking Change]
+> - :rocket:     [New Feature]
+> - :bug:        [Bug Fix]
+> - :memo:       [Documentation]
+> - :house:      [Internal]
+> - :nail_care:  [Polish]
+
+_Note: Gaps between patch versions are faulty, broken or test releases._
+
+## (2020-08-09) :house:
+
+* Extract DS plugins group to the folder
---
+++ build/stylus/ds/helpers.js
@@ -0,0 +1,126 @@
+'use strict';
+
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+/**
+ * Sets a variable into cssVars dictionary by the specified path
+ *
+ * @param {string} path
+ * @param {unknown} dsValue
+ * @param {string} [theme]
+ */
+function setVar(path, dsValue, theme) {
+	const
+		variable = `--${path.split('.').join('-')}`;
+
+	const
+		mapValue = [variable, dsValue];
+
+	$C(cssVars).set(`var(${variable})`, path);
+
+	if (!theme) {
+		cssVars.__map__[path] = mapValue;
+
+	} else {
+		if (!cssVars.__map__[theme]) {
+			Object.defineProperty(cssVars.__map__, theme, {value: {}, enumerable: false});
+		}
+
+		cssVars.__map__[theme][path] = mapValue;
+	}
+}
+
+/**
+ * Returns path with a dot delimiter between prefix and suffix
+ *
+ * @param {string} prefix
+ * @param {string} suffix
+ * @returns {string}
+ */
+function genPath(prefix, suffix) {
+	return `${prefix ? `${prefix}.${suffix}` : suffix}`;
+}
+
+/**
+ * Converts object prop values to Stylus values
+ *
+ * @param {Object} data
+ * @param {string=} [path]
+ * @param {string|boolean=} [theme]
+ */
+function prepareData(data, path, theme) {
+	$C(data).forEach((d, val) => {
+		if (theme === true) {
+			if (Object.isObject(d)) {
+				if (theme === true) {
+					prepareData(d, path, val);
+				}
+
+			} else {
+				throw new Error('Cannot find theme dictionary');
+			}
+
+		} else if (val === 'theme') {
+			if (Object.isObject(d)) {
+				prepareData(d, path, true);
+
+			} else {
+				throw new Error('Cannot find themes dictionary');
+			}
+
+		} else if (Object.isObject(d)) {
+			prepareData(d, genPath(path, val), theme);
+
+		} else if (Object.isArray(d)) {
+			prepareData(d, genPath(path, val), theme);
+			d = stylus.utils.coerceArray(d, true);
+
+		} else {
+			if (/^[a-z-_]+\(.*\)$/.test(d)) {
+				// Built-in function
+				data[val] = new stylus.Parser(d).function();
+				setVar(genPath(path, val), data[val], theme);
+				return;
+			}
+
+			if (/^#(?=[0-9a-fA-F]*$)(?:.{3,4}|.{6}|.{8})$/.test(d)) {
+				// HEX value
+				data[val] = new stylus.Parser(d).peek().val;
+				setVar(genPath(path, val), data[val], theme);
+				return;
+			}
+
+			if (Object.isString(d)) {
+				const
+					reg = /(\d+(?:\.\d+)?)(?=(px|em|rem|%)$)/,
+					unit = d.match(reg);
+
+				if (unit) {
+					// Value with unit
+					data[val] = new stylus.nodes.Unit(parseFloat(unit[1]), unit[2]);
+					setVar(genPath(path, val), data[val], theme);
+					return;
+				}
+
+				data[val] = new stylus.nodes.String(d);
+				setVar(genPath(path, val), data[val], theme);
+				return;
+			}
+
+			data[val] = new stylus.nodes.Unit(d);
+			setVar(genPath(path, val), data[val], theme);
+		}
+	});
+}
+
+module.exports = {
+	setVar,
+	prepareData,
+	genPath
+};
