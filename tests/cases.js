5d7e9756c24dc27a58b3636d16d32e2ce1d78dfa
bonkalol <bonkalol@list.ru>
2020-09-01 12:11:36 +0300

    Disable comments

--- tests/cases.js
+++ tests/cases.js
@@ -10,25 +10,25 @@
 
 module.exports = [
 	// b-router
-	// '--test-entry base/b-router/test',
+	'--test-entry base/b-router/test',
 
 	// b-virtual-scroll
-	// '--test-entry base/b-virtual-scroll/test --runner slots/empty',
+	'--test-entry base/b-virtual-scroll/test --runner slots/empty',
 	'--test-entry base/b-virtual-scroll/test --runner slots/render-next',
 
-	// '--test-entry base/b-virtual-scroll/test --runner render/render',
+	'--test-entry base/b-virtual-scroll/test --runner render/render',
 
-	// '--test-entry base/b-virtual-scroll/test --runner events/data-change',
-	// '--test-entry base/b-virtual-scroll/test --runner events/chunk-loaded',
-	// '--test-entry base/b-virtual-scroll/test --runner events/chunk-loading',
-	// '--test-entry base/b-virtual-scroll/test --runner events/db-change',
+	'--test-entry base/b-virtual-scroll/test --runner events/data-change',
+	'--test-entry base/b-virtual-scroll/test --runner events/chunk-loaded',
+	'--test-entry base/b-virtual-scroll/test --runner events/chunk-loading',
+	'--test-entry base/b-virtual-scroll/test --runner events/db-change',
 
-	// '--test-entry base/b-virtual-scroll/test --runner functional/state',
-	// '--test-entry base/b-virtual-scroll/test --runner functional/render-next',
+	'--test-entry base/b-virtual-scroll/test --runner functional/state',
+	'--test-entry base/b-virtual-scroll/test --runner functional/render-next',
 
-	// // b-button
-	// '--test-entry form/b-button/test',
+	// b-button
+	'--test-entry form/b-button/test',
 
-	// // in-view
-	// '--test-entry core/component/directives/in-view/test'
+	// in-view
+	'--test-entry core/component/directives/in-view/test'
 ];
