63c6beaf32bed9caecd2b82e39ed7ab5c4df63b7
kobezzza <kobezzza@gmail.com>
2020-10-09 21:09:28 +0300

    Marked as non-functional: `stageStore` `componentStatusStore` `watchModsStore`

--- index.d.ts
+++ index.d.ts
@@ -110,3 +110,74 @@ interface Document {
 		ready: Promise<void>;
 	};
 }
+
+interface RenderParams {
+	/**
+	 * Component attrs
+	 */
+	attrs: Dictionary;
+
+	/** @see [[RenderContent]] */
+	content?: Dictionary<RenderContent | string>;
+}
+
+/**
+ * Content to render into an element
+ *
+ * @example
+ *
+ * ```typescript
+ * globalThis.renderComponents('b-button', {
+ *   attrs: {
+ *      testProp: 1
+ *   },
+ *
+ *   content: {
+ *     default: {
+ *       tag: 'b-button',
+ *       content: {
+ *         default: 'Test'
+ *       }
+ *     }
+ *   }
+ * });
+ * ```
+ *
+ * This schema is the equivalent of such a template:
+ *
+ * ```ss
+ * < b-button :testProp = 1
+ *   < b-button
+ *     Test
+ * ```
+ */
+interface RenderContent {
+	/**
+	 * Component name or tagName
+	 */
+	tag: string;
+
+	/**
+	 * Component attrs
+	 */
+	attrs: Dictionary;
+
+	/** @see [[RenderContent]] */
+	content?: Dictionary<RenderContent | string>;
+}
+
+// eslint-disable-next-line no-var,vars-on-top
+declare var
+	/**
+	 * Renders specified components
+	 *
+	 * @param componentName
+	 * @param scheme
+	 * @param options
+	 */
+	renderComponents: (componentName: string, scheme: RenderParams[], options?: RenderOptions) => void,
+
+	/**
+	 * Removes all components created via `globalThis.renderComponents`
+	 */
+	removeCreatedComponents: () => void;
