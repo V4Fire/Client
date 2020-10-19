b6f90a5942b33136e86842c1be45107f007f941e
dmitrybrovka <dmitrybrovka@yandex.ru>
2020-10-19 08:25:00 +0300

    :art: file headers

--- build/stylus/ds/spec.js
+++ build/stylus/ds/spec.js
@@ -1,11 +1,11 @@
 'use strict';
 
 /*!
- * V4Fire Core
- * https://github.com/V4Fire/Core
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
  *
  * Released under the MIT license
- * https://github.com/V4Fire/Core/blob/master/LICENSE
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
 const
