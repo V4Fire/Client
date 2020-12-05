7af25ea08b833624c1b3c97f632586e9d4a14d38
dmitrybrovka <dmitrybrovka@yandex.ru>
2020-12-05 12:39:16 +0300

    :memo: link to packageDocumentation

--- src/base/b-matryoshka/b-matryoshka.ts
+++ src/base/b-matryoshka/b-matryoshka.ts
@@ -6,6 +6,11 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
+/**
+ * [[include:base/b-matryoshka/README.md]]
+ * @packageDocumentation
+ */
+
 import symbolGenerator from 'core/symbol';
 
 import iItems from 'traits/i-items/i-items';
