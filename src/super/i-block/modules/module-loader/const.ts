cca98c1e710e3dec0791a5599113005bf1aa07eb
kobezzza <kobezzza@gmail.com>
2020-12-18 12:57:37 +0300

    :wrench:

--- src/super/i-block/modules/module-loader/const.ts
+++ src/super/i-block/modules/module-loader/const.ts
@@ -11,12 +11,3 @@ import { Module } from 'super/i-block/modules/module-loader/interface';
 export const
 	cache = new Map<unknown, Module>(),
 	cachedModules = <Module[]>[];
-
-const set = cache.set.bind(cache);
-cache.set = (key, value) => {
-	if (cache.has(key)) {
-		throw new Error(`A module with the "${String(key)}" id is already set`);
-	}
-
-	return set(key, value);
-};
