b4207cae2e3da88e588add49d2ff22166197f2e6
kobezzza <kobezzza@gmail.com>
2021-01-04 16:11:07 +0300

    base/b-dummy -> dummies/b-dummy

--- src/dummies/b-dummy/b-dummy.ts
+++ src/dummies/b-dummy/b-dummy.ts
@@ -12,14 +12,14 @@ import { ResizeWatcher } from 'core/dom/resize-observer';
 
 import iLockPageScroll from 'traits/i-lock-page-scroll/i-lock-page-scroll';
 import iData, { component } from 'super/i-data/i-data';
-import { Directives, Modules } from 'base/b-dummy/interface';
+import { Directives, Modules } from 'dummies/b-dummy/interface';
 
 const
 	inViewMutation = inViewFactory('mutation'),
 	inViewObserver = inViewFactory('observer');
 
 export * from 'super/i-data/i-data';
-export * from 'base/b-dummy/interface';
+export * from 'dummies/b-dummy/interface';
 
 @component({
 	functional: {
