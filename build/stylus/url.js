bf43018deb53483e1f5359b2a1e86078e3d1326b
dmitrybrovka <dmitrybrovka@yandex.ru>
2021-02-07 12:49:16 +0300

    Formatting plugins improvements

--- build/stylus/url.js
+++ build/stylus/url.js
@@ -9,18 +9,64 @@
  */
 
 const
-	url = require('url'),
+	querystring = require('querystring'),
 	{parseObject} = include('build/stylus/object');
+
+/**
+ * Parameters to create an url:
+ *
+ * @typedef {{
+ *   href?: string,
+ *   query?: Object,
+ *   hash?: string
+ * }} URLParams
+ */
 
 module.exports = function addPlugins(api) {
 	/**
 	 * Converts the specified object to an url string
 	 *
-	 * @param {object} obj
 	 * @see https://nodejs.org/api/url.html#url_class_url
+	 *
+	 * @param {URLParams} params
+	 * @returns {string}
 	 */
-	api.define('formatURL', (obj) => {
-		const urlObject = parseObject(obj);
-		return url.format(urlObject);
+	api.define('formatURL', (params) => {
+		const
+			parsedOpts = parseObject(params);
+
+		let
+			href = '',
+			search = '',
+			hash = '';
+
+		if (Object.isString(parsedOpts.href)) {
+			href = parsedOpts.href;
+		}
+
+		if (Object.isObject(parsedOpts.query)) {
+			search = `?${querystring.stringify(parsedOpts.query)}`;
+		}
+
+		if (Object.isString(parsedOpts.hash)) {
+			hash = `#${parsedOpts.hash}`;
+		}
+
+		return [href, hash, search].join('');
+	});
+
+	/**
+	 * Converts the specified object to a query string
+	 *
+	 * @see https://nodejs.org/api/querystring.html#querystring_querystring_stringify_obj_sep_eq_options
+	 * @param {Object} obj
+	 *
+	 * @returns {string}
+	 */
+	api.define('toQueryString', (obj) => {
+		const
+			parsedOpts = parseObject(obj);
+
+		return querystring.stringify(parsedOpts);
 	});
 };
