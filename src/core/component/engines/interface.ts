5d19ee71c76f5d74cf37638301d629246f22da78
kobezzza <kobezzza@gmail.com>
2021-02-11 08:17:45 +0300

    Added a new property `fakeContext` to `VNode`

---
+++ src/core/component/engines/interface.ts
@@ -0,0 +1,14 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import { VNode as RawVNode } from 'vue';
+import { ComponentInterface } from 'core/component/interface';
+
+export interface VNode extends RawVNode {
+	fakeContext?: ComponentInterface;
+}
