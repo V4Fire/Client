76cc411800478d63b986aab78f1c36d16b100d2e
kobezzza <kobezzza@gmail.com>
2021-02-20 15:44:09 +0300

    Added a context for render functions

---
+++ src/core/component/engines/zero/context/const.ts
@@ -0,0 +1,15 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+export const reservedAttrs = {
+	is: true,
+	key: true,
+	ref: true,
+	slot: true,
+	'slot-scope': true
+};
