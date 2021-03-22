57557a7b8998492c7ce1dd6bfae30a7ee70fbfb9
kobezzza <kobezzza@gmail.com>
2021-03-22 16:52:18 +0300

    Fixed TS errors

--- src/base/b-dummy/interface.ts
+++ src/base/b-dummy/interface.ts
@@ -9,9 +9,9 @@
 import type { ImageLoader, imageLoaderFactory } from 'core/dom/image';
 import type { InViewAdapter } from 'core/dom/in-view';
 import type { ResizeWatcher } from 'core/dom/resize-observer';
-import type updateOn from 'core/component/directives/update-on/engines';
 
-import iObserveDOM from 'traits/i-observe-dom/i-observe-dom';
+import type updateOn from 'core/component/directives/update-on/engines';
+import type iObserveDOM from 'traits/i-observe-dom/i-observe-dom';
 
 export interface Directives {
 	imageFactory: typeof imageLoaderFactory;
