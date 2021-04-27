b9ae75826d598b536caf075b9ad2afc82369cdce
dmitrybrovka <dmitrybrovka@yandex.ru>
2021-04-27 10:32:52 +0300

    :art:

--- src/core/router/interface.ts
+++ src/core/router/interface.ts
@@ -310,7 +310,7 @@ export interface RouteBlueprint<META extends object = Dictionary> {
 	/**
 	 * Pattern of the route path
 	 */
-	pattern?: string | ((params: Route) => CanUndef<string>);
+	pattern?: string | ((route: RouteAPI) => CanUndef<string>);
 
 	/**
 	 * RegExp to parse the route path
