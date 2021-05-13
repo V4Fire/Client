c544065417cd46ddcfb95bcfbd6dac33ff5ae121
gretzkiy <dias7707@gmail.com>
2021-05-13 12:23:48 +0300

    Fix bug with `pointer-events: none` in Safari 14.1
    Using `pointer-events: none` causes rendering bug in Safari (tested on
    14.1) both on desktop and iOS. If you try to change the color of the
    element with this css property, it won't work.
    Demo: https://codepen.io/gretzkiy/pen/qBrbLXQ

--- src/traits/i-progress/i-progress.ts
+++ src/traits/i-progress/i-progress.ts
@@ -37,14 +37,34 @@ export default abstract class iProgress {
 	 */
 	static initModEvents<T extends iBlock>(component: T): void {
 		const
-			{localEmitter: $e} = component.unsafe;
+			{localEmitter: $e, async: $a} = component.unsafe;
 
 		$e.on('block.mod.*.progress.*', (e: ModEvent) => {
-			if (e.type === 'remove' && e.reason !== 'removeMod') {
-				return;
-			}
+			const
+				asyncGroup = 'progressHelpers';
 
-			component.emit(e.value === 'false' || e.type === 'remove' ? 'progressEnd' : 'progressStart');
+			if (e.value === 'false' || e.type === 'remove') {
+				$a.off({group: asyncGroup});
+
+				if (e.type !== 'remove' || e.reason === 'removeMod') {
+					component.emit('progressEnd');
+				}
+
+			} else if (component.$el != null) {
+				component.emit('progressStart');
+
+				const handler = (e) => {
+					e.preventDefault();
+					e.stopImmediatePropagation();
+				};
+
+				$a.on(component.$el, 'click mousedown touchstart keydown input change scroll', handler, {
+					group: asyncGroup,
+					options: {
+						capture: true
+					}
+				});
+			}
 		});
 	}
 }
