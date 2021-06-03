8b56a0ba669b31ca064284fdee7453ed835519b0
kobezzza <kobezzza@gmail.com>
2021-06-03 12:11:15 +0300

    Added validators

---
+++ src/super/i-input-text/modules/validators/interface.ts
@@ -0,0 +1,38 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import type { ValidatorParams, ValidatorError } from 'super/i-input/i-input';
+
+/**
+ * Parameters to validate inputted strings
+ */
+export interface PatternValidatorParams extends ValidatorParams {
+	/***
+	 * RegExp or RegExp text to validate a string
+	 */
+	pattern?: RegExp | string;
+
+	/**
+	 * The minimum length of a string
+	 */
+	min?: number;
+
+	/**
+	 * The maximum length of a string
+	 */
+	max?: number;
+
+	/**
+	 * If true, the specified `min` and `max` values are ignored
+	 */
+	skipLength?: boolean;
+}
+
+export interface PatternValidatorResult extends ValidatorError<string | number> {
+	name: 'NOT_MATCH' | 'MIN' | 'MAX';
+}
