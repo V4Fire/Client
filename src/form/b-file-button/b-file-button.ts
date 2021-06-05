31bd7aa52855c205856e6d4ada94ef0493f3ede3
kobezzza <kobezzza@gmail.com>
2021-06-05 14:28:07 +0300

    :art:

--- src/form/b-file-button/b-file-button.ts
+++ src/form/b-file-button/b-file-button.ts
@@ -7,12 +7,14 @@
  */
 
 import bButton, { component, prop, wait } from 'form/b-button/b-button';
+
 export * from 'form/b-button/b-button';
 
 @component({flyweight: true})
 export default class bFileButton extends bButton {
 	/**
-	 * Accept string
+	 * Which file types are selectable in a file upload control
+	 * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#htmlattrdefaccept
 	 */
 	@prop({type: String, required: false})
 	readonly accept?: string;
@@ -41,8 +43,9 @@ export default class bFileButton extends bButton {
 	}
 
 	/** @override */
-	protected async onClick(e: Event): Promise<void> {
+	protected onClick(e: Event): Promise<void> {
 		this.$refs.file.click();
 		this.emit('click', e);
+		return Promise.resolve();
 	}
 }
