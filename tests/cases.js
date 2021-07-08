aae46062b0f21b8d883746e8e46336177ce70cf0
bonkalol <bonkalol@list.ru>
2021-07-08 20:07:59 +0300

    Added core/cookies into test run

--- tests/cases.js
+++ tests/cases.js
@@ -48,6 +48,7 @@ module.exports = [
 	'--test-entry traits/i-observe-dom/test',
 
 	'--test-entry core/html/test',
+	'--test-entry core/cookies/test',
 	'--test-entry core/dom/image/test',
 	'--test-entry core/dom/in-view/test',
 
