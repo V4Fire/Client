7de405b4240d94ea9126e11a551fa9af80b10184
Bogdan Blinnikov <bonkalol@list.ru>
2021-07-08 18:35:01 +0300

    Issues/576 (#580)
    * issues/576
    
    * Added core/html test into test-run

---
+++ src/core/html/test/index.js
@@ -0,0 +1,39 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+// @ts-check
+
+const
+	h = include('tests/helpers');
+
+/**
+ * Starts a test
+ *
+ * @param {Playwright.Page} page
+ * @returns {void}
+ */
+module.exports = (page) => {
+	let dummyComponent;
+
+	describe('`core/html`', () => {
+		beforeEach(async () => {
+			dummyComponent = await h.component.waitForComponent(page, '.b-dummy');
+		});
+
+		describe('`getSrcSet`', () => {
+			it('returns a `srcset` string', async () => {
+				const result = (await dummyComponent.evaluate(({modules: {htmlHelpers}}) => htmlHelpers.getSrcSet({
+					'2x': 'http://img-hdpi.png',
+					'3x': 'http://img-xhdpi.png'
+				}))).trim();
+
+				expect(result).toBe('http://img-hdpi.png 2x, http://img-xhdpi.png 3x');
+			});
+		});
+	});
+};
