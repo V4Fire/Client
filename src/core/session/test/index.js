25559d5181038c25b550279e649c0cafbceaf7bb
kobezzza <kobezzza@gmail.com>
2021-07-09 18:17:40 +0300

    :art:

--- src/core/session/test/index.js
+++ src/core/session/test/index.js
@@ -49,7 +49,7 @@ module.exports = (page, {browser, contextOpts}) => {
 				expect(testVal).toEqual({auth: 'authToken', params: {someParam: 1}});
 			});
 
-			it('emits `set` event', async () => {
+			it('emits a `set` event', async () => {
 				const
 					eventPr = session.evaluate(({emitter}) => new Promise((res) => emitter.on('set', res)));
 
@@ -60,7 +60,7 @@ module.exports = (page, {browser, contextOpts}) => {
 		});
 
 		describe('get', () => {
-			it('returns session data if session was initialized', async () => {
+			it('returns session data if the session was initialized', async () => {
 				await session.evaluate((ctx) => ctx.set('authToken', {someParam: 1}));
 
 				const
@@ -69,7 +69,7 @@ module.exports = (page, {browser, contextOpts}) => {
 				expect(testVal).toEqual({auth: 'authToken', params: {someParam: 1}});
 			});
 
-			it('returns undefined if session was not initialized', async () => {
+			it('returns `undefined` if the session was not initialized', async () => {
 				const
 					testVal = await session.evaluate((ctx) => ctx.get());
 
@@ -78,7 +78,7 @@ module.exports = (page, {browser, contextOpts}) => {
 		});
 
 		describe('clear', () => {
-			it('clears a stored session', async () => {
+			it('clears the stored session', async () => {
 				await session.evaluate((ctx) => ctx.set('authToken', {someParam: 1}));
 				await session.evaluate((ctx) => ctx.clear());
 
@@ -88,7 +88,7 @@ module.exports = (page, {browser, contextOpts}) => {
 				expect(testVal).toEqual({auth: undefined, params: undefined});
 			});
 
-			it('emits `clear` event', async () => {
+			it('emits a `clear` event', async () => {
 				const
 					eventPr = session.evaluate(({emitter}) => new Promise((res) => emitter.on('clear', res)));
 
@@ -118,7 +118,7 @@ module.exports = (page, {browser, contextOpts}) => {
 		});
 
 		describe('isExists', () => {
-			it('returns `true` is session does exists', async () => {
+			it('returns `true` is the session exists', async () => {
 				await session.evaluate((ctx) => ctx.set('authToken', {someParam: 1}));
 
 				const
@@ -127,7 +127,7 @@ module.exports = (page, {browser, contextOpts}) => {
 				expect(testVal).toBeTrue();
 			});
 
-			it('returns `false` is session does not exists', async () => {
+			it('returns `false` is the session does not exist', async () => {
 				const
 					testVal = await session.evaluate((ctx) => ctx.isExists());
 
