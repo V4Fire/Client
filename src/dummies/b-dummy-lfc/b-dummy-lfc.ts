75de5ae75e6b05b177c80ae43029f3418eef3a3f
kobezzza <kobezzza@gmail.com>
2021-07-12 15:05:12 +0300

    Added lfc tests

--- src/dummies/b-dummy-lfc/b-dummy-lfc.ts
+++ src/dummies/b-dummy-lfc/b-dummy-lfc.ts
@@ -11,7 +11,7 @@
  * @packageDocumentation
  */
 
-import iData, { component, hook } from 'super/i-data/i-data';
+import iData, { component, hook, field } from 'super/i-data/i-data';
 
 export * from 'super/i-data/i-data';
 
@@ -23,14 +23,55 @@ export * from 'super/i-data/i-data';
 })
 
 export default class bDummyLfc extends iData {
-	@hook('beforeDataCreate')
-	beforeDataCreate(): void {
-		(<Promise<any>>this.lfc.execCbAtTheRightTime(() => {
-			this.tmp.beforeDataCreate1 = this.hook;
+	/**
+	 * Field for test purposes
+	 */
+	@field()
+	foo: number = 1;
+
+	/**
+	 * Field for test purposes
+	 */
+	@field()
+	bar: number = 2;
+
+	beforeCreate(): void {
+		this.tmp.beforeCreateHook = this.hook;
+		this.tmp.beforeCreateIsBefore = this.lfc.isBeforeCreate();
+
+		void (<Promise<any>>this.lfc.execCbAtTheRightTime(() => {
+			this.tmp.rightTimeHookFromBeforeCreate = this.hook;
 			return this.hook;
 
 		})).then((res) => {
-			this.tmp.beforeDataCreate2 = res;
+			this.tmp.rightTimeHookFromBeforeCreate2 = res;
+		});
+	}
+
+	@hook('beforeDataCreate')
+	beforeDataCreate(): void {
+		this.tmp.fooBar = this.field.get<number>('foo')! + this.field.get<number>('bar')!;
+		this.tmp.beforeDataCreateHook = this.hook;
+		this.tmp.beforeDataCreateIsBefore = this.lfc.isBeforeCreate();
+		this.tmp.beforeDataCreateIsBeforeWithSkipping = this.lfc.isBeforeCreate('beforeDataCreate');
+
+		this.tmp.rightTimeHook2 = this.lfc.execCbAtTheRightTime(() => {
+			this.tmp.rightTimeHook = this.hook;
+			this.tmp.rightTimeHookIsBefore = this.lfc.isBeforeCreate();
+			return this.hook;
+		});
+
+		void this.lfc.execCbAfterBlockReady(() => {
+			this.tmp.blockReady = this.block != null;
+			this.tmp.blockReadyIsBefore = this.lfc.isBeforeCreate();
+		});
+
+		void (<Promise<any>>this.lfc.execCbAfterComponentCreated(() => {
+			this.tmp.componentCreatedHook = this.hook;
+			return this.hook;
+
+		})).then((res) => {
+			this.tmp.componentCreatedHook2 = res;
 		});
 	}
 }
--- src/super/i-block/modules/lfc/test/index.js
+++ src/super/i-block/modules/lfc/test/index.js
@@ -38,158 +38,70 @@ module.exports = async (page, params) => {
 				}
 			];
 
-			globalThis.renderComponents('b-dummy', scheme);
+			globalThis.renderComponents('b-dummy-lfc', scheme);
 		});
 
 		target = await h.component.waitForComponent(page, '#target');
 	});
 
-	beforeEach(async () => {
-		await page.evaluate(() => {
-			globalThis.removeCreatedComponents();
-		});
-	});
-
 	describe('`iBlock.lfc`', () => {
-		describe('`fullComponentName`', () => {
-			it('simple usage', async () => {
-				expect(await target.evaluate(({provide}) => provide.fullComponentName()))
-					.toBe('b-dummy');
-			});
+		it('checking the `beforeCreate` state', async () => {
+			expect(
+				await target.evaluate((ctx) => ({
+					beforeCreateHook: ctx.tmp.beforeCreateHook,
+					beforeCreateIsBefore: ctx.tmp.beforeCreateIsBefore
+				}))
 
-			it('providing a modifier', async () => {
-				expect(await target.evaluate(({provide}) => provide.fullComponentName('opened', true)))
-					.toBe('b-dummy_opened_true');
-			});
-
-			it('providing a component name', async () => {
-				expect(await target.evaluate(({provide}) => provide.fullComponentName('b-foo')))
-					.toBe('b-foo');
-			});
-
-			it('providing a component name and modifiers', async () => {
-				expect(await target.evaluate(({provide}) => provide.fullComponentName('b-foo', 'opened', true)))
-					.toBe('b-foo_opened_true');
+			).toEqual({
+				beforeCreateHook: 'beforeCreate',
+				beforeCreateIsBefore: true
 			});
 		});
 
-		describe('`fullElName`', () => {
-			it('simple usage', async () => {
-				expect(await target.evaluate(({provide}) => provide.fullElName('foo')))
-					.toBe('b-dummy__foo');
-			});
+		it('checking the `beforeDataCreate` state', async () => {
+			expect(
+				await target.evaluate((ctx) => ({
+					fooBar: ctx.tmp.fooBar,
+					rightTimeHookFromBeforeCreate: ctx.tmp.rightTimeHookFromBeforeCreate,
+					rightTimeHookFromBeforeCreate2: ctx.tmp.rightTimeHookFromBeforeCreate2,
+					beforeDataCreateHook: ctx.tmp.beforeDataCreateHook,
+					beforeDataCreateIsBefore: ctx.tmp.beforeDataCreateIsBefore,
+					beforeDataCreateIsBeforeWithSkipping: ctx.tmp.beforeDataCreateIsBeforeWithSkipping
+				}))
 
-			it('providing a modifier', async () => {
-				expect(await target.evaluate(({provide}) => provide.fullElName('foo', 'opened', true)))
-					.toBe('b-dummy__foo_opened_true');
-			});
-
-			it('providing a component name', async () => {
-				expect(await target.evaluate(({provide}) => provide.fullElName('b-foo', 'foo')))
-					.toBe('b-foo__foo');
-			});
-
-			it('providing a component name and modifiers', async () => {
-				expect(await target.evaluate(({provide}) => provide.fullElName('b-foo', 'foo', 'opened', true)))
-					.toBe('b-foo__foo_opened_true');
-			});
-		});
-
-		describe('`mods`', () => {
-			it('simple usage', async () => {
-				expect(await target.evaluate(({provide}) => provide.mods()))
-					.toEqual({foo: 'bar'});
-			});
-
-			it('providing additional modifiers', async () => {
-				expect(await target.evaluate(({provide}) => provide.mods({baz: 'bla'})))
-					.toEqual({foo: 'bar', baz: 'bla'});
+			).toEqual({
+				fooBar: 3,
+				rightTimeHookFromBeforeCreate: 'beforeDataCreate',
+				rightTimeHookFromBeforeCreate2: 'beforeDataCreate',
+				beforeDataCreateHook: 'beforeDataCreate',
+				beforeDataCreateIsBefore: true,
+				beforeDataCreateIsBeforeWithSkipping: false
 			});
 		});
 
-		describe('`classes`', () => {
-			it('simple usage', async () => {
-				expect(await target.evaluate(({provide}) => provide.classes({button: true})))
-					.toEqual({button: 'b-dummy__button'});
+		it('`execCbAfterBlockReady`', async () => {
+			expect(
+				await target.evaluate((ctx) => ({
+					blockReady: ctx.tmp.blockReady,
+					blockReadyIsBefore: ctx.tmp.blockReadyIsBefore
+				}))
 
-				expect(await target.evaluate(({provide}) => provide.classes({button: 'submit'})))
-					.toEqual({button: 'b-dummy__submit'});
-			});
-
-			it('providing additional modifiers', async () => {
-				expect(await target.evaluate(({provide}) => provide.classes({button: ['submit', 'focused', 'true']})))
-					.toEqual({button: 'b-dummy__submit_focused_true'});
-			});
-
-			it('providing a component name', async () => {
-				expect(await target.evaluate(({provide}) => provide.classes('b-foo', {button: true})))
-					.toEqual({button: 'b-foo__button'});
-
-				expect(await target.evaluate(({provide}) => provide.classes('b-foo', {button: 'submit'})))
-					.toEqual({button: 'b-foo__submit'});
-			});
-
-			it('providing a component name and additional modifiers', async () => {
-				expect(await target.evaluate(({provide}) => provide.classes('b-foo', {button: ['submit', 'focused', 'true']})))
-					.toEqual({button: 'b-foo__submit_focused_true'});
+			).toEqual({
+				blockReady: true,
+				blockReadyIsBefore: false
 			});
 		});
 
-		describe('`componentClasses`', () => {
-			it('simple usage', async () => {
-				expect(await target.evaluate(({provide}) => provide.componentClasses()))
-					.toEqual(['b-dummy']);
-			});
+		it('`execCbAfterComponentCreated`', async () => {
+			expect(
+				await target.evaluate((ctx) => ({
+					componentCreatedHook: ctx.tmp.componentCreatedHook,
+					componentCreatedHook2: ctx.tmp.componentCreatedHook2
+				}))
 
-			it('providing additional modifiers', async () => {
-				expect(await target.evaluate(({provide}) => provide.componentClasses({opened: true, selected: false})))
-					.toEqual(['b-dummy', 'b-dummy_opened_true', 'b-dummy_selected_false']);
-			});
-
-			it('providing a component name', async () => {
-				expect(await target.evaluate(({provide}) => provide.componentClasses('b-foo')))
-					.toEqual(['b-foo']);
-			});
-
-			it('providing a component name and additional modifiers', async () => {
-				expect(await target.evaluate(({provide}) => provide.componentClasses('b-foo', {opened: true, selected: false})))
-					.toEqual(['b-foo', 'b-foo_opened_true', 'b-foo_selected_false']);
-			});
-		});
-
-		describe('`elClasses`', () => {
-			it('simple usage', async () => {
-				const id = await target.evaluate(({componentId}) => componentId);
-
-				expect(await target.evaluate(({provide}) => provide.elClasses({button: {focused: true}})))
-					.toEqual([id, 'b-dummy__button', 'b-dummy__button_focused_true']);
-			});
-
-			it('providing a component name', async () => {
-				expect(await target.evaluate(({provide}) => provide.elClasses('b-foo', {button: {focused: true}})))
-					.toEqual(['b-foo__button', 'b-foo__button_focused_true']);
-			});
-
-			it('providing a component context', async () => {
-				const ctx = {
-					componentId: 'baz',
-					componentName: 'b-foo'
-				};
-
-				expect(await target.evaluate(({provide}, ctx) => provide.elClasses(ctx, {button: {focused: true}}), ctx))
-					.toEqual(['baz', 'b-foo__button', 'b-foo__button_focused_true']);
-			});
-		});
-
-		describe('`hintClasses`', () => {
-			it('simple usage', async () => {
-				expect(await target.evaluate(({provide}) => provide.hintClasses()))
-					.toEqual(['g-hint', 'g-hint_pos_bottom']);
-			});
-
-			it('providing position', async () => {
-				expect(await target.evaluate(({provide}) => provide.hintClasses('top')))
-					.toEqual(['g-hint', 'g-hint_pos_top']);
+			).toEqual({
+				componentCreatedHook: 'created',
+				componentCreatedHook2: 'created'
 			});
 		});
 	});
