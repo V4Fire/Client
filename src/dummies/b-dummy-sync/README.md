a8ee4fb1ebe78c0b25a2ce560468becdd0fff4dc
kobezzza <kobezzza@gmail.com>
2021-07-13 09:18:46 +0300

    Added b-dummy-sync

---
+++ src/dummies/b-dummy-sync/README.md
@@ -0,0 +1,3 @@
+# dummies/b-dummy-async-render
+
+Dummy component to test async rendering of components.
---
+++ src/dummies/b-dummy-sync/b-dummy-sync.ts
@@ -0,0 +1,63 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+/**
+ * [[include:dummies/b-dummy-async-render/README.md]]
+ * @packageDocumentation
+ */
+
+import iData, { component, field, prop, system } from 'super/i-data/i-data';
+
+export * from 'super/i-data/i-data';
+
+@component({
+	functional: {
+		functional: true,
+		dataProvider: undefined
+	}
+})
+
+export default class bDummySync extends iData {
+	/**
+	 * Prop for test purposes
+	 */
+	@prop(Object)
+	readonly dictProp: Dictionary = {
+		a: {
+			b: 2,
+			c: 3
+		}
+	};
+
+	/**
+	 * Field for test purposes
+	 */
+	@field((o) => o.sync.link())
+	dict!: Dictionary;
+
+	/**
+	 * Field for test purposes
+	 */
+	@system((o) => o.sync.link('dict.a.b', {immediate: true, collapse: false}, (val: number) => val + 1))
+	linkToNestedFieldWithInitializer!: number;
+
+	/**
+	 * Field for test purposes
+	 */
+	@field({
+		after: 'dict',
+		init: (o) => o.sync.object({immediate: true, collapse: false}, [
+			'dict',
+			['linkToNestedFieldWithInitializer', (val) => Number(val) * 2],
+			['linkToPath', 'dict.a.b'],
+			['linkToPathWithInitializer', 'dict.a.c', (val) => Number(val) * 2]
+		])
+	})
+
+	watchableObject!: Dictionary;
+}
