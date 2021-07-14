36b99524eaa469f318c512d70d4d1b2726220472
kobezzza <kobezzza@gmail.com>
2021-07-14 14:33:57 +0300

    Added new tests

--- src/super/i-block/test/runners/watchers/fields.js
+++ src/super/i-block/test/runners/watchers/fields.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-lines */
+
 /*!
  * V4Fire Client Core
  * https://github.com/V4Fire/Client
@@ -445,7 +447,7 @@ module.exports = (page) => {
 						}, field);
 
 						expect(scan).toEqual([
-							[{a: {c: 2}}, {a: {b: 1}}, false, [field]],
+							[{a: {c: 2}}, {a: {b: {c: 1, d: 2}}}, false, [field]],
 							[{a: {c: 3}}, {a: {c: 2}}, false, [field]]
 						]);
 					});
@@ -594,204 +596,186 @@ module.exports = (page) => {
 						}, field);
 
 						expect(scan).toEqual([
-							[{a: {c: 2}}, {a: {b: 1}}, false, [field]],
-							[{a: {c: 3}}, {a: {c: 2}}, false, [field]],
-							[{a: {c: 5}}, {a: {c: 4}}, false, [field, 'a', 'c']]
+							[
+								{a: {c: 2}},
+								{a: {b: {c: 1, d: 2}}},
+								false,
+								[field]
+							],
+
+							[
+								{a: {c: 3}},
+								{a: {c: 2}},
+								false,
+								[field]
+							],
+
+							[
+								{a: {c: 5}},
+								{a: {c: 3}},
+								false,
+								[field, 'a', 'c']
+							]
 						]);
 					});
 
-					// it('deep watching without collapsing', async () => {
-					// 	const
-					// 		target = await init();
-					//
-					// 	const scan = await target.evaluate(async (ctx, field) => {
-					// 		const res = [];
-					//
-					// 		ctx.watch(field, {deep: true, collapse: false}, (mutations) => {
-					// 			mutations.forEach(([val, oldVal, i]) => {
-					// 				res.push([
-					// 					Object.fastClone(val),
-					// 					Object.fastClone(oldVal),
-					// 					i.path,
-					// 					i.originalPath
-					// 				]);
-					// 			});
-					// 		});
-					//
-					// 		ctx[field] = {a: {b: 1}};
-					// 		await ctx.nextTick();
-					//
-					// 		ctx[field].a.b++;
-					// 		ctx[field].a.b++;
-					// 		await ctx.nextTick();
-					//
-					// 		ctx[field].a = {d: 1};
-					// 		await ctx.nextTick();
-					//
-					// 		return res;
-					// 	}, field);
-					//
-					// 	expect(scan).toEqual([
-					// 		[
-					// 			{a: {b: 1}},
-					// 			{a: {b: {c: 1, d: 2}}},
-					// 			[field],
-					// 			[field]
-					// 		],
-					//
-					// 		[2, 1, [field], [field, 'a', 'b']],
-					// 		[3, 2, [field], [field, 'a', 'b']],
-					//
-					// 		[
-					// 			{d: 1},
-					// 			{b: 3},
-					// 			[field],
-					// 			[field, 'a']
-					// 		]
-					// 	]);
-					// });
-					//
-					// it('deep immediate watching without collapsing', async () => {
-					// 	const
-					// 		target = await init();
-					//
-					// 	const scan = await target.evaluate((ctx, field) => {
-					// 		const res = [];
-					//
-					// 		ctx.watch(field, {deep: true, immediate: true, collapse: false}, (val, oldVal, i) => {
-					// 			res.push([
-					// 				Object.fastClone(val),
-					// 				Object.fastClone(oldVal),
-					// 				i?.path,
-					// 				i?.originalPath
-					// 			]);
-					// 		});
-					//
-					// 		ctx[field] = {a: {b: 1}};
-					//
-					// 		ctx[field].a.b++;
-					// 		ctx[field].a.b++;
-					//
-					// 		ctx[field].a = {d: 1};
-					//
-					// 		return res;
-					// 	}, field);
-					//
-					// 	expect(scan).toEqual([
-					// 		[
-					// 			{a: {b: {c: 1, d: 2}}},
-					// 			undefined,
-					// 			undefined,
-					// 			undefined
-					// 		],
-					//
-					// 		[
-					// 			{a: {b: 1}},
-					// 			{a: {b: {c: 1, d: 2}}},
-					// 			[field],
-					// 			[field]
-					// 		],
-					//
-					// 		[2, 1, [field], [field, 'a', 'b']],
-					// 		[3, 2, [field], [field, 'a', 'b']],
-					//
-					// 		[
-					// 			{d: 1},
-					// 			{b: 3},
-					// 			[field],
-					// 			[field, 'a']
-					// 		]
-					// 	]);
-					// });
-					//
-					// it('removing watchers', async () => {
-					// 	const
-					// 		target = await init();
-					//
-					// 	const scan = await target.evaluate((ctx, field) => {
-					// 		const res = [];
-					//
-					// 		ctx.watch(field, {deep: true, immediate: true, collapse: false, group: 'foo'}, (val, oldVal, i) => {
-					// 			res.push([
-					// 				Object.fastClone(val),
-					// 				Object.fastClone(oldVal),
-					// 				i?.path,
-					// 				i?.originalPath
-					// 			]);
-					// 		});
-					//
-					// 		ctx[field].a.b.c++;
-					// 		ctx.async.terminateWorker({group: 'foo'});
-					// 		ctx[field].a.b.c++;
-					//
-					// 		return res;
-					// 	}, field);
-					//
-					// 	expect(scan).toEqual([
-					// 		[
-					// 			{a: {b: {c: 1, d: 2}}},
-					// 			undefined,
-					// 			undefined,
-					// 			undefined
-					// 		],
-					//
-					// 		[2, 1, [field], [field, 'a', 'b', 'c']]
-					// 	]);
-					// });
-					//
-					// it('suspending watchers', async () => {
-					// 	const
-					// 		target = await init();
-					//
-					// 	const scan = await target.evaluate((ctx, field) => {
-					// 		const res = [];
-					// 		globalThis._res = res;
-					//
-					// 		ctx.watch(field, {deep: true, immediate: true, collapse: false, group: 'foo'}, (val, oldVal, i) => {
-					// 			res.push([
-					// 				Object.fastClone(val),
-					// 				Object.fastClone(oldVal),
-					// 				i?.path,
-					// 				i?.originalPath
-					// 			]);
-					// 		});
-					//
-					// 		ctx[field].a.b.c++;
-					// 		ctx.async.suspendEventListener({group: /foo/});
-					// 		ctx[field].a.b.c++;
-					//
-					// 		return res;
-					// 	}, field);
-					//
-					// 	expect(scan).toEqual([
-					// 		[
-					// 			{a: {b: {c: 1, d: 2}}},
-					// 			undefined,
-					// 			undefined,
-					// 			undefined
-					// 		],
-					//
-					// 		[2, 1, [field], [field, 'a', 'b', 'c']]
-					// 	]);
-					//
-					// 	const scan2 = await target.evaluate((ctx) => {
-					// 		const res = globalThis._res;
-					// 		ctx.async.unsuspendEventListener({group: /foo/});
-					// 		return res;
-					// 	});
-					//
-					// 	expect(scan2).toEqual([
-					// 		[
-					// 			{a: {b: {c: 1, d: 2}}},
-					// 			undefined,
-					// 			undefined,
-					// 			undefined
-					// 		],
-					//
-					// 		[2, 1, [field], [field, 'a', 'b', 'c']],
-					// 		[3, 2, [field], [field, 'a', 'b', 'c']]
-					// 	]);
-					// });
+					it('deep watching without collapsing', async () => {
+						const
+							target = await init();
+
+						const scan = await target.evaluate(async (ctx, field) => {
+							const res = [];
+
+							ctx.watch(field, {deep: true, collapse: false}, (mutations) => {
+								mutations.forEach(([val, oldVal, i]) => {
+									res.push([
+										Object.fastClone(val),
+										Object.fastClone(oldVal),
+										val === oldVal,
+										i.originalPath
+									]);
+								});
+							});
+
+							ctx[field] = {a: {b: 1}};
+							await ctx.nextTick();
+
+							ctx[field].a.b++;
+							ctx[field].a.b++;
+							await ctx.nextTick();
+
+							ctx[field].a = {d: 1};
+							await ctx.nextTick();
+
+							return res;
+						}, field);
+
+						expect(scan).toEqual([
+							[
+								{a: {b: 1}},
+								{a: {b: {c: 1, d: 2}}},
+								false,
+								[field]
+							],
+
+							[2, 1, false, [field, 'a', 'b']],
+							[3, 2, false, [field, 'a', 'b']],
+
+							[
+								{d: 1},
+								{b: 3},
+								false,
+								[field, 'a']
+							]
+						]);
+					});
+
+					it('deep immediate watching without collapsing', async () => {
+						const
+							target = await init();
+
+						const scan = await target.evaluate((ctx, field) => {
+							const res = [];
+
+							ctx.watch(field, {deep: true, immediate: true, collapse: false}, (val, oldVal, i) => {
+								res.push([
+									Object.fastClone(val),
+									Object.fastClone(oldVal),
+									val === oldVal,
+									i?.originalPath
+								]);
+							});
+
+							ctx[field] = {a: {b: 1}};
+
+							ctx[field].a.b++;
+							ctx[field].a.b++;
+
+							ctx[field].a = {d: 1};
+
+							return res;
+						}, field);
+
+						expect(scan).toEqual([
+							[
+								{a: {b: {c: 1, d: 2}}},
+								undefined,
+								false,
+								undefined
+							],
+
+							[
+								{a: {b: 1}},
+								{a: {b: {c: 1, d: 2}}},
+								false,
+								[field]
+							],
+
+							[2, 1, false, [field, 'a', 'b']],
+							[3, 2, false, [field, 'a', 'b']],
+
+							[
+								{d: 1},
+								{b: 3},
+								false,
+								[field, 'a']
+							]
+						]);
+					});
+
+					it('suspending watchers', async () => {
+						const
+							target = await init();
+
+						const scan = await target.evaluate((ctx, field) => {
+							const res = [];
+							globalThis._res = res;
+
+							ctx.watch(field, {deep: true, immediate: true, collapse: false, group: 'foo'}, (val, oldVal, i) => {
+								res.push([
+									Object.fastClone(val),
+									Object.fastClone(oldVal),
+									val === oldVal,
+									i?.originalPath
+								]);
+							});
+
+							ctx[field].a.b.c++;
+							ctx.async.suspendEventListener({group: /foo/});
+							ctx[field].a.b.c++;
+
+							return res;
+						}, field);
+
+						expect(scan).toEqual([
+							[
+								{a: {b: {c: 1, d: 2}}},
+								undefined,
+								false,
+								undefined
+							],
+
+							[2, 1, false, [field, 'a', 'b', 'c']]
+						]);
+
+						const scan2 = await target.evaluate((ctx) => {
+							const res = globalThis._res;
+							ctx.async.unsuspendEventListener({group: /foo/});
+							return res;
+						});
+
+						expect(scan2).toEqual([
+							[
+								{a: {b: {c: 1, d: 2}}},
+								undefined,
+								false,
+								undefined
+							],
+
+							[2, 1, false, [field, 'a', 'b', 'c']],
+							[3, 2, false, [field, 'a', 'b', 'c']]
+						]);
+					});
 				});
 			});
 		});
