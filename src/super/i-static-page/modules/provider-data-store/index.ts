fba3cc295bf084378aec2d8d0706a12d953e5b1f
kobezzza <kobezzza@gmail.com>
2021-07-22 14:59:32 +0300

    Refactoring

--- src/super/i-static-page/modules/provider-data-store/index.ts
+++ src/super/i-static-page/modules/provider-data-store/index.ts
@@ -11,5 +11,19 @@
  * @packageDocumentation
  */
 
-export { default } from 'super/i-static-page/modules/provider-data-store/store';
-export { default as ProviderDataItem } from 'super/i-static-page/modules/provider-data-store/item';
+import Cache from 'core/cache/simple';
+import ProviderDataItem from 'super/i-static-page/modules/provider-data-store/item';
+
+export { ProviderDataItem };
+
+/**
+ * Class to store data of data providers
+ */
+export default class ProviderDataStore extends Cache<ProviderDataItem> {
+	/** @override */
+	set<T>(key: string, value: T): ProviderDataItem<T> {
+		const item = new ProviderDataItem(key, value);
+		this.storage.set(key, item);
+		return item;
+	}
+}
