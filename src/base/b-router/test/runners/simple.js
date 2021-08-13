617c02b0dc5aa9e34146837f52828fd986cd8a65
gretzkiy <dias7707@gmail.com>
2021-08-13 17:15:16 +0300

    Add tests for in-memory engine

--- src/base/b-router/test/runners/simple.js
+++ src/base/b-router/test/runners/simple.js
@@ -20,195 +20,265 @@ module.exports = (page) => {
 	let
 		root;
 
-	describe('b-router simple using', () => {
-		beforeEach(async () => {
-			root = await initRouter(page);
-		});
-
-		it('checking the root', async () => {
-			expect(await root.evaluate((ctx) => ctx.meta.params.root)).toBe(true);
-		});
-
-		it('checking the `route` property', async () => {
-			expect(await root.evaluate(({route}) => route != null)).toBeTrue();
-		});
-
-		it('checking the `router` property', async () => {
-			expect(await root.evaluate(({router}) => router.componentName)).toBe('b-router');
-		});
-
-		it('`push` a page by a route identifier', async () => {
-			expect(await root.evaluate(async (ctx) => {
-				await ctx.router.push('second');
-				return ctx.route.meta.content;
-			})).toBe('Second page');
-		});
-
-		it('`push` a page by a path', async () => {
-			expect(await root.evaluate(async (ctx) => {
-				await ctx.router.push('/');
-				return ctx.route.meta.content;
-			})).toBe('Main page');
-		});
-
-		it('`replace` a page by a path', async () => {
-			expect(await root.evaluate(async (ctx) => {
-				const
-					historyLength = history.length,
-					res = {};
-
-				await ctx.router.replace('second');
-
-				res.content = ctx.route.meta.content;
-				res.lengthDoesntChange = historyLength === history.length;
-
-				return res;
-
-			})).toEqual({
-				content: 'Second page',
-				lengthDoesntChange: true
+	['historyApiRouterEngine', 'inMemoryRouterEngine'].forEach((engine) => {
+		describe(`b-router with engine ${engine} simple using`, () => {
+			beforeEach(async () => {
+				root = await initRouter(page, engine);
 			});
-		});
 
-		it('`replace` a page by null', async () => {
-			expect(await root.evaluate(async (ctx) => {
-				const
-					{router} = ctx;
+			if (engine === 'inMemoryRouterEngine') {
+				generateInMemoryEngineSpecs();
 
-				await router.replace('/');
+			} else {
+				generateHistoryApiEngineSpecs();
+			}
 
-				const
-					historyLength = history.length,
-					res = {};
-
-				await router.replace('second');
-				await router.replace(null, {query: {bla: 1}});
-
-				res.content = ctx.route.meta.content;
-				res.query = location.search;
-				res.lengthDoesntChange = historyLength === history.length;
-
-				return res;
-
-			})).toEqual({
-				query: '?bla=1',
-				content: 'Second page',
-				lengthDoesntChange: true
+			it('checking the root', async () => {
+				expect(await root.evaluate((ctx) => ctx.meta.params.root)).toBe(true);
 			});
-		});
 
-		it('checking the `activePage` property', async () => {
-			expect(await root.evaluate(async (ctx) => {
-				await ctx.router.push('second');
-				return ctx.activePage;
-			})).toBe('second');
+			it('checking the `router` property', async () => {
+				expect(await root.evaluate(({router}) => router.componentName)).toBe('b-router');
+			});
 
-			expect(await root.evaluate(async (ctx) => {
-				await ctx.router.push('main');
-				return ctx.activePage;
-			})).toBe('main');
-		});
+			it('`push` a page by a route identifier', async () => {
+				expect(await root.evaluate(async (ctx) => {
+					await ctx.router.push('second');
+					return ctx.route.meta.content;
+				})).toBe('Second page');
+			});
 
-		it('updating of routes', async () => {
-			expect(await root.evaluate(async (ctx) => {
+			it('`push` a page by a path', async () => {
+				expect(await root.evaluate(async (ctx) => {
+					await ctx.router.push('/');
+					return ctx.route.meta.content;
+				})).toBe('Main page');
+			});
+
+			it('checking the `activePage` property', async () => {
+				expect(await root.evaluate(async (ctx) => {
+					await ctx.router.push('second');
+					return ctx.activePage;
+				})).toBe('second');
+
+				expect(await root.evaluate(async (ctx) => {
+					await ctx.router.push('main');
+					return ctx.activePage;
+				})).toBe('main');
+			});
+
+			it('updating of routes', async () => {
+				expect(await root.evaluate(async (ctx) => {
+					const
+						{router} = ctx;
+
+					const
+						res = {},
+						oldRoutes = ctx.router.routes;
+
+					await router.updateRoutes({
+						main: {
+							path: '/',
+							default: true,
+							content: 'Dynamic main page'
+						}
+					});
+
+					res.dynamicPage = ctx.route.meta.content;
+
+					router.routes = oldRoutes;
+					router.routeStore = undefined;
+
+					await router.initRoute('main');
+					res.restoredPage = ctx.route.meta.content;
+
+					return res;
+
+				})).toEqual({
+					dynamicPage: 'Dynamic main page',
+					restoredPage: 'Main page'
+				});
+			});
+
+			it('updating of a base path', async () => {
+				expect(await root.evaluate(async (ctx) => {
+					const
+						{router} = ctx;
+
+					const
+						res = {},
+						oldRoutes = ctx.router.routes;
+
+					await router.updateRoutes('/demo', '/demo/second', {
+						main: {
+							path: '/',
+							default: true,
+							content: 'Dynamic main page'
+						},
+
+						second: {
+							path: '/second',
+							default: true,
+							content: 'Dynamic second page'
+						}
+					});
+
+					res.dynamicPage = ctx.route.meta.content;
+
+					router.basePath = '/';
+					router.routes = oldRoutes;
+					router.routeStore = undefined;
+
+					await router.initRoute('/');
+					res.restoredPage = ctx.route.meta.content;
+
+					return res;
+
+				})).toEqual({
+					dynamicPage: 'Dynamic second page',
+					restoredPage: 'Main page'
+				});
+			});
+
+			it('getting URL by a query', async () => {
+				expect(await root.evaluate(({router}) => router.getRoutePath('second', {query: {bla: 1}})))
+					.toBe('/second?bla=1');
+
+				expect(await root.evaluate(({router}) => router.getRoutePath('/', {query: {bla: 1}})))
+					.toBe('/?bla=1');
+			});
+
+			it('getting route parameters by a query', async () => {
+				const pageMeta = {
+					name: 'main',
+					page: 'main',
+					path: '/',
+					default: false,
+					external: false,
+					content: 'Main page'
+				};
+
+				expect(await root.evaluate(({router}) => {
+					const route = router.getRoute('main');
+					return route.meta;
+				})).toEqual(pageMeta);
+
+				expect(await root.evaluate(({router}) => {
+					const route = router.getRoute('/');
+					return route.meta;
+				})).toEqual(pageMeta);
+			});
+
+			function generateInMemoryEngineSpecs() {
 				const
-					{router} = ctx;
+					engine = 'inMemoryRouterEngine';
 
-				const
-					res = {},
-					oldRoutes = ctx.router.routes;
-
-				await router.updateRoutes({
-					main: {
-						path: '/',
-						default: true,
-						content: 'Dynamic main page'
-					}
+				it('checking the `route` property with initialRoute', async () => {
+					expect(await root.evaluate(({route}) => route != null)).toBeTrue();
 				});
 
-				res.dynamicPage = ctx.route.meta.content;
-
-				router.routes = oldRoutes;
-				router.routeStore = undefined;
-
-				await router.initRoute('main');
-				res.restoredPage = ctx.route.meta.content;
-
-				return res;
-
-			})).toEqual({
-				dynamicPage: 'Dynamic main page',
-				restoredPage: 'Main page'
-			});
-		});
-
-		it('updating of a base path', async () => {
-			expect(await root.evaluate(async (ctx) => {
-				const
-					{router} = ctx;
-
-				const
-					res = {},
-					oldRoutes = ctx.router.routes;
-
-				await router.updateRoutes('/demo', '/demo/second', {
-					main: {
-						path: '/',
-						default: true,
-						content: 'Dynamic main page'
-					},
-
-					second: {
-						path: '/second',
-						default: true,
-						content: 'Dynamic second page'
-					}
+				it('checking the `route` property without initialRoute', async () => {
+					root = await initRouter(page, engine, null);
+					expect(await root.evaluate(({route}) => route == null)).toBeTrue();
 				});
 
-				res.dynamicPage = ctx.route.meta.content;
+				it('`replace` a page by a path', async () => {
+					expect(await root.evaluate(async (ctx) => {
+						const
+							{router} = ctx,
+							historyLength = router.engine.history.length,
+							res = {};
 
-				router.basePath = '/';
-				router.routes = oldRoutes;
-				router.routeStore = undefined;
+						await router.replace('second');
 
-				await router.initRoute('/');
-				res.restoredPage = ctx.route.meta.content;
+						res.content = ctx.route.meta.content;
+						res.lengthDoesntChange = historyLength === router.engine.history.length;
 
-				return res;
+						return res;
 
-			})).toEqual({
-				dynamicPage: 'Dynamic second page',
-				restoredPage: 'Main page'
-			});
-		});
+					})).toEqual({
+						content: 'Second page',
+						lengthDoesntChange: true
+					});
+				});
 
-		it('getting URL by a query', async () => {
-			expect(await root.evaluate(({router}) => router.getRoutePath('second', {query: {bla: 1}})))
-				.toBe('/second?bla=1');
+				it('`replace` a page by null', async () => {
+					expect(await root.evaluate(async (ctx) => {
+						const
+							{router} = ctx;
 
-			expect(await root.evaluate(({router}) => router.getRoutePath('/', {query: {bla: 1}})))
-				.toBe('/?bla=1');
-		});
+						await router.replace('/');
 
-		it('getting route parameters by a query', async () => {
-			const pageMeta = {
-				name: 'main',
-				page: 'main',
-				path: '/',
-				default: false,
-				external: false,
-				content: 'Main page'
-			};
+						const
+							historyLength = router.engine.history.length,
+							res = {};
 
-			expect(await root.evaluate(({router}) => {
-				const route = router.getRoute('main');
-				return route.meta;
-			})).toEqual(pageMeta);
+						await router.replace('second');
+						await router.replace(null, {query: {bla: 1}});
 
-			expect(await root.evaluate(({router}) => {
-				const route = router.getRoute('/');
-				return route.meta;
-			})).toEqual(pageMeta);
+						res.content = ctx.route.meta.content;
+						res.lengthDoesntChange = historyLength === router.engine.history.length;
+
+						return res;
+
+					})).toEqual({
+						content: 'Second page',
+						lengthDoesntChange: true
+					});
+				});
+			}
+
+			function generateHistoryApiEngineSpecs() {
+				it('checking the `route` property', async () => {
+					expect(await root.evaluate(({route}) => route != null)).toBeTrue();
+				});
+
+				it('`replace` a page by a path', async () => {
+					expect(await root.evaluate(async (ctx) => {
+						const
+							historyLength = history.length,
+							res = {};
+
+						await ctx.router.replace('second');
+
+						res.content = ctx.route.meta.content;
+						res.lengthDoesntChange = historyLength === history.length;
+
+						return res;
+
+					})).toEqual({
+						content: 'Second page',
+						lengthDoesntChange: true
+					});
+				});
+
+				it('`replace` a page by null', async () => {
+					expect(await root.evaluate(async (ctx) => {
+						const
+							{router} = ctx;
+
+						await router.replace('/');
+
+						const
+							historyLength = history.length,
+							res = {};
+
+						await router.replace('second');
+						await router.replace(null, {query: {bla: 1}});
+
+						res.content = ctx.route.meta.content;
+						res.query = location.search;
+						res.lengthDoesntChange = historyLength === history.length;
+
+						return res;
+
+					})).toEqual({
+						query: '?bla=1',
+						content: 'Second page',
+						lengthDoesntChange: true
+					});
+				});
+			}
 		});
 	});
 };
