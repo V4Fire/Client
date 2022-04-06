a356eebaf7adc652df717c8ce7530e30310f3ec2
kobezzza <kobezzza@gmail.com>
2022-04-06 16:48:02 +0300

    chore: added the missing header

--- src/core/prelude/test-env/components/json-parser.ts
+++ src/core/prelude/test-env/components/json-parser.ts
@@ -1,3 +1,11 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
 export const
 	fnAlias = 'FN__',
 	regExpAlias = 'REGEX__';
