1006d4237737bed169d1036910656dc010b5cbee
kobezzza <kobezzza@gmail.com>
2022-04-18 12:34:52 +0300

    feat: expose new render api

---
+++ src/core/component/engines/vue3/render.ts
@@ -0,0 +1,39 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+export {
+
+	Fragment,
+	Transition,
+	TransitionGroup,
+
+	openBlock,
+	toDisplayString,
+
+	renderList,
+	renderSlot,
+
+	createBlock,
+	createElementBlock,
+
+	createVNode,
+	createStaticVNode,
+	createElementVNode,
+	createTextVNode,
+	createCommentVNode,
+
+	normalizeClass,
+	normalizeStyle,
+
+	resolveComponent,
+	resolveDirective,
+
+	withCtx,
+	withDirectives
+
+} from 'vue';
