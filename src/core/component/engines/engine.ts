517a57ef05df01d22ef80cd61e6cbd7c6a91f460
kobezzza <kobezzza@gmail.com>
2022-04-19 16:10:50 +0300

    refactor: temporary removed `zero`

--- src/core/component/engines/engine.ts
+++ src/core/component/engines/engine.ts
@@ -10,10 +10,5 @@
 export * from 'core/component/engines/vue3';
 //#endif
 
-//#if runtime.engine = zero
-// @ts-ignore (double export)
-export * from 'core/component/engines/zero';
-//#endif
-
 export * from 'core/component/engines/interface';
 export { VNode } from 'core/component/engines/interface';
