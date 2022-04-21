907a06dd33900741cf599db525709199c32b0a26
kobezzza <kobezzza@gmail.com>
2022-04-21 19:22:02 +0300

    feat: added render API

--- src/core/component/interface/engine.ts
+++ src/core/component/interface/engine.ts
@@ -6,16 +6,100 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import type { VNode } from 'core/component/engines';
-import type { ComponentInterface } from 'core/component/interface/component/component';
+import type {
+
+	Fragment,
+	Transition,
+	TransitionGroup,
+
+	toHandlers,
+	toDisplayString,
+
+	renderList,
+	renderSlot,
+
+	openBlock,
+	createBlock,
+	createElementBlock,
+
+	createVNode,
+	createStaticVNode,
+	createElementVNode,
+	createTextVNode,
+	createCommentVNode,
+
+	normalizeClass,
+	normalizeStyle,
+	mergeProps,
+
+	resolveComponent,
+	resolveDirective,
+
+	withCtx,
+	withKeys,
+	withModifiers,
+	withDirectives,
+
+	vShow,
+	vModelText,
+	vModelSelect,
+	vModelCheckbox,
+	vModelRadio,
+	vModelDynamic
+
+} from 'vue';
 
 export interface RenderEngineFeatures {
 	regular: boolean;
 	functional: boolean;
-	composite: boolean;
-
 	ssr: boolean;
-	boundCreateElement: boolean;
+}
+
+export interface RenderEngine<T extends object = object> {
+	supports: RenderEngineFeatures;
+	proxyGetters: ProxyGetters<T>;
+	r: RenderAPI;
+}
+
+export interface RenderAPI {
+	Fragment: typeof Fragment;
+	Transition: typeof Transition;
+	TransitionGroup: typeof TransitionGroup;
+
+	toHandlers: typeof toHandlers;
+	toDisplayString: typeof toDisplayString;
+
+	renderList: typeof renderList;
+	renderSlot: typeof renderSlot;
+
+	openBlock: typeof openBlock;
+	createBlock: typeof createBlock;
+	createElementBlock: typeof createElementBlock;
+
+	createVNode: typeof createVNode;
+	createStaticVNode: typeof createStaticVNode;
+	createElementVNode: typeof createElementVNode;
+	createTextVNode: typeof createTextVNode;
+	createCommentVNode: typeof createCommentVNode;
+
+	normalizeClass: typeof normalizeClass;
+	normalizeStyle: typeof normalizeStyle;
+	mergeProps: typeof mergeProps;
+
+	resolveComponent: typeof resolveComponent;
+	resolveDirective: typeof resolveDirective;
+
+	withCtx: typeof withCtx;
+	withKeys: typeof withKeys;
+	withModifiers: typeof withModifiers;
+	withDirectives: typeof withDirectives;
+
+	vShow: typeof vShow;
+	vModelText: typeof vModelText;
+	vModelSelect: typeof vModelSelect;
+	vModelCheckbox: typeof vModelCheckbox;
+	vModelRadio: typeof vModelRadio;
+	vModelDynamic: typeof vModelDynamic;
 }
 
 export type ProxyGetterType =
@@ -32,14 +116,3 @@ export type ProxyGetter<T extends object = object> = (ctx: T) => {
 };
 
 export type ProxyGetters<T extends object = object> = Record<ProxyGetterType, ProxyGetter<T>>;
-
-export interface RenderEngine<T extends object = object> {
-	supports: RenderEngineFeatures;
-	proxyGetters: ProxyGetters<T>;
-
-	cloneVNode(vnode: VNode): VNode;
-	patchVNode(vnode: VNode, component: ComponentInterface): VNode;
-
-	renderVNode(vnode: VNode, parent: ComponentInterface): Node;
-	renderVNode(vnodes: VNode[], parent: ComponentInterface): Node[];
-}
