f2f3a854bdc8077bda93f9ca5e539c05eeb9a517
kobezzza <kobezzza@gmail.com>
2022-04-22 19:31:31 +0300

    feat: added a new directive `tag`

---
+++ src/core/component/directives/tag/README.md
@@ -0,0 +1,9 @@
+# core/component/directives/tag
+
+This module brings a directive to specify dynamically a tag name to create.
+
+## Usage
+
+```
+< div v-tag = 'span'
+```
---
+++ src/core/component/directives/tag/index.ts
@@ -0,0 +1,23 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+/**
+ * [[include:core/component/directives/hook/README.md]]
+ * @packageDocumentation
+ */
+
+import { ComponentEngine, VNode } from 'core/component/engines';
+import type { DirectiveOptions } from 'core/component/directives/tag/interface';
+
+export * from 'core/component/directives/tag/interface';
+
+ComponentEngine.directive('tag', {
+	beforeCreate(opts: DirectiveOptions, vnode: VNode) {
+		vnode.type = opts.value;
+	}
+});
