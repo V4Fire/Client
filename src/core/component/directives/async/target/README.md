a4074c2fd5c74818a06c777cb6d1ad227e12f6c1
kobezzza <kobezzza@gmail.com>
2022-05-18 16:36:01 +0300

    feat: added a new directive `v-async-target`

---
+++ src/core/component/directives/async/target/README.md
@@ -0,0 +1,2 @@
+# core/component/directives/async/target
+
---
+++ src/core/component/directives/async/target/index.ts
@@ -0,0 +1,24 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+/**
+ * [[include:core/component/directives/async/target/README.md]]
+ * @packageDocumentation
+ */
+
+import { ComponentEngine, VNode, DirectiveBinding } from 'core/component/engines';
+import type { ComponentInterface } from 'core/component/interface';
+
+export * from 'core/component/directives/async/async/interface';
+
+ComponentEngine.directive('async-target', {
+	beforeCreate(opts: DirectiveBinding, vnode: VNode): void {
+		const ctx = Object.cast<ComponentInterface['unsafe']>(opts.instance);
+		ctx.$emit('[[V-ASYNC-TARGET]]', vnode);
+	}
+});
