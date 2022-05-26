9c629fd98f7d4eea0ba8c7e19485f06fd67d9fce
kholstinin <kholstinin@yandex-team.ru>
2022-05-26 16:42:36 +0500

    feat: added @statoscope/webpack-plugin

--- build/webpack/plugins.js
+++ build/webpack/plugins.js
@@ -15,6 +15,12 @@ const
 	config = require('@config/config'),
 	webpack = require('webpack');
 
+const statoscopeOptions = {
+	saveReportTo: './statoscope/report-[name].html',
+  saveStatsTo: 'stats-[name].json',
+  open: false
+};
+
 /**
  * Returns options for `webpack.plugins`
  * @returns {!Map}
@@ -26,12 +32,14 @@ module.exports = async function plugins({name}) {
 	const
 		DependenciesPlugin = include('build/webpack/plugins/dependencies'),
 		createProgressPlugin = include('build/webpack/plugins/progress-plugin'),
+		StatoscopeWebpackPlugin = require('@statoscope/webpack-plugin').default,
 		IgnoreInvalidWarningsPlugin = include('build/webpack/plugins/ignore-invalid-warnings');
 
 	const plugins = new Map([
 		['globals', new webpack.DefinePlugin(await $C(globals).async.map())],
 		['dependencies', new DependenciesPlugin()],
-		['ignoreNotFoundExport', new IgnoreInvalidWarningsPlugin()]
+		['ignoreNotFoundExport', new IgnoreInvalidWarningsPlugin()],
+		['statoscope', new StatoscopeWebpackPlugin(statoscopeOptions)]
 	]);
 
 	if (config.webpack.progress()) {
