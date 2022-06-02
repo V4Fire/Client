4b508ddf48cca88fec041557f8916a75f805823c
kobezzza <kobezzza@gmail.com>
2022-06-02 11:36:15 +0300

    refactor: removed optional imports

--- src/core/component/directives/index.ts
+++ src/core/component/directives/index.ts
@@ -6,22 +6,6 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-//#if runtime has directives/in-view
-import 'core/component/directives/in-view';
-//#endif
-
-//#if runtime has directives/resize-observer
-import 'core/component/directives/resize-observer';
-//#endif
-
-//#if runtime has directives/image
-import 'core/component/directives/image';
-//#endif
-
-//#if runtime has directives/update-on
-import 'core/component/directives/update-on';
-//#endif
-
 import 'core/component/directives/tag';
 import 'core/component/directives/hook';
 import 'core/component/directives/attrs';
