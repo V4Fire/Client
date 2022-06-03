56c6f555b27cb281d10c4095149cf9073329c848
kobezzza <kobezzza@gmail.com>
2022-06-03 14:51:50 +0300

    refactor: split interface into separated files

--- src/friends/async-render/interface/task.ts
+++ src/friends/async-render/interface/task.ts
@@ -109,26 +109,6 @@ export interface TaskEl<D = unknown> {
 }
 
 /**
- * Additional options to render an iterable structure
- */
-export interface IterOptions {
-	/**
-	 * A start index to iterate
-	 */
-	start?: number;
-
-	/**
-	 * How many fragments can be rendered at the same time
-	 */
-	perChunk?: number;
-
-	/**
-	 * A function to filter elements to render
-	 */
-	filter?: TaskFilter;
-}
-
-/**
  * A filter function for render tasks
  */
 export interface TaskFilter<E = unknown, D = unknown> {
@@ -151,39 +131,4 @@ export interface NodeDestructor {
 	 * @param childComponentEls - root elements of the child components
 	 */
 	(node: Node, childComponentEls: Element[]): AnyToBoolean;
-}
-
-/**
- * A descriptor of the iterable-based rendering structure
- */
-export interface IterDescriptor {
-	/**
-	 * Is this iterator asynchronous or not
-	 */
-	isAsync: boolean;
-
-	/**
-	 * An index of the last synchronously read element for the first render
-	 */
-	readI: number;
-
-	/**
-	 * Number of synchronously read elements for the first render
-	 */
-	readTotal: number;
-
-	/**
-	 * An array of the synchronously read elements for the first render
-	 */
-	readEls: unknown[];
-
-	/**
-	 * The original structure that we iterate
-	 */
-	iterable: CanPromise<AnyIterable>;
-
-	/**
-	 * An iterator for the structure that we iterate
-	 */
-	iterator: AnyIterableIterator;
 }
