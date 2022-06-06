29e8c31cb8eeea3365f180d010c140319ffbffb1
kobezzza <kobezzza@gmail.com>
2022-06-06 15:04:27 +0300

    feat: added a new directive `v-render`

---
+++ src/core/component/directives/render/README.md
@@ -0,0 +1,44 @@
+# core/component/directives/render
+
+This module provides a directive to create a composition of multiple functions that return VNode-s without using JSX.
+
+## Usage
+
+### Replacing a VNode with another one
+
+If you use the directive with a `template` tag without custom properties, a VNode that passed to `v-render` will replace the original VNode.
+If the passed value is undefined or null, the directive will do nothing.
+
+```
+< template v-render = myFragment
+  This content is used when the value passed to `v-render` is undefined or null.
+```
+
+### Adding child nodes
+
+If you use the directive with a regular tag, a VNode that passed to `v-render`
+will replace all children VNode-s of the original VNode. Also, in this case, you can provide a list of VNode-s to insert.
+If the passed value is undefined or null, the directive will do nothing.
+
+```
+< div v-render = myFragment
+  This content is used when the value passed to `v-render` is undefined or null.
+
+< div v-render = [myFragment1, myFragment2]
+  This content is used when the value passed to `v-render` is undefined or null.
+```
+
+### Adding component slots
+
+If you use the directive with a component, a VNode that passed to `v-render` will replace the default or named
+(if the name passed via the `slot` attribute) children slot of the original VNode.
+Also, in this case, you can provide a list of VNode-s or slots to insert.
+If the passed value is undefined or null, the directive will do nothing.
+
+```
+< b-button v-render = mySlot
+  This content is used when the value passed to `v-render` is undefined or null.
+
+< b-button v-render = [mySlot1, mySlot2]
+  This content is used when the value passed to `v-render` is undefined or null.
+```
---
+++ src/core/component/directives/render/index.ts
@@ -0,0 +1,86 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+/**
+ * [[include:core/component/directives/render/README.md]]
+ * @packageDocumentation
+ */
+
+import { ComponentEngine, VNode } from 'core/component/engines';
+import type { DirectiveOptions } from 'core/component/directives/render/interface';
+
+export * from 'core/component/directives/render/interface';
+
+ComponentEngine.directive('render', {
+	beforeCreate(opts: DirectiveOptions, vnode: VNode): CanUndef<VNode> {
+		const
+			newVNode = opts.value,
+			originalChildren = vnode.children;
+
+		if (newVNode != null) {
+			const canReplaceVNode =
+				vnode.type === 'template' &&
+				!Object.isArray(newVNode) &&
+				Object.size(vnode.props) === 0;
+
+			if (canReplaceVNode) {
+				return newVNode;
+			}
+
+			if (Object.isString(vnode.type)) {
+				vnode.children = Array.concat([], newVNode);
+
+			} else {
+				const slots = Object.isPlainObject(originalChildren) ? Object.reject(originalChildren, /^_/) : {};
+				vnode.children = slots;
+
+				if (Object.isArray(newVNode)) {
+					if (isSlot(newVNode[0])) {
+						for (let i = 0; i < newVNode.length; i++) {
+							const
+								el = newVNode[i],
+								slot = el.props?.slot;
+
+							if (slot != null) {
+								slots[slot] = () => el.children ?? getDefSlotFromChildren(slot);
+							}
+						}
+
+						return;
+					}
+
+				} else if (isSlot(newVNode)) {
+					const {slot} = newVNode.props!;
+					slots[slot] = () => newVNode.children ?? getDefSlotFromChildren(slot);
+					return;
+				}
+
+				slots.default = () => newVNode;
+			}
+		}
+
+		function isSlot(vnode: CanUndef<VNode>): boolean {
+			return vnode?.type === 'template' && vnode.props?.slot != null;
+		}
+
+		function getDefSlotFromChildren(slotName: string): unknown {
+			if (Object.isPlainObject(originalChildren)) {
+				const
+					slot = originalChildren[slotName];
+
+				if (Object.isFunction(slot)) {
+					return slot();
+				}
+
+				return slot;
+			}
+
+			return originalChildren;
+		}
+	}
+});
