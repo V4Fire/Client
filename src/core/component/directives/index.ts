08861981cc59fdcb464ecdc15a896af04af84e64
kobezzza <kobezzza@gmail.com>
2022-07-01 16:52:25 +0300

    fix: import `ref`

--- src/core/component/directives/index.ts
+++ src/core/component/directives/index.ts
@@ -7,6 +7,7 @@
  */
 
 import 'core/component/directives/tag';
+import 'core/component/directives/ref';
 import 'core/component/directives/hook';
 import 'core/component/directives/attrs';
 import 'core/component/directives/async-target';
