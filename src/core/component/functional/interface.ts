45d2114e1ecc928dadd5e31e290005d74d2c746f
kobezzza <kobezzza@gmail.com>
2022-07-01 16:52:50 +0300

    refactor: removed dead code

--- src/core/component/functional/interface.ts
+++ src/core/component/functional/interface.ts
@@ -6,15 +6,11 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import type { VNode, Slots } from 'core/component/engines';
+import type { Slots } from 'core/component/engines';
 import type { ComponentInterface } from 'core/component/interface';
 
 export interface VirtualContextOptions {
 	parent: ComponentInterface;
 	props?: Nullable<Dictionary>;
 	slots?: Nullable<Slots>;
-}
-
-export interface FunctionalVNode extends VNode {
-	virtualComponent: ComponentInterface;
 }
