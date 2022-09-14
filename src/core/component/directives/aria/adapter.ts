98275b201eb3254a72a3e41b0b50639ea8961d68
kobezzza <kobezzza@gmail.com>
2022-09-14 14:58:49 +0300

    refactor: simplify adapter API

--- src/core/component/directives/aria/adapter.ts
+++ src/core/component/directives/aria/adapter.ts
@@ -11,16 +11,16 @@ import type iBlock from 'super/i-block/i-block';
 
 import * as roles from 'core/component/directives/aria/roles';
 import type { DirectiveOptions } from 'core/component/directives/aria/interface';
-import type { AriaRole, EngineOptions } from 'core/component/directives/aria/roles';
+import type { ARIARole, EngineOptions } from 'core/component/directives/aria/roles';
 
 /**
- * An adapter to create an ARIA role instance based on the passed directive options and to add common attributes
+ * An adapter to create an ARIA role instance based on the passed directive params and to add common attributes
  */
-export default class AriaAdapter {
+export default class ARIAAdapter {
 	/**
-	 * Parameters passed from the directive
+	 * Parameters passed from the associated directive
 	 */
-	protected readonly options: DirectiveOptions;
+	protected readonly params: DirectiveOptions;
 
 	/** @see [[Async]] */
 	protected readonly async: Async = new Async();
@@ -28,52 +28,55 @@ export default class AriaAdapter {
 	/**
 	 * An instance of the associated ARIA role
 	 */
-	protected role: CanUndef<AriaRole>;
+	protected role: ARIARole | null = null;
 
-	constructor(options: DirectiveOptions) {
-		this.options = options;
-		this.setAriaRole();
+	/**
+	 * A context of the associated directive
+	 */
+	protected get ctx(): iBlock['unsafe'] {
+		return Object.cast<iBlock['unsafe']>(this.params.vnode.fakeContext);
+	}
+
+	/**
+	 * @param params - parameters passed from the associated directive
+	 */
+	constructor(params: DirectiveOptions) {
+		this.params = params;
+		this.createRole();
 		this.init();
 	}
 
 	/**
-	 * Initiates the base logic of the directive
+	 * Initializes the adapter according to the associated directive parameters
 	 */
 	init(): void {
-		this.setAriaLabelledBy();
-		this.setAriaAttributes();
-		this.addEventHandlers();
-
+		this.setAttributes();
+		this.attachRoleHandlers();
 		this.role?.init();
 	}
 
 	/**
-	 * Runs on unbind directive hook. Clears the Async instance.
+	 * Destroys the adapter and cancels all tied threads
 	 */
 	destroy(): void {
-		this.async.clearAll();
-	}
-
-	protected get ctx(): iBlock['unsafe'] {
-		return Object.cast<iBlock['unsafe']>(this.options.vnode.fakeContext);
+		this.async.clearAll().locked = true;
 	}
 
 	/**
-	 * If the role was passed as a directive argument sets specified engine
+	 * Creates an ARIA role based on the associated directive parameters.
+	 * If the role is not explicitly specified in the parameters, then the common strategy will be used.
+	 * The method returns an instance of the created role or null.
 	 */
-	protected setAriaRole(): CanUndef<AriaRole> {
+	protected createRole(): ARIARole | null {
 		const
-			{el, binding} = this.options,
+			{el, binding} = this.params,
 			{value, modifiers, arg: role} = binding;
 
 		if (role == null) {
-			return;
+			return null;
 		}
 
-		const
-			engine = `${role.capitalize()}Engine`;
-
-		const options: EngineOptions<AriaRole['Params']> = {
+		const params: EngineOptions<ARIARole['Params']> = {
 			el,
 			modifiers,
 			params: value,
@@ -81,107 +84,96 @@ export default class AriaAdapter {
 			async: this.async
 		};
 
-		this.role = new roles[engine](options);
+		return this.role = new roles[role.capitalize()](params);
 	}
 
 	/**
-	 * Sets aria-labelledby attribute to the element from directive parameters
+	 * Sets the ARIA attributes passed in the associated directive parameters
 	 */
-	protected setAriaLabelledBy(): void {
+	protected setAttributes(): void {
 		const
-			{binding, el} = this.options,
-			{dom} = this.ctx,
-			{labelledby} = binding.value ?? {},
-			attr = 'aria-labelledby';
-
-		let
-			isAttrSet = false;
+			{binding} = this.params;
 
 		for (const mod in binding.modifiers) {
 			if (!mod.startsWith('#')) {
 				continue;
 			}
 
-			const
-				title = mod.slice(1),
-				id = dom.getId(title);
-
-			el.setAttribute(attr, id);
-			isAttrSet = true;
+			this.setAttr('aria-labelledby', this.ctx.dom.getId(mod.slice(1)));
+			break;
 		}
 
-		if (labelledby != null) {
-			el.setAttribute(attr, Object.isArray(labelledby) ? labelledby.join(' ') : labelledby);
-			isAttrSet = true;
-		}
-
-		if (isAttrSet) {
-			this.async.worker(() => el.removeAttribute(attr));
-		}
-	}
-
-	/**
-	 * Sets aria attributes from passed params except `aria-labelledby`
-	 */
-	protected setAriaAttributes(): void {
-		const
-			{el, binding} = this.options,
-			params: Dictionary<string> = binding.value;
-
-		for (const key in params) {
-			if (!params.hasOwnProperty(key)) {
-				continue;
-			}
-
-			const
-				roleParams = this.role?.Params,
-				param = params[key];
-
-			if (!roleParams?.hasOwnProperty(key) && key !== 'labelledby' && param != null) {
+		if (Object.isDictionary(binding.value)) {
+			Object.forEach(binding.value, (param, key) => {
 				const
-					attr = `aria-${key}`;
+					roleParams = this.role?.Params;
 
-				el.setAttribute(attr, param);
-				this.async.worker(() => el.removeAttribute(attr));
-			}
+				if (param == null || roleParams?.hasOwnProperty(key)) {
+					return;
+				}
+
+				this.setAttr(`aria-${key}`.dasherize(), param);
+			});
 		}
 	}
 
 	/**
-	 * Sets handlers for the base role events: open, close, change.
-	 * Expects the passed into directive specified event properties to be Function, Promise or String.
+	 * Sets the value of the specified attribute to the node on which the ARIA directive is initialized
+	 *
+	 * @param name - the attribute name
+	 * @param value - the attribute value or a list of values
 	 */
-	protected addEventHandlers(): void {
-		if (this.role == null) {
+	protected setAttr(name: string, value: CanUndef<CanArray<unknown>>): void {
+		const
+			{el} = this.params;
+
+		if (value == null) {
 			return;
 		}
 
+		el.setAttribute(name, Object.isArray(value) ? value.join(' ') : String(value));
+		this.async.worker(() => el.removeAttribute(name));
+	}
+
+	/**
+	 * Attaches the handlers specified in the parameters of the associated directive to the created ARIA role.
+	 * Any handler can be specified as a function, a promise, or a string.
+	 *
+	 * To specify a handler, use the `@` character at the beginning of the parameter key.
+	 * For example, `@open` will be converted to the role `onOpen` handler.
+	 */
+	protected attachRoleHandlers(): void {
 		const
-			params = this.options.binding.value;
+			{role, params: {binding}} = this;
 
-		for (const key in params) {
-			if (key.startsWith('@')) {
-				const
-					callbackName = `on-${key.slice(1)}`.camelize(false);
-
-				if (!Object.isFunction(this.role[callbackName])) {
-					Object.throw('Aria role engine does not contains event handler for passed event name or the type of engine\'s property is not a function');
-				}
-
-				const
-					callback = this.role[callbackName].bind(this.role),
-					property = params[key];
-
-				if (Object.isFunction(property)) {
-					property(callback);
-
-				} else if (Object.isPromiseLike(property)) {
-					void property.then(callback);
-
-				} else if (Object.isString(property)) {
-					this.ctx.on(property, callback);
-				}
-			}
+		if (role == null || !Object.isDictionary(binding.value)) {
+			return;
 		}
+
+		Object.forEach(binding.value, (val, key) => {
+			if (val == null || !key.startsWith('@')) {
+				return;
+			}
+
+			const
+				handlerName = `on-${key.slice(1)}`.camelize(false);
+
+			if (!Object.isFunction(role[handlerName])) {
+				throw new ReferenceError(`The associated ARIA role does not have a handler named "${handlerName}"`);
+			}
+
+			const
+				handler = role[handlerName].bind(this.role);
+
+			if (Object.isFunction(val)) {
+				val(handler);
+
+			} else if (Object.isPromiseLike(val)) {
+				val.then(handler, stderr);
+
+			} else if (Object.isString(val)) {
+				this.async.on(this.ctx, val, handler);
+			}
+		});
 	}
 }
