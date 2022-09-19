47f76c73c9c25af0106a2b9e8b7c7d4700935a56
kobezzza <kobezzza@gmail.com>
2022-09-19 15:14:05 +0300

    refactor: removed the Factory class & extracted all its methods to a separated functions

--- src/core/dom/image/helpers.ts
+++ src/core/dom/image/helpers.ts
@@ -9,44 +9,163 @@
 import { concatURLs } from 'core/url';
 import { getSrcSet } from 'core/html';
 
-import type { ImageOptions } from 'core/dom/image/interface';
+import { loadImage, IS_LOADED, IS_LOADING } from 'core/dom/image/const';
+import type { Picture, ImageOptions, ImageRole, ImageState } from 'core/dom/image/interface';
 
 /**
- * Resolves the value of the `src` attribute for the given resource by the passed parameters and returns it
+ * Creates an image state object by the passed parameters and returns it
  *
- * @param src - the original resource src
- * @param resourceParams - additional parameters of the requested resource
- * @param commonParams - common additional parameters
+ * @param imageParams - the requested image parameters
+ * @param commonParams - common parameters
+ * @param role - a role of the created image
  */
-export function resolveSrc(
-	src: CanUndef<string>,
-	resourceParams: ImageOptions,
-	commonParams: ImageOptions
-): string {
-	if (src == null || src === '') {
-		return '';
+export function createImageState(
+	imageParams: ImageOptions,
+	commonParams: ImageOptions,
+	role: ImageRole
+): ImageState {
+	const state = {
+		role,
+		isFailed: false,
+		imageParams,
+		commonParams
+	};
+
+	if (Object.isArray(imageParams.sources) && imageParams.sources.length > 0) {
+		return {...state, ...createPicture(imageParams, commonParams, role)};
 	}
 
-	const
-		baseSrc = getBaseSrc(resourceParams, commonParams);
-
-	if (baseSrc == null || baseSrc === '') {
-		return src;
-	}
-
-	return concatURLs(baseSrc, src);
+	return {
+		...state,
+		picture: null,
+		img: createImgElement(imageParams, commonParams, role)
+	};
 }
 
 /**
- * Resolves the value of the `srcset` attribute for the given resource by the passed parameters and returns it
+ * Creates a `picture` element with resources by the passed parameters.
+ * The method returns a structure with a link to the created `picture` element,
+ * as well as to the associated `img` element.
  *
- * @param srcset - the original resource src set
- * @param resourceParams - additional parameters of the requested resource
- * @param commonParams - common additional parameters
+ * @see https://developer.mozilla.org/ru/docs/Web/HTML/Element/picture
+ *
+ * @example
+ * ```typescript
+ * const {picture, img} = createPicture({
+ *   src: 'preview.jpg',
+ *   sources: [{srcset: 'srcset-with-webp-img', role: 'webp'}]
+ * }, {
+ *   src: 'main.jpg',
+ *   sources: [{srcset: 'srcset-with-webp-img', role: 'webp'}],
+ *   baseSrc: 'https://path'
+ * });
+ *
+ * document.appendChild(picture);
+ * ```
+ *
+ * ```html
+ * <picture>
+ *   <source srcset="https://path/srcset-with-webp-img" role="image/webp">
+ *   <img src="https://path/preview.jpg">
+ * </picture>
+ * ```
+ *
+ * @param imageParams - the requested image parameters
+ * @param commonParams - common parameters
+ * @param role - a role of the created image
+ */
+export function createPicture(
+	imageParams: ImageOptions,
+	commonParams: ImageOptions,
+	role: ImageRole
+): Picture {
+	const
+		picture = document.createElement('picture'),
+		img = createImgElement(imageParams, commonParams, role);
+
+	if (imageParams.sources != null && imageParams.sources.length > 0) {
+		picture.appendChild(createSourceElements(imageParams, commonParams));
+	}
+
+	picture.appendChild(img);
+	return {picture, img};
+}
+
+/**
+ * Creates `source` elements by the passed parameters and returns them in a single document fragment
+ * @see https://developer.mozilla.org/ru/docs/Web/HTML/Element/source
+ *
+ * @param imageParams - the requested image parameters
+ * @param commonParams - common parameters
+ */
+export function createSourceElements(imageParams: ImageOptions, commonParams: ImageOptions): DocumentFragment {
+	const
+		fragment = document.createDocumentFragment();
+
+	if (imageParams.sources == null || imageParams.sources.length === 0) {
+		return fragment;
+	}
+
+	imageParams.sources.forEach((source) => {
+		const
+			elem = document.createElement('source');
+
+		elem.media = source.media ?? '';
+		elem.sizes = source.sizes ?? '';
+		elem.srcset = resolveSrcSet(source.srcset, imageParams, commonParams);
+		elem.type = getSourceType(source.type);
+
+		fragment.appendChild(elem);
+	});
+
+	return fragment;
+}
+
+/**
+ * Creates an `img` element by the passed parameters and returns it
+ *
+ * @param imageParams - the requested image parameters
+ * @param commonParams - common parameters
+ * @param role - a role of the created image
+ */
+export function createImgElement(
+	imageParams: ImageOptions,
+	commonParams: ImageOptions,
+	role: ImageRole
+): HTMLImageElement {
+	const
+		img = document.createElement('img');
+
+	img[loadImage] = () => {
+		img[IS_LOADING] = true;
+
+		img.src = resolveSrc(imageParams.src, imageParams, commonParams);
+		img.srcset = resolveSrcSet(imageParams.srcset, imageParams, commonParams);
+		img.sizes = imageParams.sizes ?? '';
+
+		img.init.then(
+			() => img[IS_LOADED] = true,
+			() => img[IS_LOADED] = false
+		);
+	};
+
+	if (role !== 'broken') {
+		img[loadImage]();
+	}
+
+	return img;
+}
+
+/**
+ * Resolves a value of the `srcset` attribute for the given image by the passed parameters and returns it
+ *
+ * @param srcset - the original image src set
+ * @param imageParams - the requested image parameters
+ * @param commonParams - common parameters
  */
 export function resolveSrcSet(
-	srcset: CanUndef<Dictionary<string> | string>,
-	resourceParams: ImageOptions,
+	srcset: Nullable<Dictionary<string> | string>,
+	imageParams: ImageOptions,
 	commonParams: ImageOptions
 ): string {
 	const normalizedSrcset = Object.isDictionary(srcset) ?
@@ -58,24 +177,61 @@ export function resolveSrcSet(
 	}
 
 	const
-		baseSrc = getBaseSrc(resourceParams, commonParams);
+		baseSrc = getBaseSrc(imageParams, commonParams);
 
 	if (baseSrc == null || baseSrc === '') {
 		return normalizedSrcset;
 	}
 
-	return normalizedSrcset
-		.split(',')
+	return normalizedSrcset.split(',')
 		.map((val) => concatURLs(baseSrc, val.trim()))
 		.join(',');
 }
 
 /**
- * Returns the underlying resource src based on the given parameters
+ * Resolves a value of the `src` attribute for the given image by the passed parameters and returns it
  *
- * @param resourceParams - additional parameters of the requested resource
- * @param commonParams - common additional parameters
+ * @param src - the original image src
+ * @param imageParams - the requested image parameters
+ * @param commonParams - common parameters
  */
-function getBaseSrc(resourceParams: ImageOptions, commonParams: ImageOptions): CanUndef<string> {
-	return resourceParams.baseSrc ?? commonParams.baseSrc ?? '';
+export function resolveSrc(
+	src: Nullable<string>,
+	imageParams: ImageOptions,
+	commonParams: ImageOptions
+): string {
+	if (src == null || src === '') {
+		return '';
+	}
+
+	const
+		baseSrc = getBaseSrc(imageParams, commonParams);
+
+	if (baseSrc == null || baseSrc === '') {
+		return src;
+	}
+
+	return concatURLs(baseSrc, src);
+}
+
+/**
+ * Returns the underlying image src based on the given parameters
+ *
+ * @param imageParams - the requested image parameters
+ * @param commonParams - common parameters
+ */
+function getBaseSrc(imageParams: ImageOptions, commonParams: ImageOptions): CanUndef<string> {
+	return imageParams.baseSrc ?? commonParams.baseSrc ?? '';
+}
+
+/**
+ * Returns a value of the `type` attribute for the `source` element, based on the passed image type
+ * @param type - the original image type
+ */
+export function getSourceType(type: Nullable<string>): string {
+	if (type == null || type === '') {
+		return '';
+	}
+
+	return `image/${type}`;
 }
