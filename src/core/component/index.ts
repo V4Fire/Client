de0535df375df1d6d803987c47f55b8464e450a5
kobezzza <kobezzza@gmail.com>
2022-10-03 16:44:36 +0300

    fix: fixed export of state

--- src/core/component/index.ts
+++ src/core/component/index.ts
@@ -14,7 +14,7 @@
 import 'core/component/directives';
 
 import * as init from 'core/component/init';
-import * as globalState from 'core/component/state';
+import globalState from 'core/component/state';
 
 export { init, globalState };
 export { ComponentEngine as default } from 'core/component/engines';
