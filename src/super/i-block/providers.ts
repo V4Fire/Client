331f43f67d81224fa00e91be39b4f0623e0d622e
kobezzza <kobezzza@gmail.com>
2022-10-14 15:43:36 +0300

    refactor: moved some methods to state

--- src/super/i-block/providers.ts
+++ src/super/i-block/providers.ts
@@ -14,9 +14,6 @@ import config from 'config';
 import type { AsyncOptions } from 'core/async';
 import { component } from 'core/component';
 
-import type bRouter from 'base/b-router/b-router';
-import type { ConverterCallType } from 'friends/state';
-
 import { statuses } from 'super/i-block/const';
 import { system, hook } from 'super/i-block/modules/decorators';
 import type { InitLoadCb, InitLoadOptions } from 'super/i-block/interface';
@@ -44,20 +41,6 @@ export default abstract class iBlockProviders extends iBlockState {
 	}))
 
 	dontWaitRemoteProviders!: boolean;
-
-	/**
-	 * A link to the application router
-	 */
-	get router(): CanUndef<bRouter> {
-		return this.field.get('routerStore', this.r);
-	}
-
-	/**
-	 * A link to the active route object
-	 */
-	get route(): CanUndef<this['r']['CurrentPage']> {
-		return this.field.get('route', this.r);
-	}
 
 	/**
 	 * Loads component initialization data.
@@ -233,98 +216,5 @@ export default abstract class iBlockProviders extends iBlockState {
 		}
 
 		return Promise.resolve();
-	}
-
-	/**
-	 * This method works as a two-way connector between the component and its storage.
-	 *
-	 * When the component is initializing, it requests the storage for its associated data, using the `globalName` prop
-	 * as the namespace to search. When the storage is ready to provide data to the component, it passes the data to
-	 * this method. After that, the method returns a dictionary associated with the component properties
-	 * (you can specify a complex path with dots, like `'foo.bla.bar'` or `'mods.hidden'`).
-	 *
-	 * Also, the component will watch for changes to each property in this dictionary.
-	 * If at least one of  these properties is changed, the entire data batch will be synchronized with the storage
-	 * using this method. When the component provides the storage data, the second argument to the method is `'remote'`.
-	 *
-	 * @param [data] - advanced data
-	 * @param [type] - the call type
-	 */
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
-	protected syncStorageState(data?: Dictionary, type: ConverterCallType = 'component'): Dictionary {
-		return {...data};
-	}
-
-	/**
-	 * Returns a dictionary with the default component properties to reset the storage state.
-	 * This method will be used when calling `state.resetStorage`.
-	 *
-	 * @param [data] - advanced data
-	 */
-	protected convertStateToStorageReset(data?: Dictionary): Dictionary {
-		const
-			stateFields = this.syncStorageState(data),
-			res = {};
-
-		if (Object.isDictionary(stateFields)) {
-			for (let keys = Object.keys(stateFields), i = 0; i < keys.length; i++) {
-				res[keys[i]] = undefined;
-			}
-		}
-
-		return res;
-	}
-
-	/**
-	 * This method works as a two-way connector between the component and the application router.
-	 *
-	 * When the component is initializing, it requests the router for its associated data.
-	 * The router provides the data by using this method. After that, the method returns a dictionary associated with
-	 * the component properties (you can specify a complex path with dots, like `'foo.bla.bar'` or `'mods.hidden'`).
-	 *
-	 * Also, the component will watch for changes to each property in this dictionary.
-	 * If at least one of  these properties is changed, the entire data batch will be synchronized with the router
-	 * using this method. When the component provides the router data, the second argument to the method is `'remote'`.
-	 *
-	 * Keep in mind that the router is global to all components, meaning the dictionary this method passes to the router
-	 * will extend the current route data, but not override  (`router.push(null, {...route, ...componentData}})`).
-	 *
-	 * @param [data] - advanced data
-	 * @param [type] - the call type
-	 */
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
-	protected syncRouterState(data?: Dictionary, type: ConverterCallType = 'component'): Dictionary {
-		return {};
-	}
-
-	/**
-	 * Returns a dictionary with the default component properties to reset the router state.
-	 * This method will be used when calling `state.resetRouter`.
-	 *
-	 * @param [data] - advanced data
-	 */
-	protected convertStateToRouterReset(data?: Dictionary): Dictionary {
-		const
-			stateFields = this.syncRouterState(data),
-			res = {};
-
-		if (Object.isDictionary(stateFields)) {
-			for (let keys = Object.keys(stateFields), i = 0; i < keys.length; i++) {
-				res[keys[i]] = undefined;
-			}
-		}
-
-		return res;
-	}
-
-	@hook({beforeRuntime: {functional: false}})
-	protected override initBaseAPI(): void {
-		super.initBaseAPI();
-
-		const
-			i = this.instance;
-
-		this.syncStorageState = i.syncStorageState.bind(this);
-		this.syncRouterState = i.syncRouterState.bind(this);
 	}
 }
