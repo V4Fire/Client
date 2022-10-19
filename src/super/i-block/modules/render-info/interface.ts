bc9694b95501a7b6e452d73d28c776c94e72b8d8
Anastasiia Razumova <annarzamm@yandex-team.com>
2022-10-19 13:37:41 +0300

    added copyright

--- src/super/i-block/modules/render-info/interface.ts
+++ src/super/i-block/modules/render-info/interface.ts
@@ -1,3 +1,11 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
 import type iBlock from 'super/i-block/i-block';
 
 /**
