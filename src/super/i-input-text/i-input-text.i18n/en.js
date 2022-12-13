ca4cf89d799a23637a947906a6b245c7f0ac6bf5
Dmitrii <lexur-02@yandex.ru>
2022-12-13 19:26:33 +0600

    more translate

---
+++ src/super/i-input-text/i-input-text.i18n/en.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'Required field': 'Required field',
+	'A value must match the pattern': 'A value must match the pattern',
+	'Value length must be at least {min} characters': 'Value length must be at least {min} characters',
+	'Value length must be no more than {max} characters': 'Value length must be no more than {max} characters'
+};
