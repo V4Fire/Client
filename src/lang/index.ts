b69b55bd1579655e0506a166bb9f3bc57ae4cce6
Dmitrii <lexur-02@yandex.ru>
2023-01-17 16:35:03 +0600

    return comment

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,3 +1,11 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
 import Super from '@v4fire/core/lang';
 
 export * from '@v4fire/core/lang';
