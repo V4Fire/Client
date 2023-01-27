786fd224e79f27b35379d7da033a85165b6bf8fa
Dmitrii <lexur-02@yandex.ru>
2023-01-27 16:30:20 +0600

    i18n

--- build/webpack/plugins/i18n-plugin.js
+++ build/webpack/plugins/i18n-plugin.js
@@ -10,6 +10,7 @@
 
 const
 	{resolve: pzlr} = require('@pzlr/build-core'),
+	fs = require('fs'),
 	fg = require('fast-glob');
 
 /**
@@ -20,15 +21,34 @@ module.exports = class I18NGeneratorPlugin {
 		compiler.hooks.done.tap('I18NGeneratorPlugin', doneHook);
 
 		function doneHook(stats) {
-			console.log(pzlr.sourceDirs);
 			const paths = pzlr.sourceDirs.map((el) => `${el}/**/*.i18n/*.js`);
-			const res = fg.sync(paths);
+			const result = {};
+
+			fg.sync(paths).forEach((path) => {
+				const
+					element = require(path),
+					parsedPath = /\/[^/]*?\.i18n\/(.*?)\.js$/i.exec(path);
+
+					if (parsedPath != null) {
+						const
+							lang = parsedPath[1];
+
+						result[lang] = result[lang] ?? {};
+
+						Object.keys(element).forEach((keysetName) => {
+							result[lang][keysetName] = {
+								...result[lang][keysetName] != null ? result[lang][keysetName] : {},
+								...element[keysetName]
+							};
+						});
+					}
+			});
+
+			Object.entries(result).forEach(([lang, value]) => {
+				fs.writeFileSync(`${pzlr.cwd}/dist/client/${lang}.json`, JSON.stringify(value, undefined, 2));
+			});
+
 			debugger;
-			// pzlr for all layers
-			// fg => list of files
-			// require('') || fs.readFile('path') || JSON.parse();
-			// result = {}
-			// json.stringify(result) => fs.writeFile(dist/ru.json);
 			// dist find globa('*.html')
 			// do while ru, en, pr
 			// copy => p_index_ru.html => find id='asdfads' => paste => json
