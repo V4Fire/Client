e03edb194bc21ceaa15950dda079538851ccb3ca
Georgii Prokhorov <65154994+geopr@users.noreply.github.com>
2023-03-24 15:50:03 +0300

    rewrote tests for b-slider (#796)
    * rewrote slides rendering tests for b-slider
    
    * rewrote slots rendering tests for b-slider
    
    * rewrote gestures tests for b-slider
    
    * renamed directive from 'v-resize-observer' to 'v-on-resize' in b-slider
    
    * prettify events tests for b-slider
    
    * prettify gestures tests in b-slider
    
    * refactor: refactoring tests for b-slider
    
    * refactor: refactoring tests
    
    * fix: typo
    
    * chore: better test descriptions
    
    ---------
    
    Co-authored-by: geoprv <geoprv@yandex-team.ru>
    Co-authored-by: kobezzza <kobezzza@gmail.com>

---
+++ src/components/base/b-slider/test/unit/events.ts
@@ -0,0 +1,78 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import type { JSHandle } from 'playwright';
+
+import test from 'tests/config/unit/test';
+import Gestures from 'tests/helpers/gestures';
+import Component from 'tests/helpers/component';
+import DOM from 'tests/helpers/dom';
+
+import type GesturesInterface from 'core/prelude/test-env/gestures';
+
+import type bSlider from 'components/base/b-slider/b-slider';
+
+test.describe('<b-slider> standard component events', () => {
+	let
+		slider: JSHandle<bSlider>,
+		gestures: JSHandle<GesturesInterface>;
+
+	test.beforeEach(async ({demoPage, page}) => {
+		await demoPage.goto();
+
+		const children = [1, 2, 3, 4].map((i) => ({
+			type: 'img',
+			attrs: {
+				id: `slide_${i}`,
+				src: 'https://fakeimg.pl/$300x$200',
+				width: 300,
+				height: 200
+			}
+		}));
+
+		slider = await Component.createComponent<bSlider>(page, 'b-slider', {children});
+
+		const
+			selector = DOM.elNameSelectorGenerator('b-slider', 'window');
+
+		gestures = await Gestures.create(page, {
+			targetEl: selector,
+			dispatchEl: selector
+		});
+	});
+
+	test('should emit a `change` event when a slide is swiped', async () => {
+		const isEmitted = await slider.evaluate((ctx) => new Promise<boolean>((resolve) => {
+			ctx.once('change', () => resolve(true));
+			ctx.moveSlide(1);
+		}));
+
+		test.expect(isEmitted).toBe(true);
+	});
+
+	test('should emit a `swipeStart` event when the pointer started swiping the slider', async () => {
+		await testSwipeEvent('swipeStart');
+	});
+
+	test('should emit a `swipeEnd` event when the pointer stopped swiping the slider', async () => {
+		await testSwipeEvent('swipeEnd');
+	});
+
+	/**
+	 * @param event
+	 */
+	async function testSwipeEvent(event: string): Promise<void> {
+		const isEmitted = await slider.evaluate((ctx, {gestures, event}) => new Promise((resolve) => {
+			ctx.on(event, () => resolve(true));
+
+			void gestures.swipe(gestures.buildSteps(2, 150, 50, 150, 0), true);
+		}), {gestures, event});
+
+		test.expect(isEmitted).toBe(true);
+	}
+});
