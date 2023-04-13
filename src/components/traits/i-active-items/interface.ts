0c51644b35bc66370c0d10e26d7f682c3679bcb2
Artem Shinkaruk <artasend@gmail.com>
2023-04-13 21:32:09 +0700

    feat(traits): add iActiveItems trait

---
+++ src/components/traits/i-active-items/interface.ts
@@ -0,0 +1,26 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+export interface Item extends Dictionary {
+	/**
+	 * Item label text
+	 */
+	label?: string;
+
+	/**
+	 * Item value
+	 */
+	value?: unknown;
+
+	/**
+	 * True if the item is active
+	 */
+	active?: boolean;
+}
+
+export type Active = unknown | Set<unknown>;
