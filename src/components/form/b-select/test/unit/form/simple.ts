5cc00b9becec9b64dc9f9de0daae00118a5d63d1
Artem Shinkaruk <artasend@gmail.com>
2023-04-27 00:04:10 +0700

    test(b-select): refactor form/simple runner

--- src/components/form/b-select/test/unit/form/simple.ts
+++ src/components/form/b-select/test/unit/form/simple.ts
@@ -10,9 +10,10 @@ import type { JSHandle, Page } from 'playwright';
 
 import test from 'tests/config/unit/test';
 
-import type bSelect from 'components/form/b-select/b-select';
-
 import { Component, Utils } from 'tests/helpers';
+
+import type bSelect from 'components/form/b-select/b-select';
+import { assertValueIs, assertFormValueIs, setValue } from 'components/form/b-select/test/helpers';
 
 test.describe('<b-select> form API', () => {
 
@@ -20,48 +21,48 @@ test.describe('<b-select> form API', () => {
 		await demoPage.goto();
 	});
 
-	test('providing a value', async ({page}) => {
+	test('component\'s `value` should be set via a `value` prop', async ({page}) => {
 		const target = await renderSelect(page, {
 			value: '10'
 		});
 
-		test.expect(await target.evaluate((ctx) => ctx.value)).toBe('10');
+		await assertValueIs(target, '10');
 	});
 
-	test('providing the default value', async ({page}) => {
+	test('component\'s `value` should be set via a `default` prop', async ({page}) => {
 		const target = await renderSelect(page, {
 			default: '11'
 		});
 
-		test.expect(await target.evaluate((ctx) => ctx.value)).toBe('11');
+		await assertValueIs(target, '11');
 	});
 
-	test('providing the default value and value', async ({page}) => {
+	test([
+		'component\'s `value` should be equal to the `value` prop',
+		'when the `default` and `value` props are provided simultaneously'
+	].join(' '), async ({page}) => {
 		const target = await renderSelect(page, {
 			value: '10',
 			default: '11'
 		});
 
-		test.expect(await target.evaluate((ctx) => ctx.value)).toBe('10');
+		await assertValueIs(target, '10');
 	});
 
-	test('getting a form value', async ({page}) => {
+	test('`formValue` should be modified by the `formValueConverter`', async ({page}) => {
 		const target = await renderSelect(page);
 
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBeUndefined();
+		await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBeUndefined();
 
-		await target.evaluate((ctx) => {
-			ctx.value = '10';
-		});
+		await setValue(target, '10');
 
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBe(40);
+		await assertFormValueIs(target, 40);
 	});
 
-	test('getting a group form value', async ({page}) => {
+	test([
+		'`groupFormValue` should be a tuple:',
+		'label of the item, which has the specified `value`, and the `formValue`'
+	].join(' '), async ({page}) => {
 		const target = await renderSelect(page, {
 			value: '10'
 		});
@@ -71,85 +72,19 @@ test.describe('<b-select> form API', () => {
 		).toEqual(['bar', 40]);
 	});
 
-	test('resetting a component without the default value', async ({page}) => {
-		const target = await renderSelect(page, {
-			value: '10'
-		});
+	test.describe('`reset`', () => {
+		test('should reset the component\'s value to the `default`', async ({page}) => {
+			const target = await renderSelect(page, {
+				default: '10'
+			});
 
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('10');
+			await assertValueIs(target, '10');
 
-		test.expect(
-			await target.evaluate(async (ctx) => {
-				const res: any[] = [];
+			await setValue(target, '20');
 
-				ctx.on('onReset', (v) => {
-					res.push([v, ctx.text]);
-				});
+			await assertValueIs(target, '20');
 
-				res.push(await ctx.reset());
-				res.push(await ctx.reset());
-
-				return res;
-			})
-		).toEqual([[undefined, ''], true, false]);
-
-		await target.evaluate((ctx) => ctx.reset());
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBeUndefined();
-	});
-
-	test('clearing a component without the default value', async ({page}) => {
-		const target = await renderSelect(page, {
-			value: '10'
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('10');
-
-		test.expect(
-			await target.evaluate(async (ctx) => {
-				const res: any[] = [];
-
-				ctx.on('onClear', (v) => {
-					res.push([v, ctx.text]);
-				});
-
-				res.push(await ctx.clear());
-				res.push(await ctx.clear());
-
-				return res;
-			})
-		).toEqual([[undefined, ''], true, false]);
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBeUndefined();
-	});
-
-	test('resetting a component with the default value', async ({page}) => {
-		const target = await renderSelect(page, {
-			default: '10'
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('10');
-
-		await target.evaluate((ctx) => {
-			ctx.value = '20';
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('20');
-
-		test.expect(
-			await target.evaluate(async (ctx) => {
+			await test.expect(target.evaluate(async (ctx) => {
 				const res: any[] = [];
 
 				ctx.on('onReset', (v) => {
@@ -160,15 +95,61 @@ test.describe('<b-select> form API', () => {
 				res.push(await ctx.reset());
 
 				return res;
-			})
-		).toEqual(['10', true, false]);
+			})).resolves.toEqual(['10', true, false]);
 
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('10');
+			await assertValueIs(target, '10');
+		});
+
+		test('should reset the component\'s value to undefined when the `default` is not provided', async ({page}) => {
+			const target = await renderSelect(page, {
+				value: '10'
+			});
+
+			await assertValueIs(target, '10');
+
+			await test.expect(target.evaluate(async (ctx) => {
+					const res: any[] = [];
+
+					ctx.on('onReset', (v) => {
+						res.push([v, ctx.text]);
+					});
+
+					res.push(await ctx.reset());
+					res.push(await ctx.reset());
+
+					return res;
+				})).resolves.toEqual([[undefined, ''], true, false]);
+
+			await target.evaluate((ctx) => ctx.reset());
+
+			await assertValueIs(target, undefined);
+		});
 	});
 
-	test('listening the `change` event', async ({page}) => {
+	test('should `clear` a component without the default value', async ({page}) => {
+		const target = await renderSelect(page, {
+			value: '10'
+		});
+
+		await assertValueIs(target, '10');
+
+		await test.expect(target.evaluate(async (ctx) => {
+			const res: any[] = [];
+
+			ctx.on('onClear', (v) => {
+				res.push([v, ctx.text]);
+			});
+
+			res.push(await ctx.clear());
+			res.push(await ctx.clear());
+
+			return res;
+		})).resolves.toEqual([[undefined, ''], true, false]);
+
+		await assertValueIs(target, undefined);
+	});
+
+	test('should collapse the `change` events which have occured in a single tick', async ({page}) => {
 		const target = await renderSelect(page);
 
 		test.expect(
@@ -193,6 +174,12 @@ test.describe('<b-select> form API', () => {
 		).toEqual(['2', '3']);
 	});
 
+	/**
+ 	 * Returns rendered `b-select` component with the `formValueConverter` and default items
+   *
+	 * @param page
+	 * @param attrs
+	 */
 	async function renderSelect(page: Page, attrs: RenderComponentsVnodeParams['attrs'] = {}): Promise<JSHandle<bSelect>> {
 
 		await Component.createComponent(page, 'b-select', [
