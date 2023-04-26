91d46b4aba4f9a971c2dbf79a85bbf9530d0b7e4
kobezzza <kobezzza@gmail.com>
2023-04-26 17:58:11 +0300

    refactor: extracted props

---
+++ src/components/form/b-form/props.ts
@@ -0,0 +1,98 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import iData, { component, prop, ModelMethod, CreateRequestOptions } from 'components/super/i-data/i-data';
+import type { ActionFn } from 'components/form/b-form/interface';
+
+@component()
+export default class bFormProps extends iData {
+	/** @see [[iVisible.hideIfOffline]] */
+	@prop(Boolean)
+	readonly hideIfOffline: boolean = false;
+
+	/**
+	 * The form identifier.
+	 * You can use it to connect the form to components lying "outside" from the form body (using the `form` attribute).
+	 *
+	 * @example
+	 * ```
+	 * < b-form :id = 'my-form'
+	 * < b-input :form = 'my-form'
+	 * ```
+	 */
+	@prop({type: String, required: false})
+	readonly id?: string;
+
+	/**
+	 * The form name.
+	 * You can use it to find the form element via `document.forms`.
+	 *
+	 * @example
+	 * ```
+	 * < b-form :name = 'my-form'
+	 * ```
+	 *
+	 * ```js
+	 * console.log(document.forms['my-form']);
+	 * ```
+	 */
+	@prop({type: String, required: false})
+	readonly name?: string;
+
+	/**
+	 * The form action URL (the URL where the data will be submitted) or a function to create the action.
+	 * If no value is specified, the component will use the default URLs from the data provider.
+	 *
+	 * @example
+	 * ```
+	 * < b-form :action = '/create-user'
+	 * < b-form :action = createUser
+	 * ```
+	 */
+	@prop({type: [String, Function], required: false})
+	readonly action?: string | ActionFn;
+
+	/**
+	 * The data provider method that is called when the form is submitted
+	 *
+	 * @example
+	 * ```
+	 * < b-form :dataProvider = 'User' | :method = 'upd'
+	 * ```
+	 */
+	@prop(String)
+	readonly method: ModelMethod = 'post';
+
+	/**
+	 * Additional form request parameters
+	 *
+	 * @example
+	 * ```
+	 * < b-form :params = {headers: {'x-foo': 'bla'}}
+	 * ```
+	 */
+	@prop(Object)
+	readonly paramsProp: CreateRequestOptions = {};
+
+	/**
+	 * If true, all form elements will be cached.
+	 * Caching means that if some component value has not changed since the last time the form was submitted,
+	 * it will not be resubmitted.
+	 *
+	 * @example
+	 * ```
+	 * < b-form :dataProvider = 'User' | :method = 'upd' | :cache = true
+	 *   < b-input :name = 'fname'
+	 *   < b-input :name = 'lname'
+	 *   < b-input :name = 'bd' | :cache = false
+	 *   < b-button :type = 'submit'
+	 * ```
+	 */
+	@prop(Boolean)
+	readonly cache: boolean = false;
+}
