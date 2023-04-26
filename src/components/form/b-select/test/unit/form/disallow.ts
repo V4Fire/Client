08b43a3f9d7e3ebfeb91b170dc7b57f8b3736f7e
Artem Shinkaruk <artasend@gmail.com>
2023-04-26 22:46:00 +0700

    test(b-select): refactor form/dissalow runner

--- src/components/form/b-select/test/unit/form/disallow.ts
+++ src/components/form/b-select/test/unit/form/disallow.ts
@@ -14,144 +14,114 @@ import { Component } from 'tests/helpers';
 
 import type bSelect from 'components/form/b-select/b-select';
 
-test.describe('<b-select> form API `disallow`', () => {
-
+test.describe('<b-select> form API', () => {
 	test.beforeEach(async ({demoPage}) => {
 		await demoPage.goto();
 	});
 
-	test('simple', async ({page}) => {
-		const target = await renderSelect(page, {
-			value: '10',
-			disallow: '10'
+	test.describe([
+		'`formValue` should be undefined',
+		'when the component\'s `value` matches the `disallow` provided'
+	].join(' '), () => {
+		test('as a single value', async ({page}) => {
+			const target = await renderSelect(page, {
+				value: '10',
+				disallow: '10'
+			});
+
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('10');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBeUndefined();
+
+			await target.evaluate((ctx) => {
+				ctx.value = '11';
+			});
+
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('11');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBe(11);
 		});
 
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('10');
+		test('as the multiple values', async ({page}) => {
+			const target = await renderSelect(page, {
+				value: '10',
+				disallow: ['10', '11']
+			});
 
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBeUndefined();
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('10');
 
-		await target.evaluate((ctx) => {
-			ctx.value = '11';
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBeUndefined();
+
+			await target.evaluate((ctx) => {
+				ctx.value = '11';
+			});
+
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('11');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBeUndefined();
+
+			await target.evaluate((ctx) => {
+				ctx.value = '12';
+			});
+
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('12');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBe(12);
 		});
 
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('11');
+		test('as a RegExp', async ({page}) => {
+			const target = await renderSelect(page, {
+				value: '10',
+				disallow: /^1/
+			});
 
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBe(11);
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('10');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBeUndefined();
+
+			await target.evaluate((ctx) => {
+				ctx.value = '11';
+			});
+
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('11');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBeUndefined();
+
+			await target.evaluate((ctx) => {
+				ctx.value = '5';
+			});
+
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('5');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBe(5);
+		});
+
+		test('as a function', async ({page}) => {
+			const target = await renderSelect(page, {
+				value: '10',
+				disallow: (v) => v === '10'
+			});
+
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('10');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBeUndefined();
+
+			await target.evaluate((ctx) => {
+				ctx.value = '11';
+			});
+
+			await test.expect(target.evaluate((ctx) => ctx.value)).resolves.toBe('11');
+
+			await test.expect(target.evaluate((ctx) => ctx.formValue)).resolves.toBe(11);
+		});
 	});
 
-	test('multiple', async ({page}) => {
-		const target = await renderSelect(page, {
-			value: '10',
-			disallow: ['10', '11']
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('10');
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBeUndefined();
-
-		await target.evaluate((ctx) => {
-			ctx.value = '11';
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('11');
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBeUndefined();
-
-		await target.evaluate((ctx) => {
-			ctx.value = '12';
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('12');
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBe(12);
-	});
-
-	test('RegExp', async ({page}) => {
-		const target = await renderSelect(page, {
-			value: '10',
-			disallow: /^1/
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('10');
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBeUndefined();
-
-		await target.evaluate((ctx) => {
-			ctx.value = '11';
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('11');
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBeUndefined();
-
-		await target.evaluate((ctx) => {
-			ctx.value = '5';
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('5');
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBe(5);
-	});
-
-	test('Function', async ({page}) => {
-		const target = await renderSelect(page, {
-			value: '10',
-			disallow: (v) => v === '10'
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('10');
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBeUndefined();
-
-		await target.evaluate((ctx) => {
-			ctx.value = '11';
-		});
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.value)
-		).toBe('11');
-
-		test.expect(
-			await target.evaluate((ctx) => ctx.formValue)
-		).toBe(11);
-	});
-
+	/**
+ 	 * Returns rendered `b-select` component with the `formValueConverter`
+   *
+	 * @param page
+	 * @param attrs
+	 */
 	async function renderSelect(page: Page, attrs: RenderComponentsVnodeParams['attrs'] = {}): Promise<JSHandle<bSelect>> {
 		return Component.createComponent(page, 'b-select', {
 			'data-id': 'target',
