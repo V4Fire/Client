8a468c59d88cf73db54314f56624f3dee22210fe
Artem Shinkaruk <artasend@gmail.com>
2023-04-27 22:08:24 +0700

    chore(b-bottom-slide): create unsafe interface

--- src/components/base/b-bottom-slide/interface.ts
+++ src/components/base/b-bottom-slide/interface.ts
@@ -6,5 +6,19 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
+import type bBottomSlide from 'components/base/b-bottom-slide/b-bottom-slide';
+import type { UnsafeIBlock } from 'components/base/b-bottom-slide/b-bottom-slide';
+
 export type HeightMode = 'content' | 'full';
 export type Direction = -1 | 0 | 1;
+
+export interface UnsafeBBottomSlide<CTX extends bBottomSlide = bBottomSlide> extends UnsafeIBlock<CTX> {
+	// @ts-ignore (access)
+	browser: CTX['browser'];
+
+	// @ts-ignore (access)
+	diff: CTX['diff'];
+
+	// @ts-ignore (access)
+	updateKeyframeValues: CTX['updateKeyframeValues'];
+}
