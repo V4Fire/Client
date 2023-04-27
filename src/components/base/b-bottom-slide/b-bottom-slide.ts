511355e13c83d5baffd7513228f0692671b6b063
Artem Shinkaruk <artasend@gmail.com>
2023-04-27 21:20:32 +0700

    refactor(b-bottom-slide): create props class

--- src/components/base/b-bottom-slide/b-bottom-slide.ts
+++ src/components/base/b-bottom-slide/b-bottom-slide.ts
@@ -30,7 +30,6 @@ import Block, { getFullElementName } from 'components/friends/block';
 import iBlock, {
 
 	component,
-	prop,
 	field,
 	system,
 	computed,
@@ -43,8 +42,8 @@ import iBlock, {
 
 } from 'components/super/i-block/i-block';
 
-import { heightMode } from 'components/base/b-bottom-slide/const';
-import type { HeightMode, Direction } from 'components/base/b-bottom-slide/interface';
+import type { Direction } from 'components/base/b-bottom-slide/interface';
+import bBottomSlideProps from 'components/base/b-bottom-slide/props';
 
 export * from 'components/super/i-data/i-data';
 
@@ -67,89 +66,10 @@ interface bBottomSlide extends
  */
 @component()
 @derive(iLockPageScroll, iObserveDOM, iOpen)
-class bBottomSlide extends iBlock implements iLockPageScroll, iObserveDOM, iOpen, iVisible, iHistory {
-	/** @see [[iVisible.prototype.hideIfOffline]] */
-	@prop(Boolean)
-	readonly hideIfOffline: boolean = false;
-
-	/**
-	 * Component height mode:
-	 *
-	 * 1. `content` – the height value is based on a component content, but no more than the viewport height
-	 * 2. `full` – the height value is equal to the viewport height
-	 */
-	@prop({type: String, validator: Object.hasOwnProperty(heightMode)})
-	readonly heightMode: HeightMode = 'full';
-
-	/**
-	 * List of allowed component positions relative to the screen height (in percentages)
-	 */
-	@prop({type: Array, validator: (v: number[]) => v.every((a) => a >= 0 && a <= 100)})
-	readonly stepsProp: number[] = [];
-
+class bBottomSlide extends bBottomSlideProps implements iLockPageScroll, iObserveDOM, iOpen, iVisible, iHistory {
 	/** @see [[bBottomSlide.steps]] */
 	@field<bBottomSlide>((o) => o.sync.link('stepsProp', (v: number[]) => v.slice().sort((a, b) => a - b)))
 	readonly stepsStore!: number[];
-
-	/**
-	 * The minimum height value of a visible part (in pixels), i.e.,
-	 * even the component is closed, this part still be visible
-	 */
-	// eslint-disable-next-line @typescript-eslint/unbound-method
-	@prop({type: Number, validator: Number.isNonNegative})
-	readonly visible: number = 0;
-
-	/**
-	 * The maximum height value to which you can pull the component
-	 */
-	@prop({type: Number, validator: (v: number) => v >= 0 && v <= 100})
-	readonly maxVisiblePercent: number = 90;
-
-	/**
-	 * The maximum time in milliseconds after which we can assume that there was a quick swipe
-	 */
-	// eslint-disable-next-line @typescript-eslint/unbound-method
-	@prop({type: Number, validator: Number.isPositive})
-	readonly fastSwipeDelay: number = (0.3).seconds();
-
-	/**
-	 * The minimum required amount of pixels of scrolling after which we can assume that there was a quick swipe
-	 */
-	// eslint-disable-next-line @typescript-eslint/unbound-method
-	@prop({type: Number, validator: Number.isNatural})
-	readonly fastSwipeThreshold: number = 10;
-
-	/**
-	 * The minimum required amount of pixels of scrolling to swipe
-	 */
-	// eslint-disable-next-line @typescript-eslint/unbound-method
-	@prop({type: Number, validator: Number.isNatural})
-	readonly swipeThreshold: number = 40;
-
-	/**
-	 * If true, the component will overlay background while it's opened
-	 */
-	@prop(Boolean)
-	readonly overlay: boolean = true;
-
-	/**
-	 * The maximum value of overlay opacity
-	 */
-	// eslint-disable-next-line @typescript-eslint/unbound-method
-	@prop({type: Number, validator: Number.isBetweenZeroAndOne})
-	readonly maxOpacity: number = 0.8;
-
-	/**
-	 * If true, then the content scroll will be automatically reset to the top after closing the component
-	 */
-	@prop(Boolean)
-	readonly scrollToTopOnClose: boolean = true;
-
-	/**
-	 * If false, the inner content of the component won't be rendered if the component isn't opened
-	 */
-	@prop(Boolean)
-	readonly forceInnerRender: boolean = true;
 
 	/**
 	 * True if the content is fully opened
