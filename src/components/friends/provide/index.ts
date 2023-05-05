3db4964ab1f1eec6d367f108b45527bc0b937775
Artem Shinkaruk <artasend@gmail.com>
2023-05-05 22:49:22 +0700

    test(friends/provide): add dynamic import for test env

--- src/components/friends/provide/index.ts
+++ src/components/friends/provide/index.ts
@@ -15,3 +15,7 @@ export { default } from 'components/friends/provide/class';
 
 export * from 'components/friends/provide/api';
 export * from 'components/friends/provide/interface';
+
+//#if runtime has prelude/test-env
+import('components/friends/provide/test/b-friends-provide-dummy');
+//#endif
