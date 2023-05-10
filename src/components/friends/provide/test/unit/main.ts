ce94da97427f3e80784d64ea04e70a516bdee217
Artem Shinkaruk <artasend@gmail.com>
2023-05-10 17:59:19 +0700

    test(friend/provide): improve test descriptions

--- src/components/friends/provide/test/unit/main.ts
+++ src/components/friends/provide/test/unit/main.ts
@@ -37,12 +37,12 @@ test.describe('friends/provide', () => {
 	});
 
 	test.describe('`fullComponentName`', () => {
-		test('should return current component name', async () => {
+		test('should return the current component name', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.fullComponentName()))
 				.resolves.toBe(componentName);
 		});
 
-		test('should return current component name concatenated with the specified modifier and it\'s value', async () => {
+		test('should return the current component name concatenated with the specified modifier and its value', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.fullComponentName('opened', true)))
 				.resolves.toBe(prefix`_opened_true`);
 		});
@@ -54,7 +54,7 @@ test.describe('friends/provide', () => {
 
 		test([
 			'should return the specified component name concatenated',
-			'with the specified modifier and it\'s value'
+			'with the specified modifier and its value'
 		].join(' '), async () => {
 			await test.expect(target.evaluate(({provide}) => provide.fullComponentName('b-foo', 'opened', true)))
 				.resolves.toBe('b-foo_opened_true');
@@ -62,14 +62,14 @@ test.describe('friends/provide', () => {
 	});
 
 	test.describe('`fullElementName`', () => {
-		test('should return current component name concatenated with the specified element name', async () => {
+		test('should return the current component name concatenated with the specified element name', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.fullElementName('foo')))
 				.resolves.toBe(prefix`__foo`);
 		});
 
 		test([
-			'should return current component name concatenated with the specified element name,',
-			'the specified modifier and it\'s value'
+			'should return the current component name concatenated with the specified element name,',
+			'the specified modifier and its value'
 		].join(' '), async () => {
 			await test.expect(target.evaluate(({provide}) => provide.fullElementName('foo', 'opened', true)))
 				.resolves.toBe(prefix`__foo_opened_true`);
@@ -82,7 +82,7 @@ test.describe('friends/provide', () => {
 
 		test([
 			'should return the specified component name concatenated with the specified element name,',
-			'the specified modifier and it\'s value'
+			'the specified modifier and its value'
 		].join(' '), async () => {
 			await test.expect(target.evaluate(({provide}) => provide.fullElementName('b-foo', 'foo', 'opened', true)))
 				.resolves.toBe('b-foo__foo_opened_true');
@@ -90,98 +90,115 @@ test.describe('friends/provide', () => {
 	});
 
 	test.describe('`mods`', () => {
-		test('should return dictionary of active modifiers and their values', async () => {
+		test('should return a dictionary of active modifiers and their values', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.mods()))
 				.resolves.toEqual({foo: 'bar'});
 		});
 
-		test('should return dictionary of active and provided modifiers and their values', async () => {
+		test('should return a dictionary of active and provided modifiers and their values', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.mods({baz: 'bla'})))
 				.resolves.toEqual({foo: 'bar', baz: 'bla'});
 		});
 	});
 
-	test.describe('`classes`', () => {
-		test('simple usage', async () => {
+	test.describe('`classes` should return a dictionary', () => {
+		test('with the specified element\'s class name', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.classes({button: true})))
 				.resolves.toEqual({button: prefix`__button`});
+		});
 
+		test('with the overridden element\'s class name', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.classes({button: 'submit'})))
 				.resolves.toEqual({button: prefix`__submit`});
 		});
 
-		test('providing additional modifiers', async () => {
-			await test.expect(target.evaluate(({provide}) => provide.classes({button: ['submit', 'focused', 'true']}))).resolves
-				.toEqual({button: prefix`__submit_focused_true`});
+		test('with the overridden element\'s class name concatenated with the provided modifiers', async () => {
+			await test.expect(target.evaluate(({provide}) => provide.classes({button: ['submit', 'focused', 'true']})))
+				.resolves.toEqual({button: prefix`__submit_focused_true`});
 		});
 
-		test('providing a component name', async () => {
+		test('with the specified component name concatenated with the specified element', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.classes('b-foo', {button: true})))
 				.resolves.toEqual({button: 'b-foo__button'});
+		});
 
+		test([
+			'with the class name consisting of the specified component\'s name',
+			'and overridden name of the specified element'
+		].join(' '), async () => {
 			await test.expect(target.evaluate(({provide}) => provide.classes('b-foo', {button: 'submit'})))
 				.resolves.toEqual({button: 'b-foo__submit'});
 		});
 
-		test('providing a component name and additional modifiers', async () => {
+		test([
+			'with the class name consisting of the specified component\'s name,',
+			'overridden name of the specified element and the provided modifiers'
+		].join(' '), async () => {
 			await test.expect(target.evaluate(({provide}) => provide.classes('b-foo', {button: ['submit', 'focused', 'true']})))
 				.resolves.toEqual({button: 'b-foo__submit_focused_true'});
 		});
 	});
 
 	test.describe('`componentClasses`', () => {
-		test('simple usage', async () => {
+		test('should return an array consisting of one element, which is a component name', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.componentClasses()))
 				.resolves.toEqual([componentName]);
 		});
 
-		test('providing additional modifiers', async () => {
+		test('should return an array consisting of the component name and the names generated from the provided modifiers', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.componentClasses({opened: true, selected: false})))
 				.resolves.toEqual([componentName, prefix`_opened_true`, prefix`_selected_false`]);
 		});
 
-		test('providing a component name', async () => {
+		test('should return an array consisting of one element, which is provided as an argument', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.componentClasses('b-foo')))
 				.resolves.toEqual(['b-foo']);
 		});
 
-		test('providing a component name and additional modifiers', async () => {
+		test('should return an array consisting of the provided component name and names generated from the provided modifiers', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.componentClasses('b-foo', {opened: true, selected: false})))
 				.resolves.toEqual(['b-foo', 'b-foo_opened_true', 'b-foo_selected_false']);
 		});
 	});
 
-	test.describe('`elClasses`', () => {
-		test('simple usage', async () => {
+	test.describe('`elementClasses`', () => {
+		test([
+			'should return an array consisting of the component ID,',
+			'the provided element class name, and the provided modifier classes'
+		].join(' '), async () => {
 			const id = await target.evaluate(({componentId}) => componentId);
 
 			await test.expect(target.evaluate(({provide}) => provide.elementClasses({button: {focused: true}})))
 				.resolves.toEqual([id, prefix`__button`, prefix`__button_focused_true`]);
 		});
 
-		test('providing a component name', async () => {
+		test([
+			'should return an array consisting of the element class name with the provided component name',
+			'and class names with the provided modifier classes'
+		].join(' '), async () => {
 			await test.expect(target.evaluate(({provide}) => provide.elementClasses('b-foo', {button: {focused: true}})))
 				.resolves.toEqual(['b-foo__button', 'b-foo__button_focused_true']);
 		});
 
-		test('providing a component context', async () => {
+		test('should return element class names according to the provided context', async () => {
 			const ctx = {
 				componentId: 'baz',
 				componentName: 'b-foo'
 			};
 
-			await test.expect(target.evaluate(({provide}, ctx) => provide.elementClasses(ctx, {button: {focused: true}}), ctx))
-				.resolves.toEqual(['baz', 'b-foo__button', 'b-foo__button_focused_true']);
+			await test.expect(
+				target.evaluate(({provide}, ctx) => provide.elementClasses(Object.cast(ctx), {button: {focused: true}}), ctx)
+			).resolves.toEqual(['baz', 'b-foo__button', 'b-foo__button_focused_true']);
 		});
 	});
 
 	test.describe('`hintClasses`', () => {
-		test('simple usage', async () => {
+		test('should return hint classes with the default position', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.hintClasses()))
 				.resolves.toEqual(['g-hint', 'g-hint_pos_bottom']);
 		});
 
-		test('providing position', async () => {
+		test('should return hint classes with the provided position', async () => {
 			await test.expect(target.evaluate(({provide}) => provide.hintClasses('top')))
 				.resolves.toEqual(['g-hint', 'g-hint_pos_top']);
 		});
