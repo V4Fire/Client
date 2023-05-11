c9c2421d971a33a7f1da95eb981d332f782722d6
Artem Shinkaruk <artasend@gmail.com>
2023-05-11 18:25:17 +0700

    test(friends/sync): improve sync-links runner tests

--- src/components/friends/sync/test/unit/sync-links.ts
+++ src/components/friends/sync/test/unit/sync-links.ts
@@ -26,124 +26,144 @@ test.describe('friends/sync `syncLinks`', () => {
 		target = await Component.createComponent(page, componentName);
 	});
 
-	test('global synchronization', async () => {
+	test('should sync all links without arguments', async () => {
 		const scan = await target.evaluate((ctx) => {
-			const res = [
-				Object.fastClone(ctx.dict),
-				Object.fastClone(ctx.sync.link(['bla', 'dict'], {flush: 'sync'}))
-			];
+			const res: Dictionary = {
+				initial: [
+					Object.fastClone(ctx.dict),
+					Object.fastClone(ctx.sync.link(['bla', 'dict'], {flush: 'sync'})),
+					Object.fastClone(ctx.sync.link(['bar', 'dict'], {flush: 'sync'}))
+				]
+			};
+
+			const values = () => [Object.fastClone(ctx.bla), Object.fastClone(ctx.bar)];
 
 			ctx.dict.a!.b!++;
-			res.push(Object.fastClone(ctx.bla));
+			res.synced = values();
 
 			ctx.bla = {c: 1};
-			res.push(Object.fastClone(ctx.bla));
+			ctx.bar = {d: 1};
+			res.modified = values();
 
 			ctx.sync.syncLinks();
-			res.push(Object.fastClone(ctx.bla));
+			res.reset = values();
 
 			return res;
 		});
 
-		test.expect(scan).toEqual([
-			{a: {b: 2, c: 3}},
-			{a: {b: 2, c: 3}},
-			{a: {b: 3, c: 3}},
-			{c: 1},
-			{a: {b: 3, c: 3}}
-		]);
+		test.expect(scan).toEqual({
+			initial: [{a: {b: 2, c: 3}}, {a: {b: 2, c: 3}}, {a: {b: 2, c: 3}}],
+			synced: [{a: {b: 3, c: 3}}, {a: {b: 3, c: 3}}],
+			modified: [{c: 1}, {d: 1}],
+			reset: [{a: {b: 3, c: 3}}, {a: {b: 3, c: 3}}]
+		});
 	});
 
-	test('synchronization by a main name', async () => {
+	test('should sync links of the specified source when source name is passed as an argument', async () => {
 		const scan = await target.evaluate((ctx) => {
-			const res = [
-				Object.fastClone(ctx.dict),
-				Object.fastClone(ctx.sync.link(['bla', 'dict'], {flush: 'sync'}))
-			];
+			const res: Dictionary = {
+				initial: [
+					Object.fastClone(ctx.dict),
+					Object.fastClone(ctx.sync.link(['bla', 'dict'], {flush: 'sync'})),
+					Object.fastClone(ctx.sync.link(['bar', 'foo'], {flush: 'sync'}))
+				]
+			};
+
+			const values = () => [Object.fastClone(ctx.bla), Object.fastClone(ctx.bar)];
 
 			ctx.dict.a!.b!++;
-			res.push(Object.fastClone(ctx.bla));
+			ctx.foo = 1;
+			res.synced = values();
 
 			ctx.bla = {c: 1};
-			res.push(Object.fastClone(ctx.bla));
+			ctx.bar = {d: 1};
+			res.modified = values();
 
 			ctx.sync.syncLinks('dict');
-			res.push(Object.fastClone(ctx.bla));
+			res.reset = values();
 
 			return res;
 		});
 
-		test.expect(scan).toEqual([
-			{a: {b: 2, c: 3}},
-			{a: {b: 2, c: 3}},
-			{a: {b: 3, c: 3}},
-			{c: 1},
-			{a: {b: 3, c: 3}}
-		]);
+		test.expect(scan).toEqual({
+			initial: [{a: {b: 2, c: 3}}, {a: {b: 2, c: 3}}, undefined],
+			synced: [{a: {b: 3, c: 3}}, 1],
+			modified: [{c: 1}, {d: 1}],
+			reset: [{a: {b: 3, c: 3}}, {d: 1}]
+		});
 	});
 
-	test('synchronization by a link name', async () => {
+	test([
+		'should sync only specific links of the specified source',
+		'when link or/and source name is passed as an argument'
+	].join(' '), async () => {
 		const scan = await target.evaluate((ctx) => {
-			const res = [
-				Object.fastClone(ctx.dict),
-				Object.fastClone(ctx.sync.link(['bla', 'dict'], {immediate: true}))
-			];
+			const res: Dictionary = {
+				initial: [
+					Object.fastClone(ctx.dict),
+					Object.fastClone(ctx.sync.link(['bla', 'dict'], {flush: 'sync'})),
+					Object.fastClone(ctx.sync.link(['bar', 'dict'], {flush: 'sync'}))
+				]
+			};
+
+			const values = () => [Object.fastClone(ctx.bla), Object.fastClone(ctx.bar)];
 
 			ctx.dict.a!.b!++;
-			res.push(Object.fastClone(ctx.bla));
+			res.synced = values();
 
 			ctx.bla = {c: 1};
-			res.push(Object.fastClone(ctx.bla));
+			ctx.bar = {d: 1};
+			res.modified1 = values();
 
 			ctx.sync.syncLinks(['bla']);
-			res.push(Object.fastClone(ctx.bla));
+			res.reset1 = values();
 
 			ctx.bla = {c: 1};
-			res.push(Object.fastClone(ctx.bla));
+			ctx.bar = {d: 2};
+			res.modified2 = values();
 
 			ctx.sync.syncLinks(['bla', 'dict']);
-			res.push(Object.fastClone(ctx.bla));
+			res.reset2 = values();
 
 			return res;
 		});
 
-		test.expect(scan).toEqual([
-			{a: {b: 2, c: 3}},
-			{a: {b: 2, c: 3}},
-			{a: {b: 3, c: 3}},
-			{c: 1},
-			{a: {b: 3, c: 3}},
-			{c: 1},
-			{a: {b: 3, c: 3}}
-		]);
+		test.expect(scan).toEqual({
+			initial: [{a: {b: 2, c: 3}}, {a: {b: 2, c: 3}}, {a: {b: 2, c: 3}}],
+			synced: [{a: {b: 3, c: 3}}, {a: {b: 3, c: 3}}],
+			modified1: [{c: 1}, {d: 1}],
+			reset1: [{a: {b: 3, c: 3}}, {d: 1}],
+			modified2: [{c: 1}, {d: 2}],
+			reset2: [{a: {b: 3, c: 3}}, {d: 2}]
+		});
 	});
 
-	test('providing a value', async () => {
+	test('should sync links of the specified source to the provided value', async () => {
 		const scan = await target.evaluate((ctx) => {
-			const res = [
-				Object.fastClone(ctx.dict),
-				Object.fastClone(ctx.sync.link(['bla', 'dict'], {immediate: true}))
-			];
+			const res: Dictionary = {
+				initial: [
+					Object.fastClone(ctx.dict),
+					Object.fastClone(ctx.sync.link(['bla', 'dict'], {immediate: true}))
+				]
+			};
 
 			ctx.dict.a!.b!++;
-			res.push(Object.fastClone(ctx.bla));
+			res.synced = [Object.fastClone(ctx.bla)];
 
 			ctx.bla = {c: 1};
-			res.push(Object.fastClone(ctx.bla));
+			res.modified = [Object.fastClone(ctx.bla)];
 
 			ctx.sync.syncLinks('dict', {e: 1});
-			res.push(Object.fastClone(ctx.bla));
+			res.reset = [Object.fastClone(ctx.dict), Object.fastClone(ctx.bla)];
 
 			return res;
 		});
 
-		test.expect(scan).toEqual([
-			{a: {b: 2, c: 3}},
-			{a: {b: 2, c: 3}},
-			{a: {b: 3, c: 3}},
-			{c: 1},
-			{e: 1}
-		]);
+		test.expect(scan).toEqual({
+			initial: [{a: {b: 2, c: 3}}, {a: {b: 2, c: 3}}],
+			synced: [{a: {b: 3, c: 3}}],
+			modified: [{c: 1}],
+			reset: [{a: {b: 3, c: 3}}, {e: 1}]
+		});
 	});
-
 });
