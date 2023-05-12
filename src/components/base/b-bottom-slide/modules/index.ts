4de8e5571143c2d9d4e80d0e6d7e89cb0b763d7b
Artem Shinkaruk <artasend@gmail.com>
2023-05-12 21:43:27 +0700

    chore(b-bottom-slide): add copyright

--- src/components/base/b-bottom-slide/modules/index.ts
+++ src/components/base/b-bottom-slide/modules/index.ts
@@ -1,3 +1,11 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
 export { default as Animation } from 'components/base/b-bottom-slide/modules/animation';
 export { default as Overlay } from 'components/base/b-bottom-slide/modules/overlay';
 export { default as Geometry } from 'components/base/b-bottom-slide/modules/geometry';
