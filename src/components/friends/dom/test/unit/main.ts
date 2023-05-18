9efba32bf3d13b16f3d482b00bf2e30e1e21dcba
Artem Shinkaruk <artasend@gmail.com>
2023-05-18 18:12:26 +0700

    test(friends/dom): refactor tests and improve descriptions

--- src/components/friends/dom/test/unit/main.ts
+++ src/components/friends/dom/test/unit/main.ts
@@ -6,17 +6,16 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import type { JSHandle } from 'playwright';
+import type { JSHandle, Page } from 'playwright';
 
 import test from 'tests/config/unit/test';
 
 import { BOM, Component } from 'tests/helpers';
 
-import type bDummy from 'components/dummies/b-dummy/b-dummy';
+import type iBlock from 'components/super/i-block/i-block';
 
 test.describe('friends/dom', () => {
-
-	let target: JSHandle<bDummy>;
+	let target: JSHandle<iBlock>;
 
 	test.beforeEach(async ({demoPage, page}) => {
 		await demoPage.goto();
@@ -27,22 +26,22 @@ test.describe('friends/dom', () => {
 		await page.evaluate(() => {
 			const el = document.createElement('div');
 
-			el.id = 'newNode';
+			el.id = 'emptyDiv';
 			Object.assign(el.style, {height: '20px', width: '20px'});
 
-			globalThis._testEl = el;
+			globalThis.emptyDiv = el;
 		});
 	});
 
 	test.describe('`getId`', () => {
-		test('`null | undefined`', async () => {
+		test('should return `undefined` for `null | undefined`', async () => {
 			const result = await target.evaluate((ctx) =>
 				[ctx.unsafe.dom.getId(undefined), ctx.unsafe.dom.getId(null)]);
 
 			test.expect(result).toEqual([undefined, undefined]);
 		});
 
-		test('someString', async () => {
+		test('should return the specified string prefixed with the component id', async () => {
 			const [id, result] = await target.evaluate((ctx) =>
 				[ctx.componentId, ctx.unsafe.dom.getId('someString')]);
 
@@ -50,168 +49,178 @@ test.describe('friends/dom', () => {
 		});
 	});
 
+	test.describe('`unwrapId`', () => {
+		test('should return the original id for the result of `getId`', async () => {
+			const value = 'someString';
+
+			const id = await target.evaluate((ctx, value) => ctx.unsafe.dom.getId(value), value);
+			await test.expect(id).toMatch(new RegExp(`-${value}$`));
+
+			const unwrappedId = await target.evaluate((ctx, id) => ctx.unsafe.dom.unwrapId(id), id);
+			await test.expect(unwrappedId).toEqual(value);
+		});
+	});
+
 	test.describe('`delegate`', () => {
-		test('fires a callback inside of `#foo`', async ({page}) => {
+		test('the callback should be fired when a click is performed inside of the specified element', async ({page}) => {
 			const clickResult = target.evaluate((ctx) => {
 				const fooEl = document.createElement('div');
 				fooEl.id = 'foo';
 
-				fooEl.appendChild(globalThis._testEl);
+				// Place an empty div inside the foo element
+				fooEl.appendChild(globalThis.emptyDiv);
 				ctx.$el!.appendChild(fooEl);
 
 				return new Promise((resolve) => {
-					globalThis._testEl.addEventListener('click', ctx.unsafe.dom.delegate('#foo', () => resolve(1)));
+					globalThis.emptyDiv.addEventListener('click', ctx.unsafe.dom.delegate('#foo', () => resolve(1)));
 				});
 			});
 
-			await page.locator('#newNode').click();
+			await page.locator('#emptyDiv').click();
 
 			await test.expect(clickResult).resolves.toBe(1);
 		});
 
-		test('does not fire a callback outside of `#foo`', async ({page}) => {
+		test('the callback should not be fired when a click is performed outside of the specified element', async ({page}) => {
 			await target.evaluate((ctx) => {
 				const fooEl = document.createElement('div');
 				fooEl.id = 'foo';
 
+				// Place an empty div near the foo element
 				ctx.$el!.appendChild(fooEl);
-				ctx.$el!.parentNode!.prepend(globalThis._testEl);
+				ctx.$el!.appendChild(globalThis.emptyDiv);
 
-				globalThis._testEl.addEventListener('click', ctx.unsafe.dom.delegate('#foo', () => globalThis._t = 1));
+				globalThis.emptyDiv.addEventListener('click', ctx.unsafe.dom.delegate('#foo', () => globalThis.testClicked = true));
 			});
 
-			await page.locator('#newNode').click();
+			await page.locator('#emptyDiv').click();
 			await BOM.waitForIdleCallback(page);
 
-			await test.expect(page.evaluate(() => globalThis._t)).resolves.toBeUndefined();
+			await test.expect(page.evaluate(() => globalThis.testClicked)).resolves.toBeUndefined();
 		});
 	});
 
 	test.describe('`renderTemporarily`', () => {
-		test('puts a new node into the document', async () => {
+		test('the specified node should be temporarily rendered in the document', async ({page}) => {
 			const isConnected = await target.evaluate((ctx) => new Promise((res) =>
-				ctx.unsafe.dom.renderTemporarily(globalThis._testEl, (el) => res(el.isConnected))));
+				ctx.unsafe.dom.renderTemporarily(globalThis.emptyDiv, (el) => res(el.isConnected))));
 
 			test.expect(isConnected).toBeTruthy();
+
+			await test.expect(page.evaluate(() => globalThis.emptyDiv.isConnected)).resolves.toBeFalsy();
 		});
 	});
 
 	test.describe('`appendChild`', () => {
-		test('appends a new node to the parent node', async () => {
+		test('should append a new node to the specified node', async () => {
 			await target.evaluate((ctx) => {
-				ctx.unsafe.dom.appendChild(ctx.$el!, globalThis._testEl);
+				ctx.unsafe.dom.appendChild(ctx.$el!, globalThis.emptyDiv);
 			});
 
-			const isIn = await target.evaluate((ctx) => globalThis._testEl.parentNode === ctx.$el);
+			const isIn = await target.evaluate((ctx) => globalThis.emptyDiv.parentNode === ctx.$el);
 
 			test.expect(isIn).toBeTruthy();
 		});
 
-		test('removes a node from the parent node on async `clear`', async ({page}) => {
+		test([
+			'the appended node should be removed from the parent node',
+			'when `async.clearAll` is invoked'
+		].join(' '), async ({page}) => {
 			await target.evaluate((ctx) => {
-				ctx.unsafe.dom.appendChild(ctx.$el!, globalThis._testEl, '_test-group');
+				ctx.unsafe.dom.appendChild(ctx.$el!, globalThis.emptyDiv, 'test-group');
 			});
 
-			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: '_test-group'}));
+			await test.expect(page.evaluate(() => globalThis.emptyDiv.isConnected))
+				.resolves.toBeTruthy();
 
-			const isConnected = await page.evaluate(() => globalThis._testEl.isConnected);
+			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: 'test-group'}));
 
-			test.expect(isConnected).toBeFalsy();
+			await test.expect(page.evaluate(() => globalThis.emptyDiv.isConnected))
+				.resolves.toBeFalsy();
 		});
 
-		test('destroys a node component on async `clear`', async ({page}) => {
-
-			const button = await Component.createComponent(page, 'b-button', {
-				attrs: {
-					id: 'button-test'
-				},
-				children: {
-					default: () => 'Hello there general kenobi'
-				}
-			});
-
-			await button.evaluate(() => {
-				globalThis._testEl = document.getElementById('button-test');
-			});
+		test([
+			'if the appended node has a component and `destroyIfComponent` option is provided,',
+			'the associated component should be destroyed when `async.clearAll` is invoked'
+		].join(' '), async ({page}) => {
+			await renderComponent(page);
 
 			await target.evaluate((ctx) => {
-				ctx.unsafe.dom.appendChild(ctx.$el!, globalThis._testEl, {group: '_test-group', destroyIfComponent: true});
+				ctx.unsafe.dom.appendChild(ctx.$el!, globalThis.testComponent, {group: 'test-group', destroyIfComponent: true});
 			});
 
-			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: '_test-group'}));
+			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: 'test-group'}));
 
-			const [isConnected, hook] = await page.evaluate(() => [
-				globalThis._testEl.isConnected,
-				'destroyed' // globalThis._testEl.component.hook
+			const [isConnected, componentIsDestroyed] = await page.evaluate(() => [
+				globalThis.testComponent.isConnected,
+				globalThis.testComponent.component === undefined
 			]);
 
 			test.expect(isConnected).toBeFalsy();
-			test.expect(hook).toBe('destroyed');
+			test.expect(componentIsDestroyed).toBeTruthy();
 		});
 	});
 
 	test.describe('`replaceWith`', () => {
 		test.beforeEach(async () => {
 			await target.evaluate((ctx) => {
-				globalThis._testEl2 = document.createElement('div');
-				ctx.$el!.appendChild(globalThis._testEl2);
+				globalThis.emptyDiv2 = document.createElement('div');
+				ctx.$el!.appendChild(globalThis.emptyDiv2);
 			});
 		});
 
-		test('replaces an old node with a new one', async ({page}) => {
+		test('should replace an old node with a new one', async ({page}) => {
 			await target.evaluate((ctx) => {
-				ctx.unsafe.dom.replaceWith(globalThis._testEl2, globalThis._testEl);
+				ctx.unsafe.dom.replaceWith(globalThis.emptyDiv2, globalThis.emptyDiv);
 			});
 
 			const [aIsConnected, bIsConnected] = await page.evaluate(() => [
-				globalThis._testEl2.isConnected,
-				globalThis._testEl.isConnected
+				globalThis.emptyDiv2.isConnected,
+				globalThis.emptyDiv.isConnected
 			]);
 
 			test.expect(aIsConnected).toBeFalsy();
 			test.expect(bIsConnected).toBeTruthy();
 		});
 
-		test('removes a node on async `clear`', async ({page}) => {
+		test('should remove the new node when `async.clearAll` is invoked', async ({page}) => {
 			await target.evaluate((ctx) => {
-				ctx.unsafe.dom.replaceWith(globalThis._testEl2, globalThis._testEl, '_test-group');
+				ctx.unsafe.dom.replaceWith(globalThis.emptyDiv2, globalThis.emptyDiv, 'test-group');
 			});
 
-			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: '_test-group'}));
+			await test.expect(page.evaluate(() => globalThis.emptyDiv.isConnected))
+				.resolves.toBeTruthy();
 
-			const isConnected = await page.evaluate(() => globalThis._testEl.isConnected);
+			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: 'test-group'}));
 
-			test.expect(isConnected).toBeFalsy();
+			const [aIsConnected, bIsConnected] = await page.evaluate(() => [
+				globalThis.emptyDiv2.isConnected,
+				globalThis.emptyDiv.isConnected
+			]);
+
+			test.expect(aIsConnected).toBeFalsy();
+			test.expect(bIsConnected).toBeFalsy();
 		});
 
-		test('destroys a node component on async `clear`', async ({page}) => {
-
-			const button = await Component.createComponent(page, 'b-button', {
-				attrs: {
-					id: 'button-test'
-				},
-				children: {
-					default: () => 'Hello there general kenobi'
-				}
-			});
-
-			await button.evaluate(() => {
-				globalThis._testEl = document.getElementById('button-test');
-			});
+		test([
+			'if the new node has a component and `destroyIfComponent` option is provided,',
+			'the associated component should be destroyed when `async.clearAll` is invoked'
+		].join(' '), async ({page}) => {
+			await renderComponent(page);
 
 			await target.evaluate((ctx) => {
-				ctx.unsafe.dom.replaceWith(globalThis._testEl2, globalThis._testEl, {group: '_test-group', destroyIfComponent: true});
+				ctx.unsafe.dom.replaceWith(globalThis.emptyDiv2, globalThis.testComponent, {group: 'test-group', destroyIfComponent: true});
 			});
 
-			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: '_test-group'}));
+			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: 'test-group'}));
 
-			const [isConnected, hook] = await page.evaluate(() => [
-				globalThis._testEl.isConnected,
-				'destroyed' // globalThis._testEl.component.hook
+			const [isConnected, componentIsDestroyed] = await page.evaluate(() => [
+				globalThis.testComponent.isConnected,
+				globalThis.testComponent.component === undefined
 			]);
 
 			test.expect(isConnected).toBeFalsy();
-			test.expect(hook).toBe('destroyed');
+			test.expect(componentIsDestroyed).toBeTruthy();
 		});
 	});
 
@@ -222,96 +231,112 @@ test.describe('friends/dom', () => {
 			targetComponentId = await target.evaluate((ctx) => ctx.componentId);
 		});
 
-		test('.b-friends-dom-dummy', async () => {
-			const foundedId = await target.evaluate((ctx) => ctx.unsafe.dom.getComponent('.b-friends-dom-dummy')!.componentId);
+		test('should find the component by a selector', async () => {
+			const componentId = await target.evaluate((ctx) =>
+				ctx.unsafe.dom.getComponent('.b-friends-dom-dummy')!.componentId);
 
-			test.expect(foundedId).toBe(targetComponentId);
+			test.expect(componentId).toBe(targetComponentId);
 		});
 
-		test('dummy component', async () => {
-			const
-				foundedId = await target.evaluate((ctx) => ctx.unsafe.dom.getComponent(ctx.$el!)!.componentId);
+		test('should find the component by a node', async () => {
+			const componentId = await target.evaluate((ctx) =>
+				ctx.unsafe.dom.getComponent(ctx.$el!)!.componentId);
 
-			test.expect(foundedId).toBe(targetComponentId);
+			test.expect(componentId).toBe(targetComponentId);
 		});
 
-		test('nested node within the dummy component', async () => {
-			const foundedId = await target.evaluate((ctx) => {
-				ctx.$el!.appendChild(globalThis._testEl);
-				return ctx.unsafe.dom.getComponent(globalThis._testEl)!.componentId;
+		test('should find the component by a nested node', async () => {
+			const componentId = await target.evaluate((ctx) => {
+				ctx.$el!.appendChild(globalThis.emptyDiv);
+				return ctx.unsafe.dom.getComponent(globalThis.emptyDiv)!.componentId;
 			});
 
-			test.expect(foundedId).toBe(targetComponentId);
+			test.expect(componentId).toBe(targetComponentId);
 		});
 
-		test('unreachable component', async () => {
-			const foundedId = await target.evaluate((ctx) => ctx.unsafe.dom.getComponent('.unreachable-selector'));
+		test('should return `null` if the component is not found', async () => {
+			const component = await target.evaluate((ctx) => ctx.unsafe.dom.getComponent('.unreachable-selector'));
 
-			test.expect(foundedId).toBeNull();
+			test.expect(component).toBeNull();
 		});
 	});
 
 	test.describe('`watchForIntersection`', () => {
 		test.beforeEach(async () => {
 			await target.evaluate((ctx) => {
-				ctx.$el!.appendChild(globalThis._testEl);
+				ctx.$el!.appendChild(globalThis.emptyDiv);
 
-				return ctx.unsafe.dom.watchForIntersection(globalThis._testEl, {
+				return ctx.unsafe.dom.watchForIntersection(globalThis.emptyDiv, {
 					threshold: 0.1,
-					delay: 300,
-					group: '_test-group'
-				}, () => globalThis._t = 1);
+					delay: 150,
+					group: 'test-group'
+				}, () => globalThis.intersectionDetected = true);
 			});
 		});
 
-		test('starts watch for intersection', async ({page}) => {
-			await test.expect(page.waitForFunction(() => globalThis._t === 1)).toBeResolved();
+		test('should detect the intersection of the node and a viewport', async ({page}) => {
+			await test.expect(page.waitForFunction(() => globalThis.intersectionDetected)).toBeResolved();
 		});
 
-		test('clears on async `clear`', async ({page}) => {
-			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: '_test-group'}));
-			await BOM.waitForIdleCallback(page, {sleepAfterIdles: 600});
+		test('should remove the intersection observer when `async.clearAll` is invoked', async ({page}) => {
+			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: 'test-group'}));
+			await BOM.waitForIdleCallback(page, {sleepAfterIdles: 300});
 
-			const testVal = await page.evaluate(() => globalThis._t);
-
-			test.expect(testVal).toBeUndefined();
+			await test.expect(page.evaluate(() => globalThis.intersectionDetected)).resolves.toBeUndefined();
 		});
 	});
 
 	test.describe('`watchForResize`', () => {
 		test.beforeEach(async () => {
 			await target.evaluate((ctx) => {
-				ctx.$el!.appendChild(globalThis._testEl);
+				ctx.$el!.appendChild(globalThis.emptyDiv);
 
-				return ctx.unsafe.dom.watchForResize(globalThis._testEl, {
-					group: '_test-group',
+				return ctx.unsafe.dom.watchForResize(globalThis.emptyDiv, {
+					group: 'test-group',
 					watchInit: false
-				}, () => globalThis._t = 1);
+				}, () => globalThis.resizeDetected = true);
 			});
 		});
 
-		test('starts watch for resizes', async ({page}) => {
+		test('should detect the resize of the node', async ({page}) => {
 			await BOM.waitForIdleCallback(page);
 
 			await page.evaluate(() => {
-				Object.assign(globalThis._testEl.style, {
+				Object.assign(globalThis.emptyDiv.style, {
 					width: '400px',
 					height: '400px'
 				});
 			});
 
-			await test.expect(page.waitForFunction(() => globalThis._t === 1)).toBeResolved();
+			await test.expect(page.waitForFunction(() => globalThis.resizeDetected)).toBeResolved();
 		});
 
-		test('clears on async `clear`', async ({page}) => {
-			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: '_test-group'}));
+		test('should remove the resize observer when `async.clearAll` is invoked', async ({page}) => {
+			await target.evaluate((ctx) => ctx.unsafe.async.clearAll({group: 'test-group'}));
 
-			await page.evaluate(() => globalThis._testEl.style.width = '400px');
+			await page.evaluate(() => globalThis.emptyDiv.style.width = '400px');
 			await BOM.waitForIdleCallback(page, {sleepAfterIdles: 300});
 
-			const testVal = await page.evaluate(() => globalThis._t);
-
-			test.expect(testVal).toBeUndefined();
+			await test.expect(page.evaluate(() => globalThis.resizeDetected)).resolves.toBeUndefined();
 		});
 	});
+
+	/**
+	 * Renders the component and creates a global link to the node
+	 * @param page
+	 */
+	async function renderComponent(page: Page): Promise<void> {
+		await Component.createComponent(page, 'b-button', {
+			attrs: {
+				id: 'testComponent'
+			},
+			children: {
+				default: () => 'Hello there, General Kenobi!'
+			}
+		});
+
+		await page.evaluate(() => {
+			globalThis.testComponent = document.getElementById('testComponent');
+		});
+	}
 });
