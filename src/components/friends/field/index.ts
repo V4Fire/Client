d43ec72e51c6cd1900ba0b604c7c482fde863c3c
Artem Shinkaruk <46344555+shining-mind@users.noreply.github.com>
2023-05-18 16:30:11 +0700

    Rewrite friends/field tests (#860)
    * docs(friends/field): improve readme
    
    * test(friends/field): create dummy component
    
    * test(friends/field): add dynamic import for dummy component
    
    * test(friends/field): rewrite tests to playwright

--- src/components/friends/field/index.ts
+++ src/components/friends/field/index.ts
@@ -15,3 +15,7 @@ export { default } from 'components/friends/field/class';
 
 export * from 'components/friends/field/api';
 export * from 'components/friends/field/interface';
+
+//#if runtime has prelude/test-env
+import('components/friends/field/test/b-friends-field-dummy');
+//#endif
