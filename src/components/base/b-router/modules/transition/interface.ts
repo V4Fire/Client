033932009a62ba8f0c2b63b6b65f0ea9fd71f4db
Artem Shinkaruk <artasend@gmail.com>
2023-05-18 23:03:14 +0700

    docs(b-router): add tsdoc for transition context

--- src/components/base/b-router/modules/transition/interface.ts
+++ src/components/base/b-router/modules/transition/interface.ts
@@ -11,7 +11,18 @@ import type { TransitionOptions } from 'core/router';
 import type { TransitionMethod } from 'components/base/b-router/interface';
 
 export interface TransitionContext {
+	/**
+	 * The route name or URL or `null`, if the route is equal to the previous
+	 */
 	ref: Nullable<string>;
+
+	/**
+	 * Additional options: query, params, meta
+	 */
 	opts?: TransitionOptions;
+
+	/**
+	 * The transition method: push, replace, etc.
+	 */
 	method: TransitionMethod;
 }
