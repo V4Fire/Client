e5b6231cc4a5caa7514c160e62ed343bcf3779c0
Artem Shinkaruk <artasend@gmail.com>
2023-05-24 23:06:54 +0700

    test(core/component/decorators): add tests for a functional component

--- src/core/component/decorators/test/unit/main.ts
+++ src/core/component/decorators/test/unit/main.ts
@@ -21,38 +21,62 @@ test.describe('core/component/decorators', () => {
 
 	// Refer to the `b-core-component-decorators-dummy` component
 	// and check it's decorators to understand this test
-	test('initial values should be correct and all their changes should be registered', async ({page}) => {
-		const target = await renderDummy(page);
+	test.describe('initial values should be correct and all their changes should be registered', () => {
+		test('when component is regular', async ({page}) => {
+			const target = await renderDummy(page);
 
-		const {i, j, tmp} = await target.evaluate((ctx) => ({
-			i: ctx.i,
-			j: ctx.j,
-			tmp: Object.fastClone(ctx.unsafe.tmp)
-		}));
+			await runTest(target);
+		});
 
-		test.expect(i).toEqual(7);
-		test.expect(j).toEqual(1);
+		test('when component is functional', async ({page}) => {
+			const target = await renderDummy(page, true);
 
-		test.expect(tmp.immediateChanges).toEqual([
-			['created', 0, 1],
-			['created', 0, 1],
-			['created', 3, 1],
-			['created', 6, 1],
-			['created', 7, 1],
-			['created', 7, 1],
-			['mounted', 7, 1]
-		]);
+			await runTest(target);
+		});
 
-		test.expect(tmp.changes).toEqual([
-			[7, 6, ['i']],
-			[undefined, undefined, undefined],
-			['boom!', undefined, undefined]
-		]);
+		/**
+		 * Runs a decorators test for the specified target
+		 * @param target
+		 */
+		async function runTest(target: JSHandle<bCoreComponentDecoratorsDummy>) {
+			const {i, j, tmp} = await target.evaluate((ctx) => ({
+				i: ctx.i,
+				j: ctx.j,
+				tmp: Object.fastClone(ctx.unsafe.tmp)
+			}));
+
+			test.expect(i).toEqual(7);
+			test.expect(j).toEqual(1);
+
+			test.expect(tmp.immediateChanges).toEqual([
+				['created', 0, 1],
+				['created', 0, 1],
+				['created', 3, 1],
+				['created', 6, 1],
+				['created', 7, 1],
+				['created', 7, 1],
+				['mounted', 7, 1]
+			]);
+
+			test.expect(tmp.changes).toEqual([
+				[7, 6, ['i']],
+				[undefined, undefined, undefined],
+				['boom!', undefined, undefined]
+			]);
+		}
 	});
 
+	/**
+	 * Returns the rendered dummy component. It can be a regular or a functional component.s
+	 *
+	 * @param page
+	 * @param functional
+	 */
 	async function renderDummy(
-		page: Page, attrs: RenderComponentsVnodeParams['attrs'] = {}
+		page: Page, functional: boolean = false
 	): Promise<JSHandle<bCoreComponentDecoratorsDummy>> {
-		return Component.createComponent(page, 'b-core-component-decorators-dummy', attrs);
+		return Component.createComponent(
+			page, `b-core-component-decorators-dummy${functional ? '-functional' : ''}`
+		);
 	}
 });
