6246aa36bbcd78ffef0e444b5e81043951d1f7e2
Artem Shinkaruk <artasend@gmail.com>
2023-05-25 22:23:53 +0700

    docs(super/i-block): disable jsdoc rule for watch dummy component

--- src/components/super/i-block/test/b-super-i-block-watch-dummy/b-super-i-block-watch-dummy.ts
+++ src/components/super/i-block/test/b-super-i-block-watch-dummy/b-super-i-block-watch-dummy.ts
@@ -6,6 +6,8 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
+/* eslint-disable jsdoc/require-jsdoc */
+
 import watch from 'core/object/watch';
 import iData, { component, field, system, computed, ModsDecl } from 'components/super/i-data/i-data';
 
