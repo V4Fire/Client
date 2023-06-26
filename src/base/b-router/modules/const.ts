1741f103f243fc32cfa419b68917075beb88afc2
geopr <prohorovgosa@gmail.com>
2023-06-26 08:29:09 +0300

    style: add copyright and rename constant

--- src/base/b-router/modules/const.ts
+++ src/base/b-router/modules/const.ts
@@ -1,7 +1,15 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
 /**
  * URI schemes that will be hanlded by the environment not the router
  */
-export const URISchemes = [
+export const environmentURISchemes = [
 	/^#/,
 	/^javascript:/,
 	/^mailto:/,
