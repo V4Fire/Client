e8380d652b304d679ca1af45402f7245f887388a
Mikhail Kormanowsky <i@kormanowsky.ru>
2023-06-26 22:26:06 +0300

    fix: linter error

--- src/components/directives/bind-with/test/interface.ts
+++ src/components/directives/bind-with/test/interface.ts
@@ -1,3 +1,11 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
 /**
  * A call to v-bind-with's .then() or .catch()
  */
