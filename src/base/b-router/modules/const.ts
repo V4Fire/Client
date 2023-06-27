b41a1415415dc80503165deddb54d0ab5fa08a79
geopr <prohorovgosa@gmail.com>
2023-06-27 13:28:35 +0300

    fix: eslint

--- src/base/b-router/modules/const.ts
+++ src/base/b-router/modules/const.ts
@@ -7,7 +7,7 @@
  */
 
 /**
- * A list of URL patterns that will not be intercepted by the router upon clicking. 
+ * A list of URL patterns that will not be intercepted by the router upon clicking.
  * For instance, navigating to an external resource or using anchor-based navigation within a page.
  */
 export const urlsToIgnore = [
