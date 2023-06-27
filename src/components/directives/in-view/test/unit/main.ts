8d1604ac9c3b5b3ebdc8888c61d8c9c3d9ed2a1d
Mikhail Kormanowsky <i@kormanowsky.ru>
2023-06-27 23:13:51 +0300

    add: test placeholders for v-in-view

---
+++ src/components/directives/in-view/test/unit/main.ts
@@ -0,0 +1,24 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import test from 'tests/config/unit/test';
+
+describe('<div> v-in-view', () => {
+	test('handler should be called when element becomes visible', async () => {
+		test.expect(true).toBe(true);
+	});
+
+	test('handler should be called only once, if `once` option is set', async () => {
+		test.expect(true).toBe(true);
+	});
+
+	test('all handlers should be called, if there are many', async () => {
+		test.expect(true).toBe(true);
+	});
+
+});
