2a451e9aa5a49dd86368b07361a1467209f8a630
Mikhail Kormanowsky <i@kormanowsky.ru>
2023-06-29 13:54:54 +0300

    style: empty lines for convenience and fix text errors

--- src/components/directives/image/test/unit/main.ts
+++ src/components/directives/image/test/unit/main.ts
@@ -30,22 +30,28 @@ test.describe('components/directives/image', () => {
 
 	test('the provided main image should be shown when loaded successfully', async ({page}) => {
 		const {divLocator} = await createDivForTest(page, {src: EXISTING_PICTURE_SRC});
+
 		await waitForImageLoad(page, divLocator);
+
 		const {span, img} = await getImageTestData(divLocator);
+
 		test.expect(span.dataImage).toBe('preview');
 		test.expect(span.style).toBeNull();
+
 		test.expect(img!.dataImg).toBe('loaded');
 		test.expect(img!.style).toBeNull();
 		test.expect(img!.src).toBe(EXISTING_PICTURE_SRC);
 	});
 
-	test('the provided prefix should be used as prefix for `src` attribute', async ({page}) => {
+	test('the provided prefix should be used as a prefix for `src` attribute', async ({page}) => {
 		const {divLocator} = await createDivForTest(page, {src: 'test.png', baseSrc: 'http://127.0.0.1:1234'});
+
 		const {img} = await getImageTestData(divLocator);
+
 		test.expect(img!.src).toBe('http://127.0.0.1:1234/test.png');
 	});
 
-	test('the `srcset` attribute of img should be set if provided', async ({page}) => {
+	test('the `srcset` attribute should be set', async ({page}) => {
 		const srcset = {
 			'1x': EXISTING_PICTURE_SRC,
 			'2x': BROKEN_PICTURE_SRC
@@ -57,26 +63,29 @@ test.describe('components/directives/image', () => {
 
 		const {img} = await getImageTestData(divLocator);
 		const expectedSrcset = Object.entries(srcset).map(([k, v]) => `${v} ${k}`).join(', ');
+
 		test.expect(img!.srcset).toBe(expectedSrcset);
 	});
 
-	test('the `width` and the `height` attributes should be set to provided values', async ({page}) => {
+	test('the `width` and the `height` attributes should be set', async ({page}) => {
 		const {divLocator} = await createDivForTest(page, {
 			width: 100,
 			height: 50
 		});
 
 		const {img} = await getImageTestData(divLocator);
+
 		test.expect(img!.width).toBe(100);
 		test.expect(img!.height).toBe(50);
 	});
 
-	test('the `sizes` attribute should be set to provided value', async ({page}) => {
+	test('the `sizes` attribute should be set', async ({page}) => {
 		const {divLocator} = await createDivForTest(page, {
 			sizes: '20px'
 		});
 
 		const {img} = await getImageTestData(divLocator);
+
 		test.expect(img!.sizes).toBe('20px');
 	});
 
@@ -97,12 +106,13 @@ test.describe('components/directives/image', () => {
 
 	});
 
-	test('the `alt` attribute should be set to provided value', async ({page}) => {
+	test('the `alt` attribute should be set', async ({page}) => {
 		const {divLocator} = await createDivForTest(page, {
 			alt: 'Alt text'
 		});
 
 		const {img} = await getImageTestData(divLocator);
+
 		test.expect(img!.alt).toBe('Alt text');
 	});
 
@@ -115,7 +125,9 @@ test.describe('components/directives/image', () => {
 		});
 
 		await waitForImageLoad(page, divLocator);
+
 		const {img} = await getImageTestData(divLocator);
+
 		test.expect(img!.dataImg).toBe('loaded');
 	});
 
@@ -131,9 +143,12 @@ test.describe('components/directives/image', () => {
 		});
 
 		const {divLocator} = await createDivForTest(page, {src: SLOW_LOAD_PICTURE_SRC, preview: EXISTING_PICTURE_SRC});
+
 		const {span, img} = await getImageTestData(divLocator);
+
 		test.expect(span.dataImage).toBe('preview');
 		test.expect(span.style?.startsWith(`background-image: url("${EXISTING_PICTURE_SRC}");`)).toBe(true);
+
 		test.expect(img!.style).toBe('opacity: 0;');
 	});
 
@@ -144,9 +159,12 @@ test.describe('components/directives/image', () => {
 		});
 
 		await waitForImageLoadFail(page, divLocator);
+
 		const {span, img} = await getImageTestData(divLocator);
+
 		test.expect(span.dataImage).toBe('broken');
 		test.expect(span.style).toBe(`background-image: url("${EXISTING_PICTURE_SRC}");`);
+
 		test.expect(img!.dataImg).toBe('failed');
 		test.expect(img!.style).toBe('opacity: 0;');
 	});
@@ -161,6 +179,7 @@ test.describe('components/directives/image', () => {
 		});
 
 		await waitForAttribute(page, imgLocator, 'data-on-load-called');
+
 		await test.expect(imgLocator.getAttribute('data-on-load-called')).toBeResolvedTo('1');
 	});
 
@@ -174,6 +193,7 @@ test.describe('components/directives/image', () => {
 		});
 
 		await waitForAttribute(page, imgLocator, 'data-on-error-called');
+
 		await test.expect(imgLocator.getAttribute('data-on-error-called')).toBeResolvedTo('1');
 	});
 
@@ -184,6 +204,7 @@ test.describe('components/directives/image', () => {
 		});
 
 		const {img} = await getImageTestData(divLocator);
+
 		test.expect(img!.src!.endsWith('resolver-called')).toBe(true);
 	});
 
