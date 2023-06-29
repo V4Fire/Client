0a900a0884d86b16277cd0bcb98c8dffb4e9eee3
bonkalol <bonkalol@list.ru>
2023-06-29 20:06:08 +0300

    Rollback reusable engine

--- src/components/base/b-scrolly/modules/factory/engines/reusable.ts
+++ src/components/base/b-scrolly/modules/factory/engines/reusable.ts
@@ -6,45 +6,18 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import Vue from 'vue';
 import type bScrolly from 'components/base/b-scrolly/b-scrolly';
 import type { ComponentItem } from 'components/base/b-scrolly/interface';
 import type { VNode } from 'components/base/b-scrolly/b-scrolly';
 
 export class ReusableRenderEngine {
-	/**
-	 * Движок который выполняет отрисовку узлов
-	 */
-	protected vueInstance?: Vue;
-
 	protected vNodesToRender?: VNode[];
 
 	reset(): void {
-		this.vueInstance?.$destroy();
+		// ...
 	}
 
-	render(ctx: bScrolly, items: ComponentItem[]): HTMLElement[] {
-		 this.vNodesToRender = items.map((item) => ctx.unsafe.$createElement(item.item, {
-			attrs: {
-				'v-attrs': item.props
-			}
-		}));
-
-		if (this.vueInstance) {
-			this.vueInstance.$forceUpdate();
-		}
-
-		this.vueInstance = new Vue({
-			render: (c) => c('div', this.vNodesToRender)
-		});
-
-		Object.set(this.vueInstance, '$root', Object.create(ctx.$root));
-		Object.set(this.vueInstance, '$root.$remoteParent', ctx);
-		Object.set(this.vueInstance, '$root.unsafe', this.vueInstance.$root);
-
-		const el = document.createElement('div');
-		this.vueInstance.$mount(el);
-
-		return <HTMLElement[]>Array.from(el.firstChild?.firstChild?.childNodes ?? []);
+	render(_ctx: bScrolly, _items: ComponentItem[]): HTMLElement[] {
+		return Object.cast({});
 	}
 }
