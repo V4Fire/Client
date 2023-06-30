0b6cdc749368036740c6b0f5ae3827d4ac9edcd8
kobezzza <kobezzza@gmail.com>
2023-06-30 18:27:11 +0300

    chore: better doc

--- src/components/directives/bind-with/test/unit/main.ts
+++ src/components/directives/bind-with/test/unit/main.ts
@@ -12,9 +12,7 @@ import test from 'tests/config/unit/test';
 import { Component } from 'tests/helpers';
 
 import type iBlock from 'components/super/i-block/i-block';
-
 import type { Listener } from 'components/directives/bind-with';
-import type { BindWithTestInfo } from 'components/directives/bind-with/test/interface';
 
 test.describe('components/directives/bind-with', () => {
 	let rootHandle: JSHandle<iBlock>;
@@ -24,26 +22,33 @@ test.describe('components/directives/bind-with', () => {
 		rootHandle = await Component.waitForRoot(page);
 	});
 
-	test.describe('events', () => {
-		test('the handler should be executed when an event is emitted', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, {
-				on: 'testEvent'
-			});
+	test.describe('event binding', () => {
+		test(
+			'the handler should be executed when the component, within which the directive is used, fires an event',
 
-			await rootHandle.evaluate((root) => {
-				root.emit('testEvent');
-			});
+			async ({page}) => {
+				const el = await createElementWithDirective(page, {
+					on: 'testEvent'
+				});
 
-			const info = await getBindWithTestInfo(divLocator);
-			test.expect(info).toBeTruthy();
-			test.expect(info!.calls.length).toBe(1);
-		});
+				await rootHandle.evaluate((root) => {
+					root.emit('testEvent');
+				});
 
-		test('the handler should be executed when the provided emitter emits an event', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, {
+				const
+					info = await getBindWithTestInfo(el);
+
+				test.expect(info).toBeTruthy();
+				test.expect(info!.calls.length).toBe(1);
+			}
+		);
+
+		test('the handler should be executed when the provided emitter fires an event', async ({page}) => {
+			const el = await createElementWithDirective(page, {
 				emitter: (event: string, listener: AnyFunction) => {
 					document.body.addEventListener(event, listener);
 				},
+
 				on: 'testEvent'
 			});
 
@@ -51,13 +56,15 @@ test.describe('components/directives/bind-with', () => {
 				() => document.body.dispatchEvent(new Event('testEvent'))
 			);
 
-			const info = await getBindWithTestInfo(divLocator);
+			const
+				info = await getBindWithTestInfo(el);
+
 			test.expect(info).toBeTruthy();
 			test.expect(info!.calls.length).toBe(1);
 		});
 
-		test('the handler should be executed only once when `once` option is set', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, {
+		test('the handler should be executed only once when the `once` option is set', async ({page}) => {
+			const el = await createElementWithDirective(page, {
 				once: 'testEvent'
 			});
 
@@ -66,13 +73,15 @@ test.describe('components/directives/bind-with', () => {
 				root.emit('testEvent');
 			});
 
-			const info = await getBindWithTestInfo(divLocator);
+			const
+				info = await getBindWithTestInfo(el);
+
 			test.expect(info).toBeTruthy();
 			test.expect(info!.calls.length).toBe(1);
 		});
 
 		test('the handler should receive correct arguments', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, {
+			const el = await createElementWithDirective(page, {
 				on: 'onTestEvent'
 			});
 
@@ -80,36 +89,44 @@ test.describe('components/directives/bind-with', () => {
 				root.emit('testEvent', 1, 2, 3);
 			});
 
-			const info = await getBindWithTestInfo(divLocator);
+			const
+				info = await getBindWithTestInfo(el);
+
 			test.expect(info).toBeTruthy();
 			test.expect(info!.calls[0].args).toStrictEqual([1, 2, 3]);
 		});
 
-		test('the handler should be executed on every event when an array is passed', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, [
-				{
-					once: 'testEvent'
-				},
-				{
-					once: 'anotherTestEvent'
-				}
-			]);
+		test(
+			'the handler should be triggered when each of the specified events of the component is fired',
 
-			await rootHandle.evaluate((root) => {
-				root.emit('testEvent');
-				root.emit('anotherTestEvent');
-			});
+			async ({page}) => {
+				const el = await createElementWithDirective(page, [
+					{
+						once: 'testEvent'
+					},
 
-			const info = await getBindWithTestInfo(divLocator);
-			test.expect(info).toBeTruthy();
-			test.expect(info!.calls.length).toBe(2);
-		});
+					{
+						once: 'anotherTestEvent'
+					}
+				]);
 
+				await rootHandle.evaluate((root) => {
+					root.emit('testEvent');
+					root.emit('anotherTestEvent');
+				});
+
+				const
+					info = await getBindWithTestInfo(el);
+
+				test.expect(info).toBeTruthy();
+				test.expect(info!.calls.length).toBe(2);
+			}
+		);
 	});
 
-	test.describe('fields', () => {
-		test('the handler should be executed when a field is changed', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, {
+	test.describe("tracking changes of a component's property by a given path", () => {
+		test('the handler should be executed when a property is changed', async ({page}) => {
+			const el = await createElementWithDirective(page, {
 				path: 'testField'
 			});
 
@@ -117,59 +134,75 @@ test.describe('components/directives/bind-with', () => {
 				root.field.set('testField', 0);
 			});
 
-			const info = await getBindWithTestInfo(divLocator);
+			const
+				info = await getBindWithTestInfo(el);
+
 			test.expect(info).toBeTruthy();
 			test.expect(info!.calls.length).toBe(1);
 		});
 	});
 
-	test.describe('callbacks', () => {
+	test.describe("passing the directive's handler as a handler to another function", () => {
 		test('the handler should be executed as a callback', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, {
+			const el = await createElementWithDirective(page, {
 				callback: (handler) => [1].forEach(handler)
 			});
 
-			const info = await getBindWithTestInfo(divLocator);
+			const
+				info = await getBindWithTestInfo(el);
+
 			test.expect(info).toBeTruthy();
 			test.expect(info!.calls.length).toBe(1);
 			test.expect(info!.calls[0].args).toStrictEqual([1, 0, [1]]);
 		});
 	});
 
-	test.describe('promises', () => {
+	test.describe("passing the directive's handler as a promise handler", () => {
 		test('the handler should be executed when a promise is resolved', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, {
+			const el = await createElementWithDirective(page, {
 				promise: () => Promise.resolve()
 			});
 
-			const info = await getBindWithTestInfo(divLocator);
+			const
+				info = await getBindWithTestInfo(el);
+
 			test.expect(info).toBeTruthy();
 			test.expect(info!.calls.length).toBe(1);
 			test.expect(info!.errorCalls.length).toBe(0);
 		});
 
 		test('the error handler should be executed when a promise is rejected', async ({page}) => {
-			const divLocator = await createDivForBindWithTest(page, {
+			const el = await createElementWithDirective(page, {
 				promise: () => Promise.reject(new Error('rejection'))
 			});
 
-			const info = await getBindWithTestInfo(divLocator);
+			const
+				info = await getBindWithTestInfo(el);
+
 			test.expect(info).toBeTruthy();
 			test.expect(info!.calls.length).toBe(0);
 			test.expect(info!.errorCalls.length).toBe(1);
 		});
 	});
 
-	/**
-	 * A handler to pass as `.then()`/`.catch()` in v-bind-with
-	 *
-	 * @param element - the target element
-	 * @param args - args provided by v-bind-with trigger (on/path/callback...)
-	 */
-	function handler(element: HTMLElement, ...args: any[]) {
+	interface BindWithTestCallInfo {
+		args: unknown[];
+	}
 
+	interface BindWithTestInfo {
+		calls: BindWithTestCallInfo[];
+		errorCalls: BindWithTestCallInfo[];
+	}
+
+	/**
+	 * A handler to pass to the `bind-with` directive
+	 *
+	 * @param el - the target element
+	 * @param args - arguments provided by `bind-with` triggers (on/path/callback...)
+	 */
+	function handler(el: HTMLElement, ...args: unknown[]) {
 		const previousInfo: Partial<BindWithTestInfo> =
-			JSON.parse(element.getAttribute('data-test-bind-with') ?? '{}');
+			JSON.parse(el.getAttribute('data-test-bind-with') ?? '{}');
 
 		const newInfo: BindWithTestInfo = {
 			calls: previousInfo.calls ?? [],
@@ -177,11 +210,12 @@ test.describe('components/directives/bind-with', () => {
 		};
 
 		const preparedArgs = args.map(
-			(arg: any) => {
+			(arg) => {
 				// Avoid converting circular structure to JSON
 				try {
 					JSON.stringify(arg);
-				} catch (e) {
+
+				} catch {
 					return null;
 				}
 
@@ -189,7 +223,8 @@ test.describe('components/directives/bind-with', () => {
 			}
 		);
 
-		let callDestination: keyof BindWithTestInfo = 'calls';
+		let
+			callDestination: keyof BindWithTestInfo = 'calls';
 
 		if (args.length > 0 && args[0] instanceof Error) {
 			callDestination = 'errorCalls';
@@ -199,48 +234,44 @@ test.describe('components/directives/bind-with', () => {
 			args: preparedArgs
 		});
 
-		element.setAttribute('data-test-bind-with', JSON.stringify(newInfo));
+		el.setAttribute('data-test-bind-with', JSON.stringify(newInfo));
 	}
 
 	/**
-	 * Add our handlers to given v-bind-with listener.
-	 * @param listener - a v-bind-with listener to process
-	 */
-	function addTestHandlersToListener(listener: Partial<Listener>) {
-		return {
-			...listener,
-			then: handler,
-			catch: handler
-		};
-	}
-
-	/**
-	 * Create a `<div>` with v-bind-with set by test code.
+	 * Creates an element with the applied `bind-with` directive
 	 *
-	 * @param page - the page.
-	 * @param bindWithValue - value to pass to v-bind-with
+	 * @param page
+	 * @param bindWithOpts - options for the `bind-with` directive
 	 */
-	async function createDivForBindWithTest(
-		page: Page, bindWithValue: CanArray<Partial<Listener>>
+	async function createElementWithDirective(
+		page: Page, bindWithOpts: CanArray<Partial<Listener>>
 	): Promise<Locator> {
 		await Component.createComponent(page, 'div', {
-			'v-bind-with': Object.isArray(bindWithValue) ?
-				bindWithValue.map(addTestHandlersToListener) :
-				addTestHandlersToListener(bindWithValue),
+			'v-bind-with': Object.isArray(bindWithOpts) ?
+				bindWithOpts.map(addHandler) :
+				addHandler(bindWithOpts),
+
 			'data-testid': 'div'
 		});
 
 		return page.getByTestId('div');
+
+		function addHandler(listener: Partial<Listener>) {
+			return {
+				...listener,
+				then: handler,
+				catch: handler
+			};
+		}
 	}
 
 	/**
-	 * Get v-bind-with calls info by given locator
-	 * @param locator - the source locator
+	 * Returns information about the calls of the `bind-with` directive for a given element
+	 * @param el
 	 */
-	async function getBindWithTestInfo(
-		locator: Locator
-	): Promise<BindWithTestInfo | null> {
-		const attrValue = await locator.getAttribute('data-test-bind-with');
+	async function getBindWithTestInfo(el: Locator): Promise<BindWithTestInfo | null> {
+		const
+			attrValue = await el.getAttribute('data-test-bind-with');
 
 		if (attrValue == null) {
 			return null;
@@ -248,6 +279,5 @@ test.describe('components/directives/bind-with', () => {
 
 		return <BindWithTestInfo>JSON.parse(attrValue);
 	}
-
 });
 
