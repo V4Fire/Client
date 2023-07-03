7a751ae7f6d3561fbab06d8698c529529108e105
Artem Shinkaruk <artasend@gmail.com>
2023-07-03 14:32:24 +0700

    chore(build/monic): create function to check component styles

--- src/core/prelude/webpack.ts
+++ src/core/prelude/webpack.ts
@@ -15,3 +15,26 @@ try {
 	__webpack_nonce__ = ctx[CSP_NONCE_STORE];
 	__webpack_public_path__ = ctx.PUBLIC_PATH ?? PUBLIC_PATH;
 } catch {}
+
+/**
+ * Checks that styles for the given component are loaded
+ * @param componentName
+ */
+function checkComponentStylesAreLoaded(componentName: string): boolean {
+	try {
+		const el = document.createElement('i');
+		el.className = `${componentName}-is-style-loaded`;
+		document.body.appendChild(el);
+		const isStylesLoaded = getComputedStyle(el).color === 'rgba(0, 250, 154, 0)';
+		document.body.removeChild(el);
+		if (isStylesLoaded) {
+			return true;
+		}
+	} catch (err) {
+		stderr(err);
+	}
+
+	return false;
+}
+
+globalThis.checkComponentStylesAreLoaded = checkComponentStylesAreLoaded;
