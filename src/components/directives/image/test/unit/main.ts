f49742f9845ee6478b1eb28e39a56e212d14e1af
Mikhail Kormanovskii <i@kormanowsky.ru>
2023-07-03 17:53:58 +0300

    style: test names and ordering

--- src/components/directives/image/test/unit/main.ts
+++ src/components/directives/image/test/unit/main.ts
@@ -32,177 +32,193 @@ test.describe('components/directives/image', () => {
 		await demoPage.goto();
 	});
 
-	test('the provided main image should be shown when loaded successfully', async ({page}) => {
-		const {imageWrapper, image} = await createImageForTest(page, {src: EXISTING_PICTURE_SRC});
+	test.describe('load states', () => {
+		test('the attributes of image and wrapper should indicate successful load',
+			async ({page}) => {
+				const {imageWrapper, image} = await createImageForTest(page, {src: EXISTING_PICTURE_SRC});
 
-		await waitForImageLoad(page, imageWrapper);
+				await waitForImageLoad(page, imageWrapper);
 
-		const imageWrapperStyle = await imageWrapper.getAttribute('style');
+				const imageWrapperStyle = await imageWrapper.getAttribute('style');
 
-		test.expect(imageWrapperStyle).toBe(null);
-		await test.expect(imageWrapper.getAttribute('data-image')).toBeResolvedTo('loaded');
+				test.expect(imageWrapperStyle).toBe(null);
+				await test.expect(imageWrapper.getAttribute('data-image')).toBeResolvedTo('loaded');
 
-		await test.expect(image.getAttribute('style')).toBeResolvedTo('opacity: 1;');
-		await test.expect(image.getAttribute('data-img')).toBeResolvedTo('loaded');
-		await test.expect(image.getAttribute('src')).toBeResolvedTo(EXISTING_PICTURE_SRC);
+				await test.expect(image.getAttribute('style')).toBeResolvedTo('opacity: 1;');
+				await test.expect(image.getAttribute('data-img')).toBeResolvedTo('loaded');
+				await test.expect(image.getAttribute('src')).toBeResolvedTo(EXISTING_PICTURE_SRC);
+			});
+
+		test('the initially invisible image should be loaded when `lazy` is set to false',
+			async ({page}) => {
+				const {imageWrapper, image} = await createImageForTest(page, {
+					lazy: false,
+					src: EXISTING_PICTURE_SRC
+				}, {
+					style: 'margin-top: 200px'
+				});
+
+				await waitForImageLoad(page, imageWrapper);
+
+				await test.expect(image.getAttribute('data-img')).toBeResolvedTo('loaded');
+			});
+
+		test('the attributes of image and wrapper should indicate image preview if the image is not loaded yet',
+			async ({page, context}) => {
+				await context.route(SLOW_LOAD_PICTURE_SRC, (route) => {
+					const buffer = getPngBuffer();
+
+					setTimeout(() => route.fulfill({
+						contentType: 'image/png',
+						body: buffer
+					}), 500);
+
+				});
+
+				const {imageWrapper, image} = await createImageForTest(page, {
+					src: SLOW_LOAD_PICTURE_SRC,
+					preview: EXISTING_PICTURE_SRC
+				});
+
+				const imageWrapperStyle = await imageWrapper.getAttribute('style');
+
+				test.expect(imageWrapperStyle?.startsWith(`background-image: url("${EXISTING_PICTURE_SRC}");`))
+					.toBe(true);
+
+				await test.expect(imageWrapper.getAttribute('data-image')).toBeResolvedTo('preview');
+				await test.expect(image.getAttribute('style')).toBeResolvedTo('opacity: 0;');
+			});
+
+		test('the attributes of image and wrapper should indicate fallback image when main image failed loading',
+			async ({page}) => {
+				const {imageWrapper, image} = await createImageForTest(page, {
+					src: BROKEN_PICTURE_SRC,
+					broken: EXISTING_PICTURE_SRC
+				});
+
+				await waitForImageLoadFail(page, imageWrapper);
+
+				await test.expect(imageWrapper.getAttribute('data-image')).toBeResolvedTo('broken');
+
+				await test.expect(imageWrapper.getAttribute('style'))
+					.toBeResolvedTo(`background-image: url("${EXISTING_PICTURE_SRC}");`);
+
+				await test.expect(image.getAttribute('data-img')).toBeResolvedTo('failed');
+				await test.expect(image.getAttribute('style')).toBeResolvedTo('opacity: 0;');
+			});
+
 	});
 
-	test('the provided prefix should be used as a prefix for `src` attribute', async ({page}) => {
-		const {image} = await createImageForTest(page, {src: 'test.png', baseSrc: 'http://127.0.0.1:1234'});
+	test.describe('options as attributes', () => {
+		test('the provided `baseSrc` option should be used as a prefix for the `src` attribute of the `img` element', async ({page}) => {
+			const {image} = await createImageForTest(page, {src: 'test.png', baseSrc: 'http://127.0.0.1:1234'});
 
-		await test.expect(image.getAttribute('src')).toBeResolvedTo('http://127.0.0.1:1234/test.png');
+			await test.expect(image.getAttribute('src')).toBeResolvedTo('http://127.0.0.1:1234/test.png');
+		});
+
+		test('the provided `srcset` option should be set as an attribute of the `img` element', async ({page}) => {
+			const srcset = {
+				'1x': EXISTING_PICTURE_SRC,
+				'2x': BROKEN_PICTURE_SRC
+			};
+
+			const {image} = await createImageForTest(page, {
+				srcset
+			});
+
+			const expectedSrcset = Object.entries(srcset).map(([k, v]) => `${v} ${k}`).join(', ');
+
+			await test.expect(image.getAttribute('srcset')).toBeResolvedTo(expectedSrcset);
+		});
+
+		test('the provided `width` and `height` options should be set as attributes of the `img` element', async ({page}) => {
+			const {image} = await createImageForTest(page, {
+				width: 100,
+				height: 50
+			});
+
+			await test.expect(image.getAttribute('width')).toBeResolvedTo('100');
+			await test.expect(image.getAttribute('height')).toBeResolvedTo('50');
+		});
+
+		test('the provided `sizes` option should be set as an attribute of the `img` element', async ({page}) => {
+			const {image} = await createImageForTest(page, {
+				sizes: '20px'
+			});
+
+			await test.expect(image.getAttribute('sizes')).toBeResolvedTo('20px');
+		});
+
+		test('the provided `alt` option should be set as an attribute of the `img` element', async ({page}) => {
+			const {image} = await createImageForTest(page, {
+				alt: 'Alt text'
+			});
+
+			await test.expect(image.getAttribute('alt')).toBeResolvedTo('Alt text');
+		});
 	});
 
-	test('the `srcset` attribute should be set', async ({page}) => {
-		const srcset = {
-			'1x': EXISTING_PICTURE_SRC,
-			'2x': BROKEN_PICTURE_SRC
-		};
+	test.describe('options resolver', () => {
+		test('the return value of `optionsResolver` should be used instead of provided options', async ({page}) => {
+			const {image} = await createImageForTest(page, {
+				src: BROKEN_PICTURE_SRC,
+				optionsResolver: (opts) => ({...opts, src: `${opts.src}#resolver-called`})
+			});
 
-		const {image} = await createImageForTest(page, {
-			srcset
+			const imageSrc = await image.getAttribute('src');
+
+			test.expect(imageSrc?.endsWith('resolver-called')).toBe(true);
 		});
-
-		const expectedSrcset = Object.entries(srcset).map(([k, v]) => `${v} ${k}`).join(', ');
-
-		await test.expect(image.getAttribute('srcset')).toBeResolvedTo(expectedSrcset);
 	});
 
-	test('the `width` and the `height` attributes should be set', async ({page}) => {
-		const {image} = await createImageForTest(page, {
-			width: 100,
-			height: 50
+	test.describe('handlers', () => {
+		test('the provided `onLoad` handler should be called on successful image load', async ({page}) => {
+			const {image} = await createImageForTest(page, {
+				src: EXISTING_PICTURE_SRC,
+
+				onLoad: (el: Element) => {
+					el.setAttribute('data-on-load-called', '1');
+				}
+			});
+
+			await waitForAttribute(page, image, 'data-on-load-called');
+
+			await test.expect(image.getAttribute('data-on-load-called')).toBeResolvedTo('1');
 		});
 
-		await test.expect(image.getAttribute('width')).toBeResolvedTo('100');
-		await test.expect(image.getAttribute('height')).toBeResolvedTo('50');
+		test('the provided `onError` handler  should be called on image load error', async ({page}) => {
+			const {image} = await createImageForTest(page, {
+				src: BROKEN_PICTURE_SRC,
+
+				onError: (el: Element) => {
+					el.setAttribute('data-on-error-called', '1');
+				}
+			});
+
+			await waitForAttribute(page, image, 'data-on-error-called');
+
+			await test.expect(image.getAttribute('data-on-error-called')).toBeResolvedTo('1');
+		});
 	});
 
-	test('the `sizes` attribute should be set', async ({page}) => {
-		const {image} = await createImageForTest(page, {
-			sizes: '20px'
+	test.describe('tags', () => {
+		test('a `picture` tag should be rendered when a list of sources is provided', async ({page}) => {
+			const {picture} = await createImageForTest(page, {
+				sources: [
+					{width: 20, height: 20, srcset: EXISTING_PICTURE_SRC},
+					{width: 0, height: 0, srcset: BROKEN_PICTURE_SRC}
+				]
+			});
+
+			const sourcesLocators = await picture.locator('source').all();
+
+			await test.expect(
+				Promise.all(
+					sourcesLocators.map((locator) => locator.getAttribute('srcset'))
+				)
+			).toBeResolvedTo([EXISTING_PICTURE_SRC, BROKEN_PICTURE_SRC]);
 		});
 
-		await test.expect(image.getAttribute('sizes')).toBeResolvedTo('20px');
-	});
-
-	test('a `picture` tag should be rendered when a list of sources is provided', async ({page}) => {
-		const {picture} = await createImageForTest(page, {
-			sources: [
-				{width: 20, height: 20, srcset: EXISTING_PICTURE_SRC},
-				{width: 0, height: 0, srcset: BROKEN_PICTURE_SRC}
-			]
-		});
-
-		const sourcesLocators = await picture.locator('source').all();
-
-		await test.expect(
-			Promise.all(
-				sourcesLocators.map((locator) => locator.getAttribute('srcset'))
-			)
-		).toBeResolvedTo([EXISTING_PICTURE_SRC, BROKEN_PICTURE_SRC]);
-	});
-
-	test('the `alt` attribute should be set', async ({page}) => {
-		const {image} = await createImageForTest(page, {
-			alt: 'Alt text'
-		});
-
-		await test.expect(image.getAttribute('alt')).toBeResolvedTo('Alt text');
-	});
-
-	test('the initially invisible image should be loaded when `lazy` is set to false', async ({page}) => {
-		const {imageWrapper, image} = await createImageForTest(page, {
-			lazy: false,
-			src: EXISTING_PICTURE_SRC
-		}, {
-			style: 'margin-top: 200px'
-		});
-
-		await waitForImageLoad(page, imageWrapper);
-
-		await test.expect(image.getAttribute('data-img')).toBeResolvedTo('loaded');
-	});
-
-	test('the preview image should be shown if image is not loaded yet', async ({page, context}) => {
-		await context.route(SLOW_LOAD_PICTURE_SRC, (route) => {
-			const buffer = getPngBuffer();
-
-			setTimeout(() => route.fulfill({
-				contentType: 'image/png',
-				body: buffer
-			}), 500);
-
-		});
-
-		const {imageWrapper, image} = await createImageForTest(page, {
-			src: SLOW_LOAD_PICTURE_SRC,
-			preview: EXISTING_PICTURE_SRC
-		});
-
-		const imageWrapperStyle = await imageWrapper.getAttribute('style');
-
-		test.expect(imageWrapperStyle?.startsWith(`background-image: url("${EXISTING_PICTURE_SRC}");`))
-			.toBe(true);
-
-		await test.expect(imageWrapper.getAttribute('data-image')).toBeResolvedTo('preview');
-		await test.expect(image.getAttribute('style')).toBeResolvedTo('opacity: 0;');
-	});
-
-	test('the fallback image should be shown when main image failed loading', async ({page}) => {
-		const {imageWrapper, image} = await createImageForTest(page, {
-			src: BROKEN_PICTURE_SRC,
-			broken: EXISTING_PICTURE_SRC
-		});
-
-		await waitForImageLoadFail(page, imageWrapper);
-
-		await test.expect(imageWrapper.getAttribute('data-image')).toBeResolvedTo('broken');
-
-		await test.expect(imageWrapper.getAttribute('style'))
-			.toBeResolvedTo(`background-image: url("${EXISTING_PICTURE_SRC}");`);
-
-		await test.expect(image.getAttribute('data-img')).toBeResolvedTo('failed');
-		await test.expect(image.getAttribute('style')).toBeResolvedTo('opacity: 0;');
-	});
-
-	test('the load handler should be called on image load', async ({page}) => {
-		const {image} = await createImageForTest(page, {
-			src: EXISTING_PICTURE_SRC,
-
-			onLoad: (el: Element) => {
-				el.setAttribute('data-on-load-called', '1');
-			}
-		});
-
-		await waitForAttribute(page, image, 'data-on-load-called');
-
-		await test.expect(image.getAttribute('data-on-load-called')).toBeResolvedTo('1');
-	});
-
-	test('the error handler should be called on image load error', async ({page}) => {
-		const {image} = await createImageForTest(page, {
-			src: BROKEN_PICTURE_SRC,
-
-			onError: (el: Element) => {
-				el.setAttribute('data-on-error-called', '1');
-			}
-		});
-
-		await waitForAttribute(page, image, 'data-on-error-called');
-
-		await test.expect(image.getAttribute('data-on-error-called')).toBeResolvedTo('1');
-	});
-
-	test('the options resolver`s return value should be used for loading', async ({page}) => {
-		const {image} = await createImageForTest(page, {
-			src: BROKEN_PICTURE_SRC,
-			optionsResolver: (opts) => ({...opts, src: `${opts.src}#resolver-called`})
-		});
-
-		const imageSrc = await image.getAttribute('src');
-
-		test.expect(imageSrc?.endsWith('resolver-called')).toBe(true);
 	});
 
 	/**
