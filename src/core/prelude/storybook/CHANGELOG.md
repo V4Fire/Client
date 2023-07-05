221ab1e7b4de7f586ac8d79b4d4c737c520ac54a
Artem Shinkaruk <artasend@gmail.com>
2023-07-05 21:48:03 +0700

    feat(storybook): add prelude for storybook

---
+++ src/core/prelude/storybook/CHANGELOG.md
@@ -0,0 +1,14 @@
+Changelog
+=========
+
+> **Tags:**
+> - :boom:       [Breaking Change]
+> - :rocket:     [New Feature]
+> - :bug:        [Bug Fix]
+> - :memo:       [Documentation]
+> - :house:      [Internal]
+> - :nail_care:  [Polish]
+
+## v4.??.?? (2023-??-??)
+
+* The module has been created
---
+++ src/core/prelude/storybook/init-app.ts
@@ -0,0 +1,40 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import Component, { app, rootComponents, ComponentElement } from 'core/component';
+
+/**
+ * Inits the app for the storybook canvas
+ *
+ * @param canvasElement - storybook canvas element
+ */
+export default async function initApp(canvasElement: HTMLElement): Promise<void> {
+	// TODO: create special root component for the storybook
+	const component = await rootComponents['p-v4-components-demo'];
+
+	if (component == null) {
+		throw new ReferenceError('The root component is not found');
+	}
+
+	const getData = component.data;
+
+	component.data = function data(this: unknown): Dictionary {
+		return (Object.isFunction(getData) ? getData.call(this) : null) ?? {};
+	};
+
+	app.context = new Component({
+		...component,
+		el: canvasElement
+	});
+
+	Object.defineProperty(app, 'component', {
+		configurable: true,
+		enumerable: true,
+		get: () => document.querySelector<ComponentElement>('#root-component')?.component ?? null
+	});
+}
