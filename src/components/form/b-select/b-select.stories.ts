bd181a751b2e93b7ae408b302b3ab3e883b0c4a8
Artem Shinkaruk <artasend@gmail.com>
2023-07-06 21:33:30 +0700

    chore(storybook): add b-select stories

---
+++ src/components/form/b-select/b-select.stories.ts
@@ -0,0 +1,41 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import type { Meta } from '@v4fire/storybook';
+import type bSelect from 'components/form/b-select/b-select';
+
+// More on how to set up stories at: https://storybook.js.org/docs/html/writing-stories/introduction
+// TODO: store all props inside the component bSelect['Props']
+const config: Meta<bSelect> = {
+	title: 'Form/bSelect',
+	component: 'b-select',
+	tags: ['autodocs'],
+	argTypes: {
+		items: {
+			control: 'array'
+		},
+		value: {
+			control: 'text'
+		}
+	}
+};
+
+export default config;
+
+// More on writing stories with args: https://storybook.js.org/docs/html/writing-stories/args
+export const Default: Meta<bSelect> = {
+	args: {
+		value: '1',
+		native: false,
+		items: [
+			{value: '1', label: 'Опция 1'},
+			{value: '2', label: 'Опция 2'},
+			{value: '3', label: 'Опция 3'}
+		]
+	}
+};
