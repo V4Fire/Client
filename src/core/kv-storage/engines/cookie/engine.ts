ef6f8a7c59442c5163eef0d04ec074bb805a7b18
kobezzza <kobezzza@gmail.com>
2023-07-07 13:00:07 +0300

    refactor: support for new API

--- src/core/kv-storage/engines/cookie/engine.ts
+++ src/core/kv-storage/engines/cookie/engine.ts
@@ -11,6 +11,10 @@ import StringEngine from 'core/kv-storage/engines/string/engine';
 import * as cookie from 'core/cookies';
 
 export default class CookieEngine extends StringEngine {
+	override get serializedData(): string {
+		return cookie.get(this.cookieName) ?? '';
+	}
+
 	/**
 	 * The name of the cookie in which the data is stored
 	 */
@@ -21,6 +25,16 @@ export default class CookieEngine extends StringEngine {
 	 */
 	protected setOptions: cookie.SetOptions;
 
+	// eslint-disable-next-line @typescript-eslint/adjacent-overload-signatures
+	protected override set serializedData(value: string) {
+		if (value.length === 0) {
+			cookie.remove(this.cookieName, Object.select(this.setOptions, ['path', 'domains']));
+
+		} else {
+			cookie.set(this.cookieName, value, this.setOptions);
+		}
+	}
+
 	/**
 	 * @param cookieName - the name of the cookie in which the data is stored
 	 * @param [setOpts] - additional options for setting cookies
@@ -29,17 +43,5 @@ export default class CookieEngine extends StringEngine {
 		super();
 		this.cookieName = cookieName;
 		this.setOptions = setOpts ?? {};
-	}
-
-	protected override getRawData(): string {
-		return cookie.get(this.cookieName) ?? '';
-	}
-
-	protected override setRawData(value: string): void {
-		if (value.length === 0) {
-			cookie.remove(this.cookieName, Object.select(this.setOptions, ['path', 'domains']));
-		} else {
-			cookie.set(this.cookieName, value, this.setOptions);
-		}
 	}
 }
