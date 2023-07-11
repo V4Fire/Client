092836b279d58921a5838c2b89ad76bd5224139a
Artem Shinkaruk <artasend@gmail.com>
2023-07-11 20:10:39 +0700

    chore: add license comment

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,11 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
 /** @type { import('@v4fire/storybook-framework-webpack5').StorybookConfig } */
 const config = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
