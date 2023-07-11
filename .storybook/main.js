822635d24afc987d9925c845735762e91e0f3830
Artem Shinkaruk <artasend@gmail.com>
2023-07-11 19:18:17 +0700

    chore: fix indentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,19 @@
 /** @type { import('@v4fire/storybook-framework-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-	staticDirs: [
-		'../dist/storybook'
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
 	],
-  framework: {
-    name: "@v4fire/storybook-framework-webpack5",
-    options: {
+	framework: {
+		name: "@v4fire/storybook-framework-webpack5",
+		options: {
 			rootComponent: 'p-v4-components-demo'
 		},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-	webpackFinal: async (config) => {
-		config.target = 'web';
-
-		return config;
+	},
+	docs: {
+		autodocs: "tag",
 	}
 };
 export default config;
