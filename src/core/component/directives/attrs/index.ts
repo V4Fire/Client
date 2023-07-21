bee0f8517804f93d35559e4903661c322ea6e447
kobezzza <kobezzza@gmail.com>
2023-07-21 17:25:43 +0300

    fix: directives specified in v-attrs should be applied last

--- src/core/component/directives/attrs/index.ts
+++ src/core/component/directives/attrs/index.ts
@@ -6,8 +6,6 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-/* eslint-disable complexity */
-
 /**
  * [[include:core/component/directives/attrs/README.md]]
  * @packageDocumentation
@@ -19,10 +17,11 @@ import { ComponentEngine, DirectiveBinding, VNode } from 'core/component/engines
 import {
 
 	mergeProps,
+	setVNodePatchFlags,
+
 	normalizeStyle,
 	normalizeClass,
-	normalizeComponentAttrs,
-	setVNodePatchFlags
+	normalizeComponentAttrs
 
 } from 'core/component/render';
 
@@ -50,16 +49,24 @@ export * from 'core/component/directives/attrs/interface';
 
 ComponentEngine.directive('attrs', {
 	beforeCreate(params: DirectiveParams, vnode: VNode): void {
-		let
-			handlerStore;
-
 		const
 			ctx = getDirectiveContext(params, vnode),
+			resolveDirective = this.directive.bind(this);
+
+		const
 			componentCtx = vnode.virtualComponent?.unsafe,
 			componentMeta = Object.isDictionary(vnode.type) ? components.get(vnode.type['name']) : componentCtx?.meta;
 
 		const props = vnode.props ?? {};
 		vnode.props ??= props;
+
+		let
+			r: CanUndef<ComponentInterface['$renderEngine']['r']>,
+			handlerStore;
+
+		if (ctx != null) {
+			r = ctx.$renderEngine.r;
+		}
 
 		let
 			attrs = {...params.value};
@@ -68,259 +75,28 @@ ComponentEngine.directive('attrs', {
 			attrs = normalizeComponentAttrs(attrs, vnode.dynamicProps, componentMeta)!;
 		}
 
-		let
-			r: CanUndef<ComponentInterface['$renderEngine']['r']>;
+		const attrsKeys = Object
+			.keys(attrs)
+			.sort(((key) => key.startsWith('v-') ? 1 : -1));
 
-		if (ctx != null) {
-			r = ctx.$renderEngine.r;
-		}
-
-		for (let keys = Object.keys(attrs), i = 0; i < keys.length; i++) {
-			let
-				attrName = keys[i],
+		for (let i = 0; i < attrsKeys.length; i++) {
+			const
+				attrName = attrsKeys[i],
 				attrVal = attrs[attrName];
 
-			// Directive
 			if (attrName.startsWith('v-')) {
-				const
-					decl = directiveRgxp.exec(attrName);
+				parseDirective(attrName, attrVal);
 
-				let
-					value = attrVal;
+			} else if (attrName.startsWith('@')) {
+				parseEventListener(attrName, attrVal);
 
-				if (decl == null) {
-					throw new SyntaxError('Invalid directive declaration');
-				}
-
-				const
-					[, name, arg = '', rawModifiers = ''] = decl;
-
-				let
-					dir;
-
-				switch (name) {
-					case 'show': {
-						dir = r?.vShow;
-						break;
-					}
-
-					case 'on': {
-						if (Object.isDictionary(value)) {
-							Object.entries(value).forEach(([name, handler]) => {
-								const event = `@${name}`;
-								attrs[event] = handler;
-								keys.push(event);
-							});
-						}
-
-						continue;
-					}
-
-					case 'bind': {
-						if (Object.isDictionary(value)) {
-							Object.entries(value).forEach(([name, val]) => {
-								attrs[name] = val;
-								keys.push(name);
-							});
-						}
-
-						continue;
-					}
-
-					case 'model': {
-						const
-							modelProp = arg !== '' ? arg : 'modelValue',
-							modelValLink = String(value);
-
-						const
-							handlerCache = getHandlerStore(),
-							handlerKey = `onUpdate:${modelProp}:${modelValLink}`;
-
-						let
-							handler = handlerCache.get(handlerKey);
-
-						if (handler == null) {
-							handler = (newVal) => {
-								if (ctx == null) {
-									throw new ReferenceError('The directive context is not found');
-								}
-
-								ctx[modelValLink] = newVal;
-							};
-
-							handlerCache.set(handlerKey, handler);
-						}
-
-						value = ctx?.[modelValLink];
-
-						keys.push(modelProp);
-						attrs[modelProp] = value;
-
-						const attachEvent = (event) => {
-							keys.push(event);
-							attrs[event] = handler;
-						};
-
-						switch (vnode.type) {
-							case 'input':
-							case 'textarea':
-							case 'select': {
-								dir = r?.vModelDynamic;
-								attachEvent('@update:modelValue');
-								break;
-							}
-
-							default: {
-								attachEvent(`@onUpdate:${modelProp}`);
-								continue;
-							}
-						}
-
-						break;
-					}
-
-					default:
-						dir = this.directive(name);
-				}
-
-				if (dir == null) {
-					throw new ReferenceError(`The specified directive "${name}" is not registered`);
-				}
-
-				const modifiers = {};
-				rawModifiers.split('.').forEach((modifier) => {
-					modifier = modifier.trim();
-
-					if (modifier !== '') {
-						modifiers[modifier] = true;
-					}
-				});
-
-				const bindings = vnode.dirs ?? [];
-				vnode.dirs = bindings;
-
-				const binding: DirectiveBinding = {
-					dir: Object.isFunction(dir) ? {created: dir, mounted: dir} : dir,
-					instance: params.instance,
-
-					value,
-					oldValue: undefined,
-
-					arg,
-					modifiers
-				};
-
-				const
-					cantIgnoreDir = value != null || decl.length !== 2;
-
-				if (Object.isDictionary(dir)) {
-					if (Object.isFunction(dir.beforeCreate)) {
-						const
-							newVnode = dir.beforeCreate(binding, vnode);
-
-						if (newVnode != null) {
-							vnode = newVnode;
-						}
-
-						if (Object.keys(dir).length > 1 && cantIgnoreDir) {
-							bindings.push(binding);
-						}
-
-					} else if (Object.keys(dir).length > 0 && cantIgnoreDir) {
-						bindings.push(binding);
-					}
-
-				} else if (cantIgnoreDir) {
-					bindings.push(binding);
-				}
-
-				continue;
+			} else {
+				parseProperty(attrName, attrVal);
 			}
+		}
 
-			// Event listener
-			if (attrName.startsWith('@')) {
-				let
-					isDOMEvent = true,
-					event = attrName.slice(1).camelize(false);
-
-				const
-					originalEvent = event,
-					eventChunks = event.split('.');
-
-				const
-					flags = Object.createDict<boolean>(),
-					isOnceEvent = flags.once;
-
-				eventChunks.forEach((chunk) => flags[chunk] = true);
-				event = eventChunks[0];
-
-				if (flags.right && !event.startsWith('key')) {
-					event = 'onContextmenu';
-					delete flags.right;
-
-				} else if (flags.middle && event !== 'mousedown') {
-					event = 'onMouseup';
-
-				} else {
-					event = `on${event.capitalize()}`;
-					isDOMEvent = false;
-				}
-
-				if (flags.capture) {
-					event += 'Capture';
-					isDOMEvent = true;
-					delete flags.capture;
-				}
-
-				if (flags.once) {
-					event += 'Once';
-					delete flags.once;
-				}
-
-				if (flags.passive) {
-					event += 'Passive';
-					isDOMEvent = true;
-					delete flags.passive;
-				}
-
-				if (Object.keys(flags).length > 0) {
-					const
-						registeredModifiers = Object.keys(Object.select(flags, modifiers)),
-						registeredKeyModifiers = Object.keys(Object.select(flags, keyModifiers));
-
-					if (registeredModifiers.length > 0) {
-						attrVal = r?.withModifiers.call(ctx, Object.cast(attrVal), registeredKeyModifiers);
-					}
-
-					if (registeredKeyModifiers.length > 0) {
-						attrVal = r?.withKeys.call(ctx, Object.cast(attrVal), registeredKeyModifiers);
-					}
-				}
-
-				if (componentCtx != null && !isDOMEvent && Object.isFunction(attrVal)) {
-					if (isOnceEvent) {
-						componentCtx.$on(originalEvent, attrVal);
-
-					} else {
-						componentCtx.$once(originalEvent, attrVal);
-					}
-
-				} else {
-					props[event] = attrVal;
-					setVNodePatchFlags(vnode, 'events');
-
-					const dynamicProps = vnode.dynamicProps ?? [];
-					vnode.dynamicProps = dynamicProps;
-					dynamicProps.push(event);
-				}
-
-				continue;
-			}
-
-			// Simple property
-			attrName = attrName.startsWith(':') ?
-				attrName.slice(1) :
-				attrName;
+		function parseProperty(attrName: string, attrVal: unknown) {
+			attrName = attrName.startsWith(':') ? attrName.slice(1) : attrName;
 
 			if (modRgxp.test(attrName)) {
 				const attrChunks = attrName.split('.');
@@ -388,9 +164,240 @@ ComponentEngine.directive('attrs', {
 			}
 		}
 
+		function parseDirective(attrName: string, attrVal: unknown) {
+			const
+				decl = directiveRgxp.exec(attrName);
+
+			let
+				value = attrVal;
+
+			if (decl == null) {
+				throw new SyntaxError('Invalid directive declaration');
+			}
+
+			const
+				[, name, arg = '', rawModifiers = ''] = decl;
+
+			let
+				dir;
+
+			switch (name) {
+				case 'show': {
+					dir = r?.vShow;
+					break;
+				}
+
+				case 'on': {
+					if (Object.isDictionary(value)) {
+						Object.entries(value).forEach(([name, handler]) => {
+							const event = `@${name}`;
+							attrs[event] = handler;
+							attrsKeys.push(event);
+						});
+					}
+
+					return;
+				}
+
+				case 'bind': {
+					if (Object.isDictionary(value)) {
+						Object.entries(value).forEach(([name, val]) => {
+							attrs[name] = val;
+							attrsKeys.push(name);
+						});
+					}
+
+					return;
+				}
+
+				case 'model': {
+					const
+						modelProp = arg !== '' ? arg : 'modelValue',
+						modelValLink = String(value);
+
+					const
+						handlerCache = getHandlerStore(),
+						handlerKey = `onUpdate:${modelProp}:${modelValLink}`;
+
+					let
+						handler = handlerCache.get(handlerKey);
+
+					if (handler == null) {
+						handler = (newVal) => {
+							if (ctx == null) {
+								throw new Error('No context found for the directive being created');
+							}
+
+							ctx[modelValLink] = newVal;
+						};
+
+						handlerCache.set(handlerKey, handler);
+					}
+
+					value = ctx?.[modelValLink];
+
+					attrsKeys.push(modelProp);
+					attrs[modelProp] = value;
+
+					const attachEvent = (event) => {
+						attrsKeys.push(event);
+						attrs[event] = handler;
+					};
+
+					switch (vnode.type) {
+						case 'input':
+						case 'textarea':
+						case 'select': {
+							dir = r?.vModelDynamic;
+							attachEvent('@update:modelValue');
+							break;
+						}
+
+						default: {
+							attachEvent(`@onUpdate:${modelProp}`);
+							return;
+						}
+					}
+
+					break;
+				}
+
+				default:
+					dir = resolveDirective(name);
+			}
+
+			if (dir == null) {
+				throw new ReferenceError(`The specified directive "${name}" is not registered`);
+			}
+
+			const modifiers = {};
+			rawModifiers.split('.').forEach((modifier) => {
+				modifier = modifier.trim();
+
+				if (modifier !== '') {
+					modifiers[modifier] = true;
+				}
+			});
+
+			const bindings = vnode.dirs ?? [];
+			vnode.dirs = bindings;
+
+			const binding: DirectiveBinding = {
+				dir: Object.isFunction(dir) ? {created: dir, mounted: dir} : dir,
+				instance: params.instance,
+
+				value,
+				oldValue: undefined,
+
+				arg,
+				modifiers
+			};
+
+			const
+				cantIgnoreDir = value != null || decl.length !== 2;
+
+			if (Object.isDictionary(dir)) {
+				if (Object.isFunction(dir.beforeCreate)) {
+					const
+						newVnode = dir.beforeCreate(binding, vnode);
+
+					if (newVnode != null) {
+						vnode = newVnode;
+					}
+
+					if (Object.keys(dir).length > 1 && cantIgnoreDir) {
+						bindings.push(binding);
+					}
+
+				} else if (Object.keys(dir).length > 0 && cantIgnoreDir) {
+					bindings.push(binding);
+				}
+
+			} else if (cantIgnoreDir) {
+				bindings.push(binding);
+			}
+		}
+
+		function parseEventListener(attrName: string, attrVal: unknown) {
+			let
+				isDOMEvent = true,
+				event = attrName.slice(1).camelize(false);
+
+			const
+				originalEvent = event,
+				eventChunks = event.split('.');
+
+			const
+				flags = Object.createDict<boolean>(),
+				isOnceEvent = flags.once;
+
+			eventChunks.forEach((chunk) => flags[chunk] = true);
+			event = eventChunks[0];
+
+			if (flags.right && !event.startsWith('key')) {
+				event = 'onContextmenu';
+				delete flags.right;
+
+			} else if (flags.middle && event !== 'mousedown') {
+				event = 'onMouseup';
+
+			} else {
+				event = `on${event.capitalize()}`;
+				isDOMEvent = false;
+			}
+
+			if (flags.capture) {
+				event += 'Capture';
+				isDOMEvent = true;
+				delete flags.capture;
+			}
+
+			if (flags.once) {
+				event += 'Once';
+				delete flags.once;
+			}
+
+			if (flags.passive) {
+				event += 'Passive';
+				isDOMEvent = true;
+				delete flags.passive;
+			}
+
+			if (Object.keys(flags).length > 0) {
+				const
+					registeredModifiers = Object.keys(Object.select(flags, modifiers)),
+					registeredKeyModifiers = Object.keys(Object.select(flags, keyModifiers));
+
+				if (registeredModifiers.length > 0) {
+					attrVal = r?.withModifiers.call(ctx, Object.cast(attrVal), registeredKeyModifiers);
+				}
+
+				if (registeredKeyModifiers.length > 0) {
+					attrVal = r?.withKeys.call(ctx, Object.cast(attrVal), registeredKeyModifiers);
+				}
+			}
+
+			if (componentCtx != null && !isDOMEvent && Object.isFunction(attrVal)) {
+				if (isOnceEvent) {
+					componentCtx.$on(originalEvent, attrVal);
+
+				} else {
+					componentCtx.$once(originalEvent, attrVal);
+				}
+
+			} else {
+				props[event] = attrVal;
+				setVNodePatchFlags(vnode, 'events');
+
+				const dynamicProps = vnode.dynamicProps ?? [];
+				vnode.dynamicProps = dynamicProps;
+				dynamicProps.push(event);
+			}
+		}
+
 		function getHandlerStore() {
 			if (ctx == null) {
-				throw new ReferenceError('The directive context is not found');
+				throw new Error('No context found for the directive being created');
 			}
 
 			if (handlerStore != null) {
