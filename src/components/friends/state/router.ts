172743b8f253b9c9375776e14ea8dc3431deeebc
Maxim Givanyan <givanyan99@yandex-team.ru>
2023-07-28 14:46:19 +0300

    fix resetRouter

--- src/components/friends/state/router.ts
+++ src/components/friends/state/router.ts
@@ -154,7 +154,7 @@ export async function saveToRouter(this: State, data?: Dictionary): Promise<bool
  * Resets the component router state.
  * The function takes the result of `convertStateToRouterReset` and maps it to the component.
  */
-export async function resetRouter(this: State): Promise<boolean> {
+export function resetRouter(this: State): boolean {
 	const {
 		ctx,
 		ctx: {r: {router}}
@@ -167,7 +167,6 @@ export async function resetRouter(this: State): Promise<boolean> {
 		return false;
 	}
 
-	await router.push(null);
 	ctx.log('state:reset:router', this, stateFields);
 
 	return true;
