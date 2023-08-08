b55e31fb78eff2a82c81efec84f5cb1bea4ee8ec
kobezzza <kobezzza@gmail.com>
2023-08-08 16:34:00 +0300

    chore: improvements for ssr

---
+++ src/core/init/hydrated-route.ts
@@ -0,0 +1,34 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import routes from 'routes';
+import { getRoute, compileStaticRoutes } from 'core/router';
+
+import { set } from 'core/component/state';
+import semaphore from 'core/init/semaphore';
+
+/**
+ * Initializes the initial route of the application
+ */
+// eslint-disable-next-line @typescript-eslint/require-await
+export default async function init(): Promise<void> {
+	try {
+		if (HYDRATION) {
+			const route = getRoute(
+				location.pathname + location.search,
+				compileStaticRoutes(routes)
+			);
+
+			await route?.meta.load?.();
+			set('route', route);
+		}
+
+	} finally {
+		void semaphore('hydratedRoute');
+	}
+}
