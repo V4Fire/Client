5bd4dadf2e094ae05c1a8bf93d2f25cff1fad822
Artem Shinkaruk <artasend@gmail.com>
2023-08-16 22:49:52 +0700

    feat(build): create MeasurePlugin

---
+++ build/webpack/plugins/measure-plugin/README.md
@@ -0,0 +1,3 @@
+# build/webpack/plugins/measure-plugin
+
+This module provides a plugin to measure webpack build times.
---
+++ build/webpack/plugins/measure-plugin/index.js
@@ -0,0 +1,75 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+'use strict';
+
+const {createWriteStream} = require('fs');
+
+/** @type {import('../../../helpers/tracer')} */
+const {tracer} = include('build/helpers/tracer');
+
+module.exports = class MeasurePlugin {
+	static activeCompilers = 0;
+
+	eventCallbacks = new Map();
+
+	/**
+	 * Applies measurements to webpack build
+	 * @param {import('webpack').Compiler} compiler
+	 */
+	apply(compiler) {
+		const logger = compiler.getInfrastructureLogger(this.constructor.name);
+
+		compiler.hooks.compilation.tap(this.constructor.name, (compilation) => {
+			MeasurePlugin.activeCompilers++;
+
+			const {name} = compilation;
+			const event = {
+				name: `Compilation '${name}'`,
+				id: ++tracer.counter,
+				cat: [`compilation.${name}`]
+			};
+
+			tracer.trace.begin(event);
+			this.eventCallbacks.set(`compilation.${name}`, () => tracer.trace.end(event));
+		});
+
+		compiler.hooks.done.tap(this.constructor.name, (stats) => {
+			const
+				{name} = stats.compilation,
+				key = `compilation.${name}`;
+
+			const
+				done = this.eventCallbacks.get(key);
+
+			if (typeof done === 'function') {
+				done();
+			}
+
+			this.eventCallbacks.delete(key);
+		});
+
+		compiler.hooks.shutdown.tapAsync(this.constructor.name, (cb) => {
+			if (--MeasurePlugin.activeCompilers > 0) {
+				return cb();
+			}
+
+			const ws = createWriteStream(`${process.cwd()}/measure.json`);
+
+			ws.on('error', (error) => {
+				logger.error(`Measure write failed, reason: ${error.message}`);
+			});
+
+			ws.on('close', cb);
+
+			tracer.trace.pipe(ws);
+			tracer.trace.flush();
+
+		});
+	}
+};
