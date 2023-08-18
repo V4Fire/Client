7e04f7c4da6c2264e53b2003a01a99579c364207
Maxim Givanyan <givanyan99@yandex-team.ru>
2023-08-18 09:43:27 +0300

    add possibility to specify a complicated color tokens

--- build/stylus/ds/plugins.js
+++ build/stylus/ds/plugins.js
@@ -265,6 +265,10 @@ module.exports = function getPlugins({
 				return;
 			}
 
+			name = name.includes('/') ?
+				name.split('/').map((s) => s.includes(' ') ? s.camelize(false) : s) :
+				name.split('.');
+
 			const
 				path = isOneTheme ? getThemedPathChunks('colors', theme, isFieldThemed('colors')) : ['colors'];
 
@@ -276,7 +280,7 @@ module.exports = function getPlugins({
 				}
 			}
 
-			path.push(...name.split('.'));
+			path.push(...name);
 
 			if (id !== undefined) {
 				path.push(String(id));
