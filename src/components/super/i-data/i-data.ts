ed2982d7c44557edf6064f7fcc5a218ff849bee0
kobezzza <kobezzza@gmail.com>
2023-08-25 16:48:44 +0300

    fix: ssr fix

--- src/components/super/i-data/i-data.ts
+++ src/components/super/i-data/i-data.ts
@@ -86,6 +86,10 @@ export default abstract class iData extends iDataHandlers {
 				this.emit('initLoadStart', opts);
 			}
 
+			if (this.dataProviderProp != null && this.dataProvider == null) {
+				this.syncDataProviderWatcher(false);
+			}
+
 			if (HYDRATION && !this.isReadyOnce && hydrationStore.has(this.componentId)) {
 				return callSuper();
 			}
@@ -112,10 +116,6 @@ export default abstract class iData extends iDataHandlers {
 				}
 
 				return res;
-			}
-
-			if (this.dataProviderProp != null && this.dataProvider == null) {
-				this.syncDataProviderWatcher(false);
 			}
 
 			const
