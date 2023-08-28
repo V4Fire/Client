4e61995fe0f9e5670594f1e0599ea3a22b9116f1
Artem Shinkaruk <46344555+shining-mind@users.noreply.github.com>
2023-08-28 17:41:32 +0700

    Invalidate external cache (v3) (#962)
    * feat(build): create InvalidateExternalCachePlugin
    
    * feat(build): use InvalidateExternalCachePlugin
    
    * docs: update changelog
    
    * chore: update stylus-inheritance to 3.3.0

--- build/webpack/plugins.js
+++ build/webpack/plugins.js
@@ -28,13 +28,15 @@ module.exports = async function plugins({name}) {
 		createProgressPlugin = include('build/webpack/plugins/progress-plugin'),
 		IgnoreInvalidWarningsPlugin = include('build/webpack/plugins/ignore-invalid-warnings'),
 		I18NGeneratorPlugin = include('build/webpack/plugins/i18n-plugin'),
+		InvalidateExternalCachePlugin = include('build/webpack/plugins/invalidate-external-cache'),
 		StatoscopeWebpackPlugin = require('@statoscope/webpack-plugin').default;
 
 	const plugins = new Map([
 		['globals', new webpack.DefinePlugin(await $C(globals).async.map())],
 		['dependencies', new DependenciesPlugin()],
 		['ignoreNotFoundExport', new IgnoreInvalidWarningsPlugin()],
-		['i18nGeneratorPlugin', new I18NGeneratorPlugin()]
+		['i18nGeneratorPlugin', new I18NGeneratorPlugin()],
+		['invalidateExternalCache', new InvalidateExternalCachePlugin()]
 	]);
 
 	const
