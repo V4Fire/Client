d8df30b892242cc39c3d9dfce16d171ab092f027
Maxim Givanyan <givanyan99@yandex-team.ru>
2023-08-30 10:55:47 +0300

    fix ds init

--- build/stylus/ds/index.js
+++ build/stylus/ds/index.js
@@ -24,8 +24,8 @@ const
 const
 	theme = config.theme.default(),
 	includeThemes = config.theme.include(),
-	darkThemeName = config.theme.darkThemeName(),
-	lightThemeName = config.theme.lightThemeName(),
+	darkThemeName = config.theme.darkThemeName,
+	lightThemeName = config.theme.lightThemeName,
 	{
 		'ds/use-css-vars': useCSSVarsInRuntime,
 		'ds/use-prefers-color-scheme': usePrefersColorScheme
