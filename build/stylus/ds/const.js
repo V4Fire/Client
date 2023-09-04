7538ff5a180b1c3e07080c1342ffa6fbb6c7f209
Gannicus91 <max99giv@mail.ru>
2023-09-04 17:25:49 +0300

    Apply suggestions from code review
    Co-authored-by: Artem Shinkaruk <46344555+shining-mind@users.noreply.github.com>

--- build/stylus/ds/const.js
+++ build/stylus/ds/const.js
@@ -9,7 +9,7 @@
 'use strict';
 
 module.exports = {
-	dsHasThemesNotIncluded: 'The design system object has themes, but no one included into the build',
+	dsHasThemesNotIncluded: 'The design system object has themes, but none included into the build',
 	dsNotIncludedRequiredThemes: 'The design system object does not provide "{{dark}}" and "{{light}}" theme to use "prefers-color-scheme"',
 	dsNotIncludedDarkTheme: 'The design system object does not provide "{{dark}}" theme to use "prefers-color-scheme"',
 	dsNotIncludedLightTheme: 'The design system object does not provide "{{light}}" theme to use "prefers-color-scheme"'
