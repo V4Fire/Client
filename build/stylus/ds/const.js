8b9cf3504a94749ad5c81ac8c5372a7f8c2a98fe
Maxim Givanyan <givanyan99@yandex-team.ru>
2023-09-06 16:13:37 +0300

    improved comments

--- build/stylus/ds/const.js
+++ build/stylus/ds/const.js
@@ -10,7 +10,7 @@
 
 module.exports = {
 	dsHasThemesNotIncluded: 'The design system object has themes, but none included into the build',
-	dsNotIncludedRequiredThemes: (dark, light) => `The "ds/use-prefers-color-scheme" parameter is set to true, but the design system does not provide "${dark}" and "${light}" themes, which are required for using this parameter`,
-	dsNotIncludedDarkTheme: (dark) => `The "ds/use-prefers-color-scheme" parameter is set to true, but the design system does not provide "${dark}" theme, which is required for using this parameter`,
-	dsNotIncludedLightTheme: (light) => `The "ds/use-prefers-color-scheme" parameter is set to true, but the design system does not provide "${light}" theme, which is required for using this parameter`
+	dsNotIncludedRequiredThemes: (dark, light) => `The "prefersColorScheme" is enabled, but the design system does not provide "${dark}" and "${light}" themes, which are required for using this parameter`,
+	dsNotIncludedDarkTheme: (dark) => `The "prefersColorScheme" is enabled, but the design system does not provide "${dark}" theme, which is required for using this parameter`,
+	dsNotIncludedLightTheme: (light) => `The "prefersColorScheme" is enabled, but the design system does not provide "${light}" theme, which is required for using this parameter`
 };
