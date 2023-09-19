97df90d51ff871120e5152e1177909d0d2286f90
geopr <prohorovgosa@gmail.com>
2023-09-19 13:50:53 +0300

    refactor: rewrote resize method in adapter using async/await

--- build/webpack/loaders/responsive-images-loader/adapter.js
+++ build/webpack/loaders/responsive-images-loader/adapter.js
@@ -36,30 +36,21 @@ class Adapter {
 	 * @returns {Promise<{data: Buffer, width: number, height: number}>}
 	 * @see https://github.com/dazuaz/responsive-loader/tree/master#writing-your-own-adapter
 	 */
-	resize({mime, width: scaleBy, options: {quality, sizes}}) {
-		return new Promise(async (resolve, reject) => {
-			const
-				imageClone = this.image.clone(),
-				{width, height} = await imageClone.metadata();
+	async resize({mime, width: scaleBy, options: {quality, sizes}}) {
+		const
+			imageClone = this.image.clone(),
+			{width, height} = await imageClone.metadata();
 
-			if (width == null || height == null) {
-				reject('Unable to receive width and height of the image', this.image);
-				return;
-			}
+		if (width == null || height == null) {
+			throw new Error('Unable to receive width and height of the image', this.image);
+		}
 
-			const
-				scaledImage = this.scale(imageClone, width, height, scaleBy, sizes.length),
-				convertedImage = this.convert(scaledImage, mime, quality);
+		const
+			scaledImage = this.scale(imageClone, width, height, scaleBy, sizes.length),
+			convertedImage = this.convert(scaledImage, mime, quality),
+			{data, info} = await convertedImage.toBuffer({resolveWithObject: true});
 
-			convertedImage.toBuffer((err, data, {width, height}) => {
-				if (err) {
-					reject(err);
-
-				} else {
-					resolve({data, width, height});
-				}
-			});
-		});
+		return {data, width: info.width, height: info.height};
 	}
 
 	/**
