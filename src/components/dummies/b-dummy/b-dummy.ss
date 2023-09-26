008b21101939eb67431f58eaa694621f0e75d7dd
Artem Shinkaruk <artasend@gmail.com>
2023-09-26 21:15:38 +0700

    chore: example of the computed watch bug

--- src/components/dummies/b-dummy/b-dummy.ss
+++ src/components/dummies/b-dummy/b-dummy.ss
@@ -13,3 +13,30 @@
 - template index() extends ['i-data'].index
 	- block body
 		+= self.slot()
+
+		< b-button @click = spliceItem
+			Splice
+
+		< b-button @click = modifyItem
+			Modify
+
+		< strong
+			Problem
+		< ul &
+			v-for = (group, groupIndex) in groups |
+			:key = groupIndex
+		.
+			< li
+				Group {{groupIndex}}
+			< li &
+				v-for = item in group |
+				:key = item.id
+			.
+				{{JSON.stringify(item)}}
+
+
+		< strong
+			Debug
+		/// Uncomment to fix the bug
+		/// < pre
+		/// 	{{debug}}
--- src/components/dummies/b-dummy/b-dummy.ts
+++ src/components/dummies/b-dummy/b-dummy.ts
@@ -11,9 +11,15 @@
  * @packageDocumentation
  */
 
-import iData, { component } from 'components/super/i-data/i-data';
+import iData, { component, computed, prop, field } from 'components/super/i-data/i-data';
 
 export * from 'components/super/i-data/i-data';
+
+
+interface Item {
+	id: number;
+	state: number;
+}
 
 @component({
 	functional: {
@@ -22,7 +28,59 @@ export * from 'components/super/i-data/i-data';
 })
 
 class bDummy extends iData {
+	readonly Items!: Item[];
 
+	@prop(Number)
+	readonly groupSize: number = 0;
+
+	@prop(Array)
+	readonly itemsProp!: this['Items'];
+
+	@field<bDummy>((o) => o.sync.link<Partial<Item>[]>((val) => o.normalizeItems(val)))
+	itemsStore!: this['Items'];
+
+	/** {@link bAnchorNavigation.items} */
+	@computed({dependencies: ['itemsStore']})
+	get items(): this['Items'] {
+		return <this['Items']>this.field.get('itemsStore');
+	}
+
+	/** {@link bAnchorNavigation.items} */
+	set items(value: this['Items']) {
+		this.field.set('itemsStore', value);
+	}
+
+	@computed({dependencies: ['items', 'groupSize']})
+	get debug() {
+		return JSON.stringify(this.items, null, '  ');
+	}
+
+	@computed({dependencies: ['items', 'groupSize']})
+	get groups() {
+		const
+			result: Item[][] = [],
+			arr = Array.from(this.items);
+
+		for (let i = 0; i < arr.length; i += this.groupSize) {
+			result.push(arr.slice(i, i + this.groupSize));
+		}
+
+		this.console.log(result);
+
+		return result;
+	}
+
+	normalizeItems(value: Partial<Item>[]): Item[] {
+		return (value ?? []).map((item, index) => ({...item, id: item.id ?? index + 1, state: item.state ?? 0}));
+	}
+
+	modifyItem() {
+		this.items[0].state += 1;
+	}
+
+	spliceItem() {
+		this.items.splice(0, 1);
+	}
 }
 
 export default bDummy;
