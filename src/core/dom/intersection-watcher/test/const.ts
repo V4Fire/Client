7908e04c26e89865e04689bb542a6bfbe0c96824
Dmitry Lebedko <62644372+Dimeliora@users.noreply.github.com>
2023-10-17 14:59:52 +0300

    New unit tests for the intersection-watcher module (issues/964) (#993)
    * test: completely rewrite unit tests using playwright
    
    * style: fix eslint warnings
    
    * fix: update immediate check condition
    
    * fix: add onLeave call for a last leaving element
    
    * fix: update positions sorting
    
    * fix: add empty thresholdGroup set deletion
    
    * fix: update isElementInView, add calcElementAreaInView helper
    
    * fix: fix unwatching by specified handler cb
    
    * test: delete test.fail for intersection engine
    
    * test: delete old tests
    
    * style: fix eslint errors
    
    * refactor: change observers handling within the unwatch
    
    * fix: fix width/height calculation
    
    * refactor: refactor inView conditions
    
    * refactor: add const for condition, add comment
    
    * test: refactor unit tests
    
    * test: add new test case
    
    * chore: stylish fixes
    
    * chore: stylish fixes
    
    * fix: add default value for the onlyRoot option
    
    * test: add unit test helpers
    
    * test: add unit test constants
    
    * test: regroup unit tests
    
    * test: add unit tests for the root option
    
    * chore: stylish fixes
    
    ---------
    
    Co-authored-by: Dmitry Lebedko <dimeliora@yandex-team.ru>
    Co-authored-by: kobezzza <kobezzza@gmail.com>

---
+++ src/core/dom/intersection-watcher/test/const.ts
@@ -0,0 +1,26 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+export const ENGINES = ['heightmap', 'intersection'];
+
+export const TARGET_STYLES: Partial<CSSStyleDeclaration> = {
+	width: '100px',
+	height: '100px'
+};
+
+export const ROOT_STYLES: Partial<CSSStyleDeclaration> = {
+	width: '300px',
+	height: '300px',
+	overflow: 'auto'
+};
+
+export const ROOT_INNER_STYLES: Partial<CSSStyleDeclaration> = {
+	position: 'relative',
+	width: '500px',
+	height: '500px'
+};
