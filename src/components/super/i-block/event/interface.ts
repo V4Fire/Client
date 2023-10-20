56364aa1f5564663e715b03ab064b910d4399975
kobezzza <kobezzza@gmail.com>
2023-10-20 15:43:47 +0300

    chore(TS): better type support

--- src/components/super/i-block/event/interface.ts
+++ src/components/super/i-block/event/interface.ts
@@ -7,7 +7,7 @@
  */
 
 export type InferEvents<I extends Array<[string, ...any[]]>, R extends Dictionary = {}> = {
-	0: InferEvents<TB.Tail<I>, R & (TB.Head<I> extends [infer E, ...infer A] ?
+	0: InferEvents<TB.Tail<I>, (TB.Head<I> extends [infer E, ...infer A] ?
 		E extends string ? {
 			Args: {[K in E]: A};
 
@@ -19,7 +19,7 @@ export type InferEvents<I extends Array<[string, ...any[]]>, R extends Dictionar
 
 			emit(event: E, ...args: A): void;
 			emit(event: string, ...args: unknown[]): void;
-		} : {} : {})>;
+		} : {} : {}) & R>;
 
 	1: R;
 }[TB.Length<I> extends 0 ? 1 : 0];
@@ -27,27 +27,31 @@ export type InferEvents<I extends Array<[string, ...any[]]>, R extends Dictionar
 export type GetComponentEvents<R extends {Args: Record<string, any[]>}> =
 	R extends {Args: Record<infer K, any[]>} ? K : never;
 
-export type InferComponentEvents<C, I extends Array<[string, ...any[]]>, R extends Dictionary = {}> = {
-	0: InferComponentEvents<C, TB.Tail<I>, OverrideParentComponentEvents<C, R> &
-		(TB.Head<I> extends [infer E, ...infer A] ? E extends string ? {
-			Args: {[K in E]: A};
+export type InferComponentEvents<
+	C,
+	I extends Array<[string, ...any[]]>,
+	P extends Dictionary = {},
+	R extends Dictionary = {}
+> = {
+	0: InferComponentEvents<C, TB.Tail<I>, P, (TB.Head<I> extends [infer E, ...infer A] ? E extends string ? {
+		Args: {[K in E]: A};
 
-			on(event: `on${Capitalize<E>}`, cb: (...args: A) => void): void;
-			on(event: E | `${E}:component`, cb: (component: C, ...args: A) => void): void;
+		on(event: `on${Capitalize<E>}`, cb: (...args: A) => void): void;
+		on(event: E | `${E}:component`, cb: (component: C, ...args: A) => void): void;
 
-			once(event: `on${Capitalize<E>}`, cb: (...args: A) => void): void;
-			once(event: E | `${E}:component`, cb: (component: C, ...args: A) => void): void;
+		once(event: `on${Capitalize<E>}`, cb: (...args: A) => void): void;
+		once(event: E | `${E}:component`, cb: (component: C, ...args: A) => void): void;
 
-			promisifyOnce(event: `on${Capitalize<E>}`): Promise<CanUndef<TB.Head<A>>>;
-			promisifyOnce(event: E | `${E}:component`): Promise<CanUndef<C>>;
+		promisifyOnce(event: `on${Capitalize<E>}`): Promise<CanUndef<TB.Head<A>>>;
+		promisifyOnce(event: E | `${E}:component`): Promise<CanUndef<C>>;
 
-			off(event: E | `${E}:component` | `on${Capitalize<E>}` | string, handler?: Function): void;
+		off(event: E | `${E}:component` | `on${Capitalize<E>}` | string, handler?: Function): void;
 
-			emit(event: E | `${E}:component` | `on${Capitalize<E>}`, ...args: A): void;
-			emit(event: string, ...args: unknown[]): void;
-		} : {} : {})>;
+		emit(event: E | `${E}:component` | `on${Capitalize<E>}`, ...args: A): void;
+		emit(event: string, ...args: unknown[]): void;
+	} : {} : {}) & R>;
 
-	1: R;
+	1: R & OverrideParentComponentEvents<C, P>;
 }[TB.Length<I> extends 0 ? 1 : 0];
 
 export type OverrideParentComponentEvents<C, P extends Dictionary, A = P['Args']> = A extends Record<string, any[]> ? {
