e8daa0f4b864100f5c4a42bb5c74e46e97862859
Artem Shinkaruk <artasend@gmail.com>
2023-10-27 15:34:34 +0700

    test(b-tree): add foldable test suite

---
+++ src/components/base/b-tree/test/unit/foldable.ts
@@ -0,0 +1,67 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import test from 'tests/config/unit/test';
+
+import type { Item } from 'components/base/b-tree/interface';
+
+import { renderTree, createTreeSelector, createTestModIs, waitForItems } from 'components/base/b-tree/test/helpers';
+
+test.describe('<b-tree> foldable', () => {
+	const
+		testFoldedModIs = createTestModIs('folded'),
+		foldSelector = createTreeSelector('fold');
+
+	const items: Item[] = [
+		{
+			label: 'root',
+			value: '0',
+			folded: false,
+			children: [
+				{
+					label: 'item 1',
+					value: '1',
+					children: [{label: 'item 1.1', value: '1.1'}]
+				},
+				{
+					label: 'item 2',
+					value: '2',
+					children: [{label: 'item 2.1', value: '2.1'}]
+				}
+			]
+		}
+	];
+
+	test.beforeEach(async ({demoPage}) => {
+		await demoPage.goto();
+	});
+
+	test.describe('with `folded = false`', () => {
+		const attrs = {folded: false};
+
+		test('should fold the children of the item when it\'s `fold` element is clicked', async ({page}) => {
+			const target = await renderTree(page, {items, attrs});
+			await page.getByText('item 1').locator(foldSelector).click();
+
+			await testFoldedModIs(true, await waitForItems(page, target, ['1']));
+			await testFoldedModIs(false, await waitForItems(page, target, ['2']));
+		});
+	});
+
+	test.describe('with `folded = true`', () => {
+		const attrs = {folded: true};
+
+		test('should unfold the children of the item when it\'s `fold` element is clicked', async ({page}) => {
+			const target = await renderTree(page, {items, attrs});
+			await page.getByText('item 1').locator(foldSelector).click();
+
+			await testFoldedModIs(false, await waitForItems(page, target, ['1']));
+			await testFoldedModIs(true, await waitForItems(page, target, ['2']));
+		});
+	});
+});
