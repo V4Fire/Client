99073d33387d6b604a30233d1e01bfd13dbf3210
Dmitry Lebedko <dimeliora@yandex-team.ru>
2023-11-01 15:02:46 +0300

    test: add common helper

--- src/components/base/b-router/test/helpers.ts
+++ src/components/base/b-router/test/helpers.ts
@@ -6,11 +6,16 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
+import type { JSHandle } from 'playwright';
+
+import test from 'tests/config/unit/test';
+
 import { Component, Utils } from 'tests/helpers';
 
 import type { StaticRoutes } from 'core/router';
 import type { EngineName, InitRouter } from 'components/base/b-router/test/interface';
 import type { Router } from 'components/base/b-router/interface';
+import type iStaticPage from 'components/super/i-static-page/i-static-page';
 
 /**
  * Returns a function to initialize the router on the page with the specified engine, routes and props
@@ -52,4 +57,24 @@ export function createInitRouter(engineName: EngineName, routes?: StaticRoutes, 
 
 		return Component.waitForRoot(page);
 	};
+}
+
+/**
+ * Pushes the provided route path and checks whether the value
+ * of the meta.content field matches the assertion.
+ * The function returns a Promise.
+ *
+ * @param root
+ * @param path
+ * @param content
+ */
+export async function assertPathTransitionsTo(
+	root: JSHandle<iStaticPage>,
+	path: string,
+	content: string
+): Promise<void> {
+	await test.expect(root.evaluate(async (ctx, path) => {
+		await ctx.router!.push(path);
+		return ctx.route!.meta.content;
+	}, path)).toBeResolvedTo(content);
 }
