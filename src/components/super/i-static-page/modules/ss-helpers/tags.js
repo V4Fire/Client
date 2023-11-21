609228064c35470bdc3f454150e18ad596520d2e
Andrey Kobets <kobezzza@gmail.com>
2023-11-21 13:34:04 +0300

    Update src/components/super/i-static-page/modules/ss-helpers/tags.js

--- src/components/super/i-static-page/modules/ss-helpers/tags.js
+++ src/components/super/i-static-page/modules/ss-helpers/tags.js
@@ -122,7 +122,7 @@ function getScriptDecl(lib, body = '') {
 	if (isInline && !body) {
 		return (() => {
 			if (!fs.existsSync(lib.src)) {
-				throw new Error(`Asset ${lib.src} not found`);
+				throw new Error(`The asset ${lib.src} cannot be found`);
 			}
 
 			const
