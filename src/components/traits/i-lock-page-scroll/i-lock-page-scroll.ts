5662eb1a399f8ba2b6f3fe1a3efa73656e79e628
Maxim Givanyan <givanyan99@yandex-team.ru>
2023-11-29 16:41:54 +0300

    fix: fix initIOSScrollableNodeListeners invocation

--- src/components/traits/i-lock-page-scroll/i-lock-page-scroll.ts
+++ src/components/traits/i-lock-page-scroll/i-lock-page-scroll.ts
@@ -35,7 +35,7 @@ export default abstract class iLockPageScroll {
 			r: {unsafe: {async: $a}}
 		} = component;
 
-		this.initIOSScrollableNodeListeners(component, scrollableNode);
+		iLockPageScroll.initIOSScrollableNodeListeners(component, scrollableNode);
 
 		let
 			promise = Promise.resolve();
