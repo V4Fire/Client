74bbd8f59d3c2f71054626c688c068a9dd5d3f78
kobezzza <kobezzza@gmail.com>
2024-01-17 15:42:16 +0300

    fix: prefer tmp insteadof self

--- build/snakeskin/default-filters.js
+++ build/snakeskin/default-filters.js
@@ -88,8 +88,8 @@ function tagFilter({name, attrs = {}}, tplName, cursor) {
 		attrs[':componentIdProp'] = [`componentId + ${JSON.stringify(id)}`];
 	}
 
-	attrs[':getRoot'] = ["self.__getRoot__ ??= () => ('getRoot' in self ? self.getRoot?.() : null) ?? self.$root"];
-	attrs[':getParent'] = ["self.__getParent__ ??= () => typeof $restArgs !== 'undefined' ? $restArgs?.ctx ?? self : self"];
+	attrs[':getRoot'] = ["self.tmp.__getRoot__ ??= () => ('getRoot' in self ? self.getRoot?.() : null) ?? self.$root"];
+	attrs[':getParent'] = ["self.tmp.__getParent__ ??= () => typeof $restArgs !== 'undefined' ? $restArgs?.ctx ?? self : self"];
 
 	if (component.inheritMods !== false && !attrs[':modsProp']) {
 		attrs[':modsProp'] = ['sharedMods'];
