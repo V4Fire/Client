c76464d54e828805944dd196668630b6cfc12b4a
Dzmitry Yahorau <d-yahorau@yandex-team.ru>
2024-01-31 15:42:34 +0300

    test: added unit-tests for b-slider with scrol-snap mode

--- src/components/base/b-slider/test/unit/scroll-snap.ts
+++ src/components/base/b-slider/test/unit/scroll-snap.ts
@@ -11,7 +11,7 @@ import type { JSHandle } from 'playwright';
 import test from 'tests/config/unit/test';
 
 import type bSlider from 'components/base/b-slider/b-slider';
-import { renderSlider } from 'components/base/b-slider/test/helpers';
+import { current, renderSlider } from 'components/base/b-slider/test/helpers';
 
 test.use({
 	isMobile: true,
@@ -22,30 +22,62 @@ test.use({
 	}
 });
 
-test.describe.only('<b-slider> in scroll snap mode', () => {
+test.describe('<b-slider> in scroll snap mode', () => {
 	let
-		scrollSnapSlider: JSHandle<bSlider>;
+		scrollSnapSlider: JSHandle<bSlider>,
+		scrollSlider: JSHandle<bSlider>,
+		swipeSlider: JSHandle<bSlider>;
 
 	test.beforeEach(async ({page, demoPage}) => {
 		await demoPage.goto();
 
+		const children = [
+			{type: 'img', attrs: {id: 'slide_1', src: 'https://fakeimg.pl/375x300'}},
+			{type: 'img', attrs: {id: 'slide_2', src: 'https://fakeimg.pl/375x300'}},
+			{type: 'img', attrs: {id: 'slide_3', src: 'https://fakeimg.pl/375x300'}},
+			{type: 'img', attrs: {id: 'slide_4', src: 'https://fakeimg.pl/375x300'}}
+		];
+
 		scrollSnapSlider = await renderSlider(page, {
 			childrenIds: [1, 2, 3, 4],
-			children: [
-				{type: 'img', attrs: {id: 'slide_1', src: 'https://fakeimg.pl/375x300'}},
-				{type: 'img', attrs: {id: 'slide_2', src: 'https://fakeimg.pl/375x300'}},
-				{type: 'img', attrs: {id: 'slide_3', src: 'https://fakeimg.pl/375x300'}},
-				{type: 'img', attrs: {id: 'slide_4', src: 'https://fakeimg.pl/375x300'}}
-			],
+			children,
 			attrs: {
 				useScrollSnap: true,
 				mode: 'scroll'
 			}
 		});
+
+		swipeSlider = await renderSlider(page, {
+			childrenIds: [1, 2, 3, 4],
+			children,
+			attrs: {
+				id: 'swipe_slider'
+			}
+		});
+
+		scrollSlider = await renderSlider(page, {
+			childrenIds: [1, 2, 3, 4],
+			children,
+			attrs: {
+				id: 'scroll_slider',
+				mode: 'scroll'
+			}
+		});
 	});
 
-	test('should render g-slider', async ({page}) => {
-		await test.expect(page.locator('.b-slider .g-slider')).toBeVisible();
+	test.describe('g-slider rendering', () => {
+		test('should render g-slider', async ({page}) => {
+			await test.expect(page.locator('.b-slider .g-slider')).toBeVisible();
+		});
+
+		test('should not render g-slider if mode is `swipe`', async ({page}) => {
+			await test.expect(page.locator('#swipe_slider .g-slider')).not.toBeVisible();
+		});
+
+		test('should not render g-slider if mode is `scroll` and useSnapScroll is `false`', async ({page}) => {
+			await test.expect(page.locator('#scroll_slider .g-slider')).not.toBeVisible();
+		});
+
 	});
 
 	test('should throw error `useScrollSnap = true` and `mode = slide`', async ({page}) => {
@@ -56,6 +88,20 @@ test.describe.only('<b-slider> in scroll snap mode', () => {
 				mode: 'slide'
 			}
 		})).rejects.toThrowError();
+	});
+
+	test('shouldn\'t automatically scroll', async () => {
+		test.expect(await current(scrollSnapSlider)).toBe(0);
+
+		const timeStart = new Date().getTime();
+
+		await test.expect
+			.poll(() => new Date().getTime() - timeStart, {
+				timeout: 200
+			})
+			.toBeGreaterThan(100);
+
+		test.expect(await current(scrollSnapSlider)).toBe(0);
 	});
 
 });
