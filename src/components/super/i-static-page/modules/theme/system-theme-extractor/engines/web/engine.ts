8f1f8f29e50f4db7e04a5547644d8de9841e7837
kobezzza <kobezzza@gmail.com>
2024-02-19 15:55:30 +0300

    fix(ssr): fixed ssr support

--- src/components/super/i-static-page/modules/theme/system-theme-extractor/engines/web/engine.ts
+++ src/components/super/i-static-page/modules/theme/system-theme-extractor/engines/web/engine.ts
@@ -22,33 +22,45 @@ export default class WebEngine extends Friend implements SystemThemeExtractor {
 	/**
 	 * A media query object for monitoring theme changes
 	 */
-	protected readonly darkThemeMq: MediaQueryList;
+	protected readonly darkThemeMq?: MediaQueryList;
 
 	/**
 	 * An event emitter to broadcast theme events
 	 */
-	protected readonly emitter: EventEmitterLikeP;
+	protected readonly emitter?: EventEmitterLikeP;
 
 	constructor(component: iBlock) {
 		super(component);
+
+		if (SSR) {
+			return;
+		}
 
 		this.darkThemeMq = globalThis.matchMedia('(prefers-color-scheme: dark)');
 
 		type EmitterArgs = [string, (e: Event) => void];
 
 		this.emitter = Object.cast((...args: EmitterArgs) => {
-			this.darkThemeMq.addEventListener(...args);
-			return (...args: EmitterArgs) => this.darkThemeMq.removeEventListener(...args);
+			this.darkThemeMq!.addEventListener(...args);
+			return (...args: EmitterArgs) => this.darkThemeMq!.removeEventListener(...args);
 		});
 	}
 
 	/** @inheritDoc */
 	getSystemTheme(): SyncPromise<string> {
+		if (this.darkThemeMq == null) {
+			return SyncPromise.resolve('light');
+		}
+
 		return SyncPromise.resolve(this.darkThemeMq.matches ? 'dark' : 'light');
 	}
 
 	/** @inheritDoc */
 	onThemeChange(cb: (value: string) => void, asyncOptions?: AsyncOptions): void {
+		if (this.emitter == null) {
+			return;
+		}
+
 		const changeHandler = (e: MediaQueryListEvent) => cb(e.matches ? 'dark' : 'light');
 		this.ctx.async.on(this.emitter, 'change', changeHandler, asyncOptions);
 	}
