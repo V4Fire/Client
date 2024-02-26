f9ca7de6123803ff3db089e3e87ef4ecde9e8e7f
Artem Shinkaruk <46344555+shining-mind@users.noreply.github.com>
2024-02-26 20:41:12 +0700

    Update src/components/friends/async-render/test/helpers.ts
    Co-authored-by: Andrey Kobets <kobezzza@gmail.com>

--- src/components/friends/async-render/test/helpers.ts
+++ src/components/friends/async-render/test/helpers.ts
@@ -5,6 +5,7 @@
  * Released under the MIT license
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
+
 import type { JSHandle, Page } from 'playwright';
 
 import test from 'tests/config/unit/test';
