122f0afd3231cacab1ab4745b77b044b0e15ac67
kobezzza <kobezzza@gmail.com>
2024-02-28 15:27:46 +0300

    chore: removed unused import

--- src/core/component/client-state/index.ts
+++ src/core/component/client-state/index.ts
@@ -6,9 +6,6 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
-import 'core/component/client-state/bindings';
-
 export * from 'core/component/state';
-
 export * from 'core/component/client-state/const';
 export { default } from 'core/component/client-state/const';
