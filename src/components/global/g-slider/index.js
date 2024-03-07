84fbf0c45bd77dc5f8065ca653d5e36990c64d56
Dmitry Yegorov <48996979+DmitryYegorov@users.noreply.github.com>
2024-03-07 18:19:36 +0300

    feat: added useScrollSnap flag for slider component (#1095)
    * feat: added useScrollSnap flag for slider component
    
    * refactor: removed trailing comma
    
    * fix: removed styl-prefix props
    
    * feat: described logic of g-slider components in README.MD
    
    * fix: cleaned p-v4-component-demo
    
    * fix: cleaned p-v4-component-demo
    
    * refactor: remvoed unused includes
    
    * doc: added modifiers and examples for using g-slider
    
    * doc: fixed version in changelog
    
    * test: unit-test for b-slider in useScrollSnap mode
    
    * doc: added story for g-slider in storybook
    
    * doc: added slider example for g-slider story
    
    * fix: fixed grammar mistakes in docs; set the height of g-slider iframe in story
    
    * refactor: if-else logic for b-slider template rendering
    
    * story: added story for vertical and horizontal slider
    
    * fix: added validator hook for b-slider data
    
    * doc: fixed grammar mistakes
    
    * refactor: close the brackets in one line
    
    * changelog: updated log
    
    * removed unused template g-slider; add jsdoc for beforeDataCreate hook for b-slider
    
    * test: added unit-tests for b-slider with scrol-snap mode
    
    * fixed mistakes in docs; code review for unit tests
    
    * test: unit-test for slider snap testing
    
    * refactor: removed unused console.log invocations
    
    * refactor: grammar mistakes in docs; BEM-naming
    
    * fix: removed  invocation for testing
    
    * refactor: use swipes via mouse.wheel
    
    * docs: made descriptions more laconic for CSS Scroll Snap flag
    
    * refactor: improve code quality and fixed mistakes
    
    * refactor: improve code quality and fixed mistakes
    
    * refactor: improve code quality and fixed mistakes
    
    * fix: decreased timeout and autoslideinterval
    
    * fix: decreased timeout and autoslideinterval
    
    * chore: stylish fixes
    
    ---------
    
    Co-authored-by: Dzmitry Yahorau <d-yahorau@yandex-team.ru>
    Co-authored-by: kobezzza <kobezzza@gmail.com>

---
+++ src/components/global/g-slider/index.js
@@ -0,0 +1,9 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+package('g-slider');
