d66b0bd6595d9b74c939d7d2f4ec35fb4976e31e
kobezzza <kobezzza@gmail.com>
2024-03-11 17:53:09 +0300

    chore: fixed TS types

--- src/components/super/i-page/interface.ts
+++ src/components/super/i-page/interface.ts
@@ -10,10 +10,12 @@ import type iPage from 'components/super/i-page/i-page';
 
 export type TitleValue<CTX extends iPage = iPage['unsafe']> =
 	string |
-	((ctx: CTX) => string) |
-	(() => (ctx: CTX) => string);
+	((ctx: CTX) => string);
 
 export type DescriptionValue<CTX extends iPage = iPage['unsafe']> = TitleValue<CTX>;
+
+export type TitleValueProp<CTX extends iPage = iPage['unsafe']> = TitleValue<CTX> | (() => TitleValue<CTX>);
+export type DescriptionValueProp <CTX extends iPage = iPage['unsafe']> = TitleValueProp<CTX>;
 
 export interface StageTitles<CTX extends iPage = iPage['unsafe']> extends Dictionary<TitleValue<CTX>> {
 	'[[DEFAULT]]': TitleValue<CTX>;
