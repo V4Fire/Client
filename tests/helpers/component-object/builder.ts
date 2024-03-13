537b53e0ebbae7777b266866e1e4383d6e982894
Artem Shinkaruk <artasend@gmail.com>
2024-03-13 17:04:32 +0700

    chore(tests/helpers/component-object): forbid children for functional components inside b-dummy
    Functional component is rendered to a vnode tree -
    specifying children using v-render will replace original
    children of the component.

--- tests/helpers/component-object/builder.ts
+++ tests/helpers/component-object/builder.ts
@@ -161,9 +161,14 @@ export default abstract class ComponentObjectBuilder<COMPONENT extends iBlock> {
 
 		const
 			name = this.componentName,
-			fullComponentName = `${name}${options?.functional && !name.endsWith('-functional') ? '-functional' : ''}`;
+			{functional = false} = options ?? {},
+			fullComponentName = `${name}${functional && !name.endsWith('-functional') ? '-functional' : ''}`;
 
 		if (options?.useDummy) {
+			if (functional && !Object.isEmpty(this.children)) {
+				throw new Error('Children are not supported for functional components inside b-dummy');
+			}
+
 			const component = await Component.createComponentInDummy<COMPONENT>(this.pwPage, fullComponentName, {
 				attrs: this.props,
 				children: this.children
