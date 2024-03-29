70f9442d1856281ecaecf89dc252c4dd2647585d
bonkalol <bonkalol@list.ru>
2024-03-29 11:00:01 +0300

    Add tests

---
+++ src/core/component/event/test/unit/main.ts
@@ -0,0 +1,48 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import type { JSHandle } from 'playwright';
+
+import test from 'tests/config/unit/test';
+
+import { renderDummy } from 'components/super/i-block/test/helpers';
+
+import type bDummy from 'components/dummies/b-dummy/b-dummy';
+
+test.describe('core/component/event', () => {
+	let target: JSHandle<bDummy>;
+
+	test.beforeEach(async ({demoPage, page}) => {
+		await demoPage.goto();
+		target = await renderDummy(page);
+	});
+
+	test('добавление обработчика через метод $on должно происходить через $async', async () => {
+		const isWrapped = await target.evaluate((ctx) => {
+			ctx.unsafe.$on('$onTest', () => {
+				// ...
+			});
+
+			return Object.get(ctx, '$async.cache.eventListener.groups.$onTest') != null;
+		});
+
+		test.expect(isWrapped).toBe(true);
+	});
+
+	test('добавление обработчика через метод $once должно происходить через $async', async () => {
+		const isWrapped = await target.evaluate((ctx) => {
+			ctx.unsafe.$once('$onTest', () => {
+				// ...
+			});
+
+			return Object.get(ctx, '$async.cache.eventListener.groups.$onTest') != null;
+		});
+
+		test.expect(isWrapped).toBe(true);
+	});
+});
