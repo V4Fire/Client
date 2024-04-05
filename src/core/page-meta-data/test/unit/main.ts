203a68e331d5b8e10880f0548cae093f50ff4856
kobezzza <kobezzza@gmail.com>
2024-04-05 15:01:08 +0300

    chore: improved tests

--- src/core/page-meta-data/test/unit/main.ts
+++ src/core/page-meta-data/test/unit/main.ts
@@ -22,7 +22,7 @@ test.describe('core/page-meta-data', () => {
 		root = await Component.waitForRoot(page);
 	});
 
-	test('should set and reset the page\'s title', async ({page}) => {
+	test("should set and reset the page's title", async ({page}) => {
 		const newTitle = 'Cool title';
 
 		await root.evaluate((ctx, newTitle) => ctx.pageMetaData.title = newTitle, newTitle);
@@ -36,7 +36,7 @@ test.describe('core/page-meta-data', () => {
 		await test.expect(page).toHaveTitle('');
 	});
 
-	test('should set and reset the page\'s description', async ({page}) => {
+	test("should set and reset the page's description", async ({page}) => {
 		const
 			newDescription = 'Cool description',
 			metaDescriptionLocator = page.locator('meta[name="description"]');
@@ -55,52 +55,123 @@ test.describe('core/page-meta-data', () => {
 		await test.expect(metaDescriptionLocator).toHaveAttribute('content', '');
 	});
 
-	test('`addLink` should add link to the head of the page', async ({page}) => {
-		const href = 'https://example.com/';
-		await root.evaluate((ctx, href) => ctx.pageMetaData.setCanonicalLink(href), href);
+	test.describe('managing `link` elements on the page', () => {
+		test('`addLink` should add a new link tag to the head of the page', async () => {
+			const attrs = {
+				rel: 'prefetch',
+				href: 'main.js'
+			};
 
-		const linkHref = await root.evaluate((ctx) => ctx.pageMetaData.getCanonicalLink()?.href);
+			const linkIsExists = await root.evaluate((ctx, attrs) => {
+				ctx.pageMetaData.addLink(attrs);
+				return document.head.querySelector(`link[rel="${attrs.rel}"][href="${attrs.href}"]`) != null;
+			}, attrs);
 
-		test.expect(linkHref).toEqual(href);
-		await test.expect(page.locator('head link[rel="canonical"]')).toHaveAttribute('href', href);
-	});
-
-	test('`addMeta` should add meta tag to the head of the page', async ({page}) => {
-		const content = 'noindex';
-		await root.evaluate((ctx, content) => ctx.pageMetaData.addMeta({name: 'robots', content}), content);
-
-		const metaInfo = await root.evaluate((ctx) => {
-			const metas = ctx.pageMetaData.findMetas({name: 'robots'});
-			return {content: metas[0].content, length: metas.length};
+			test.expect(linkIsExists).toBe(true);
 		});
 
-		test.expect(metaInfo.content).toEqual(content);
-		test.expect(metaInfo.length).toEqual(1);
-		await test.expect(page.locator('head meta[name="robots"]')).toHaveAttribute('content', content);
+		test('`removeLinks` should removed link tags with the specified attributes from the page', async () => {
+			const attrs: Dictionary<string> = {
+				rel: 'prefetch',
+				href: 'main.js'
+			};
+
+			const linkIsExists = await root.evaluate((ctx, attrs) => {
+				ctx.pageMetaData.addLink(attrs);
+				ctx.pageMetaData.removeLinks(attrs);
+				return document.head.querySelector(`link[rel="${attrs.rel}"][href="${attrs.href}"]`) != null;
+			}, attrs);
+
+			test.expect(linkIsExists).toBe(false);
+		});
+
+		test('`findLinks` should return all added link elements with the specified attributes', async () => {
+			await root.evaluate((ctx) => {
+				ctx.pageMetaData.addLink({rel: 'prefetch', href: 'a.js'});
+				ctx.pageMetaData.addLink({rel: 'prefetch', href: 'b.js'});
+				ctx.pageMetaData.addLink({rel: 'prefetch', href: 'c.js'});
+			});
+
+			const addedLinks = await root.evaluate((ctx) =>
+				ctx.pageMetaData.findLinks({rel: 'prefetch'}).map(({rel, href}) => ({rel, href})));
+
+			test.expect(addedLinks).toEqual([
+				{rel: 'prefetch', href: 'a.js'},
+				{rel: 'prefetch', href: 'b.js'},
+				{rel: 'prefetch', href: 'c.js'}
+			]);
+		});
+
+		test("should set and remove the page's canonical link", async () => {
+			const
+				href = 'https://example.com/';
+
+			let canonical = await root.evaluate((ctx, href) => {
+				ctx.pageMetaData.setCanonicalLink(href);
+				return ctx.pageMetaData.getCanonicalLink()?.href;
+			}, href);
+
+			await test.expect(linkIsExists()).resolves.toBe(true);
+			test.expect(canonical).toEqual(href);
+
+			await root.evaluate((ctx) => ctx.pageMetaData.removeCanonicalLink());
+			canonical = await root.evaluate((ctx) => ctx.pageMetaData.getCanonicalLink()?.href);
+
+			test.expect(canonical).toBeUndefined();
+			await test.expect(linkIsExists()).resolves.toBe(false);
+
+			function linkIsExists() {
+				return root.evaluate((_, href) =>
+					document.head.querySelector(`link[rel="canonical"][href="${href}"]`) != null, href);
+			}
+		});
 	});
 
-	test('Sets/gets/removes canonical link to the head of the page', async ({page}) => {
-		const
-			href = 'https://example.com/',
-			locator = page.locator('link[rel="canonical"]');
+	test.describe('managing `meta` elements on the page', () => {
+		test('`addMeta` should add a new meta-tag to the head of the page', async () => {
+			const attrs = {
+				name: 'keywords',
+				content: 'example'
+			};
 
-		await root.evaluate((ctx, href) => ctx.pageMetaData.setCanonicalLink(href), href);
+			const metaIsExists = await root.evaluate((ctx, attrs) => {
+				ctx.pageMetaData.addMeta(attrs);
+				return document.head.querySelector(`meta[name="${attrs.name}"][content="${attrs.content}"]`) != null;
+			}, attrs);
 
-		let canonical = await root.evaluate((ctx) => ctx.pageMetaData.getCanonicalLink());
+			test.expect(metaIsExists).toBe(true);
+		});
 
-		const element = await root.evaluate((ctx, href) => {
-			const el = document.createElement('link');
-			return Object.assign(el, {rel: 'canonical', href});
-		}, href);
+		test('`removeMetas` should removed meta-tags with the specified attributes from the page', async () => {
+			const attrs: Dictionary<string> = {
+				name: 'keywords',
+				content: 'example'
+			};
 
-		test.expect(canonical).toEqual(element);
-		await test.expect(locator).toHaveAttribute('href', href);
+			const metaIsExists = await root.evaluate((ctx, attrs) => {
+				ctx.pageMetaData.addMeta(attrs);
+				ctx.pageMetaData.removeMetas(attrs);
+				return document.head.querySelector(`meta[name="${attrs.name}"][content="${attrs.content}"]`) != null;
+			}, attrs);
 
-		await root.evaluate((ctx) => ctx.pageMetaData.removeCanonicalLink());
+			test.expect(metaIsExists).toBe(false);
+		});
 
-		canonical = await root.evaluate((ctx) => ctx.pageMetaData.getCanonicalLink());
+		test('`findMetas` should return all added meta-elements with the specified attributes', async () => {
+			await root.evaluate((ctx) => {
+				ctx.pageMetaData.addMeta({name: 'keywords', content: '1'});
+				ctx.pageMetaData.addMeta({name: 'keywords', content: '2'});
+				ctx.pageMetaData.addMeta({name: 'keywords', content: '3'});
+			});
 
-		test.expect(canonical).toBeUndefined();
-		await test.expect(locator).toBeHidden();
+			const addedMetas = await root.evaluate((ctx) =>
+				ctx.pageMetaData.findMetas({name: 'keywords'}).map(({name, content}) => ({name, content})));
+
+			test.expect(addedMetas).toEqual([
+				{name: 'keywords', content: '1'},
+				{name: 'keywords', content: '2'},
+				{name: 'keywords', content: '3'}
+			]);
+		});
 	});
 });
