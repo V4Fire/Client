a91d012e700d1baf865fe6957c53a645dbcbe36c
Artem Shinkaruk <artasend@gmail.com>
2024-04-18 20:21:39 +0700

    chore(core/component/interface): load dummy component

--- src/core/component/interface/component/index.ts
+++ src/core/component/interface/component/index.ts
@@ -9,3 +9,7 @@
 export * from 'core/component/interface/component/types';
 export * from 'core/component/interface/component/component';
 export * from 'core/component/interface/component/unsafe';
+
+//#if runtime has dummyComponents
+import('core/component/interface/component/test/b-component-interface-dummy');
+//#endif
