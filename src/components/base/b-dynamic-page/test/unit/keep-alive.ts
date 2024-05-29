3f9b1ea98d9f303af47c4288c51e8a474f447423
kobezzza <kobezzza@gmail.com>
2024-05-29 15:05:23 +0300

    chore: stylish fixes

--- src/components/base/b-dynamic-page/test/unit/keep-alive.ts
+++ src/components/base/b-dynamic-page/test/unit/keep-alive.ts
@@ -46,363 +46,379 @@ test.describe('<b-dynamic-page> providing `keep-alive`', () => {
 		]);
 	});
 
-	test([
-		'should switch pages, providing `keepAliveSize`',
-		'the number of most recently opened pages to be cached'
-	].join(' '), async ({page}) => {
-		const target = await renderDynamicPage(page, {
-			keepAlive: true,
-			keepAliveSize: 1
-		});
+	test(
+		[
+			'should switch pages, providing `keepAliveSize`',
+			'the number of most recently opened pages to be cached'
+		].join(' '),
 
-		await test.expect(
-			target.evaluate(async (ctx) => {
-				const res: string[] = [];
-
-				await ctx.router!.push('page3');
-
-				const cur: iDynamicPage = await ctx.component;
-				const prev: iDynamicPage = cur;
-
-				res.push(cur.componentName);
-				res.push(cur.hook);
-
-				await ctx.router!.push('page1');
-				await ctx.router!.push('page2');
-
-				res.push(prev.componentName);
-				res.push(prev.hook);
-
-				return res;
-			})
-		).resolves.toEqual([
-			Pages.DYNAMIC_3,
-			Hooks.ACTIVATED,
-
-			Pages.DYNAMIC_3,
-			Hooks.DESTROYED
-		]);
-	});
-
-	test.describe([
-		'`include`',
-		'includes specified pages in the `keepAlive` caching. When the prop is empty, all loaded pages will be cached'
-	].join(' '), () => {
-		test('should `include` the component using a string name', async ({page}) => {
+		async ({page}) => {
 			const target = await renderDynamicPage(page, {
 				keepAlive: true,
-				include: Pages.DYNAMIC_1
+				keepAliveSize: 1
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
+			await test.expect(
+				target.evaluate(async (ctx) => {
+					const res: string[] = [];
+
+					await ctx.router!.push('page3');
+
+					const cur: iDynamicPage = await ctx.component;
+					const prev: iDynamicPage = cur;
+
+					res.push(cur.componentName);
+					res.push(cur.hook);
+
+					await ctx.router!.push('page1');
+					await ctx.router!.push('page2');
+
+					res.push(prev.componentName);
+					res.push(prev.hook);
+
+					return res;
+				})
+			).resolves.toEqual([
+				Pages.DYNAMIC_3,
 				Hooks.ACTIVATED,
 
-				Pages.DYNAMIC_2,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DEACTIVATED),
-
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DESTROYED)
+				Pages.DYNAMIC_3,
+				Hooks.DESTROYED
 			]);
-		});
+		}
+	);
 
-		test('should `include` the components using an array of string names', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include: [Pages.DYNAMIC_1, Pages.DYNAMIC_2]
+	test.describe(
+		[
+			'`include`',
+			'includes specified pages in the `keepAlive` caching. When the prop is empty, all loaded pages will be cached'
+		].join(' '),
+
+		() => {
+			test('should `include` the component using a string name', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: Pages.DYNAMIC_1
+				});
+
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+
+					Pages.DYNAMIC_2,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DEACTIVATED),
+
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DESTROYED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
+			test('should `include` the components using an array of string names', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: [Pages.DYNAMIC_1, Pages.DYNAMIC_2]
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
 
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED),
 
-		test('should not `include` components that are absent from an array of string names', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include: [Pages.DYNAMIC_1, Pages.DYNAMIC_3]
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
+			test('should not `include` components that are absent from an array of string names', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: [Pages.DYNAMIC_1, Pages.DYNAMIC_3]
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DEACTIVATED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
 
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DESTROYED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DEACTIVATED),
 
-		test('should `include` components that match a given regular expression', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include: /^p-v4-dynamic-page/
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DESTROYED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
+			test('should `include` components that match a given regular expression', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: /^p-v4-dynamic-page/
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
 
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED),
 
-		test('should not `include` components when the function-matcher returns `null`', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include: () => null
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
+			test('should not `include` components when the function-matcher returns `null`', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: () => null
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
 
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED),
 
-		test('should not `include` components when the function-matcher returns `false`', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include: () => false
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
+			test('should not `include` components when the function-matcher returns `false`', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: () => false
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
 
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED),
 
-		test('should `include` all components when the function-matcher returns `true`', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include: () => true
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
+			test('should `include` all components when the function-matcher returns `true`', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: () => true
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
 
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED),
 
-		test('should `include` components whose names match the return value from the function-matcher', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include: (page) => page
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
+			test('should `include` components whose names match the return value from the function-matcher', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: (page) => page
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
 
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED),
 
-		test('should `include` components whose names match the return value of the function-matcher', async ({page}) => {
-			const include = (page, route, ctx) => ({
-				cacheKey: page,
-				cacheGroup: page,
-				createCache: () => ctx.keepAliveCache.global
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED)
+				]);
 			});
 
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include
+			test('should `include` components whose names match the return value of the function-matcher', async ({page}) => {
+				const include = (page, route, ctx) => ({
+					cacheKey: page,
+					cacheGroup: page,
+					createCache: () => ctx.keepAliveCache.global
+				});
+
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include
+				});
+
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+
+					Pages.DYNAMIC_2,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED),
+
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED)
+				]);
+			});
+		}
+	);
+
+	test.describe(
+		[
+			'`exclude`',
+			'excludes specified pages from the `keepAlive` caching'
+		].join(' '),
+
+		() => {
+			test('should `exclude` the component using a string name', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					exclude: Pages.DYNAMIC_1
+				});
+
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
+
+					Pages.DYNAMIC_2,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DESTROYED),
+
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DEACTIVATED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
+			test('should `exclude` the components using an array of string names', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					exclude: [Pages.DYNAMIC_1, Pages.DYNAMIC_2]
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
 
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED)
-			]);
-		});
-	});
+					Pages.DYNAMIC_2,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED),
 
-	test.describe([
-		'`exclude`',
-		'excludes specified pages from the `keepAlive` caching'
-	].join(' '), () => {
-		test('should `exclude` the component using a string name', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				exclude: Pages.DYNAMIC_1
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
+			test('should `exclude` the components that match a given regular expression', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					exclude: /^p-v4-dynamic-page/
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DESTROYED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
 
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DEACTIVATED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED),
 
-		test('should `exclude` the components using an array of string names', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				exclude: [Pages.DYNAMIC_1, Pages.DYNAMIC_2]
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
+			test('should `exclude` components when the function-matcher returns `true`', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					exclude: () => true
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
 
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED),
 
-		test('should `exclude` the components that match a given regular expression', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				exclude: /^p-v4-dynamic-page/
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DESTROYED)
+				]);
 			});
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
+			test('should not `exclude` components when the function-matcher returns `false`', async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					exclude: () => false
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
 
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED),
 
-		test('should `exclude` components when the function-matcher returns `true`', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				exclude: () => true
+					Pages.DYNAMIC_1,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DEACTIVATED)
+				]);
 			});
-
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
-
-				Pages.DYNAMIC_2,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED),
-
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DESTROYED)
-			]);
-		});
-
-		test('should not `exclude` components when the function-matcher returns `false`', async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				exclude: () => false
-			});
-
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-
-				Pages.DYNAMIC_2,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED),
-
-				Pages.DYNAMIC_1,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DEACTIVATED)
-			]);
-		});
-	});
+		}
+	);
 
 	test.describe('`include` and `exclude`', () => {
-		test([
-			'should include components that match the regular expression from the `include` prop',
-			'and should exclude components that match the string from the `exclude` prop'
-		].join(' '), async ({page}) => {
-			const target = await renderDynamicPage(page, {
-				keepAlive: true,
-				include: /p-v4-dynamic-page/,
-				exclude: Pages.DYNAMIC_1
-			});
+		test(
+			[
+				'should include components that match the regular expression from the `include` prop',
+				'and should exclude components that match the string from the `exclude` prop'
+			].join(' '),
 
-			await test.expect(target.evaluate(switcher)).resolves.toEqual([
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
+			async ({page}) => {
+				const target = await renderDynamicPage(page, {
+					keepAlive: true,
+					include: /p-v4-dynamic-page/,
+					exclude: Pages.DYNAMIC_1
+				});
 
-				Pages.DYNAMIC_2,
-				Hooks.ACTIVATED,
-				prevHookDebugString(Hooks.DESTROYED),
+				await test.expect(target.evaluate(switcher)).resolves.toEqual([
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
 
-				Pages.DYNAMIC_1,
-				Hooks.MOUNTED,
-				prevHookDebugString(Hooks.DEACTIVATED)
-			]);
-		});
+					Pages.DYNAMIC_2,
+					Hooks.ACTIVATED,
+					prevHookDebugString(Hooks.DESTROYED),
+
+					Pages.DYNAMIC_1,
+					Hooks.MOUNTED,
+					prevHookDebugString(Hooks.DEACTIVATED)
+				]);
+			}
+		);
 	});
 });
