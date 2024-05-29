c40c7f74f3d8c41d7037ec3f0b5a525048208d2e
kobezzza <kobezzza@gmail.com>
2024-05-29 17:37:44 +0300

    chore: added more tests

---
+++ src/components/super/i-block/test/b-super-i-block-destructor-dummy/b-super-i-block-destructor-dummy.ts
@@ -0,0 +1,27 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+/* eslint-disable @v4fire/require-jsdoc */
+
+import type iBlock from 'components/super/i-block/i-block';
+import bDummy, { component, field, system } from 'components/dummies/b-dummy/b-dummy';
+
+export * from 'components/dummies/b-dummy/b-dummy';
+
+@component()
+export default class bSuperIBlockDestructorDummy extends bDummy {
+	@field()
+	content: boolean = true;
+
+	@system()
+	store: iBlock[] = [];
+
+	pushToStore(component: iBlock): void {
+		this.store.push(component);
+	}
+}
---
+++ src/components/super/i-block/test/unit/destructor.ts
@@ -0,0 +1,75 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+import test from 'tests/config/unit/test';
+
+import { renderDestructorDummy } from 'components/super/i-block/test/helpers';
+
+test.describe('<i-block> calling a component\'s destructor', () => {
+	test.beforeEach(async ({demoPage}) => {
+		await demoPage.goto();
+	});
+
+	test('the destructors of all components should be implicitly called if they are removed in the template', async ({page}) => {
+		const target = await renderDestructorDummy(page);
+
+		await target.evaluate(async (ctx) => {
+			ctx.content = false;
+			await ctx.nextTick();
+		});
+
+		const componentsStatuses = await target.evaluate(
+			(ctx) => ctx.store.map((el) => el.componentStatus)
+		);
+
+		test.expect(componentsStatuses.every((status) => status === 'destroyed')).toBe(true);
+	});
+
+	test(
+		'the destructors of external root components should be called implicitly if their descendants have been removed from the template',
+
+		async ({page}) => {
+			const target = await renderDestructorDummy(page);
+
+			await target.evaluate(async (ctx) => {
+				ctx.content = false;
+				await ctx.nextTick();
+			});
+
+			// 1 because the test case itself was created using async
+			await test.expect(target.evaluate(({r}) => r.remoteRootInstances)).resolves.toBe(1);
+		}
+	);
+
+	test('the destructors of all components should be called implicitly if the `$destroy` method has been invoked on the parent', async ({page}) => {
+		const target = await renderDestructorDummy(page);
+
+		const componentsStatuses = await target.evaluate((ctx) => {
+			const {store} = ctx;
+			ctx.unsafe.$destroy();
+			return store.map((el) => el.componentStatus);
+		});
+
+		test.expect(componentsStatuses.every((status) => status === 'destroyed')).toBe(true);
+	});
+
+	test(
+		'the destructors of external root components should be called implicitly if the `$destroy` method has been invoked on the parent',
+
+		async ({page}) => {
+			const target = await renderDestructorDummy(page);
+
+			await target.evaluate(async (ctx) => {
+				ctx.unsafe.$destroy();
+			});
+
+			// 1 because the test case itself was created using async
+			await test.expect(target.evaluate(({r}) => r.remoteRootInstances)).resolves.toBe(1);
+		}
+	);
+});
