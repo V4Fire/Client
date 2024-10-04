60592eb07bfd3f0dea4657a24a7e3ee7363e0fb7
Dzmitry Yahorau <d-yahorau@yandex-team.ru>
2024-10-04 12:30:09 +0300

    fixed incorrect reference to variable

--- build/helpers/get-origin-layer.js
+++ build/helpers/get-origin-layer.js
@@ -2,7 +2,7 @@ const {getLayerName} = require('./layer-name');
 
 exports.getOriginLayerFromPath = function(filePath) {
 	const
-		pathToOriginPackage = sourceFile.path.match(/(?<path>.+)[/\\]node_modules[/\\]/)?.groups?.path;
+		pathToOriginPackage = filePath.match(/(?<path>.+)[/\\]node_modules[/\\]/)?.groups?.path;
 
 	if (pathToRootPackage == null) {
 		return getLayerName(filePath);
