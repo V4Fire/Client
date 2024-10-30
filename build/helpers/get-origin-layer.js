b9d919f4657af329b1e190fdc40e4eecfbfdf4a6
Dzmitry Yahorau <d-yahorau@yandex-team.ru>
2024-10-30 15:03:55 +0300

    jsdoc for get-origin-layer helper

--- build/helpers/get-origin-layer.js
+++ build/helpers/get-origin-layer.js
@@ -1,5 +1,11 @@
 const {getLayerName} = require('./layer-name');
 
+/**
+ * The function returns a layer name of the root package.
+ *
+ * @param {string} filePath
+ * @returns {string}
+ */
 exports.getOriginLayerFromPath = function(filePath) {
 	const
 		pathToOriginPackage = filePath.match(/(?<path>.+)[/\\]node_modules[/\\]/)?.groups?.path;
