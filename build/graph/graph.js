56cfc0885708b1652da7625fe1ca954def0044ec
Dzmitry Yahorau <d-yahorau@yandex-team.ru>
2024-10-30 15:01:47 +0300

    removed unused \n

--- build/graph/graph.js
+++ build/graph/graph.js
@@ -206,7 +206,7 @@ async function buildProjectGraph() {
 		});
 
 		if (tplContent) {
-			const tplsStore = 'globalThis.TPLS = globalThis.TPLS || Object.create(null);\n';
+			const tplsStore = 'globalThis.TPLS = globalThis.TPLS || Object.create(null);';
 			tplContent = [webpackRuntime, tplsStore, tplContent, ''].join('\n');
 		}
 
