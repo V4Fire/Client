abf1f99f91341846b233df93b7f9d9ccb687912f
gretzkiy <dias7707@gmail.com>
2024-12-19 16:06:57 +0300

    feat(core/component): runtime checker for web-components

--- src/core/component/const/validators.ts
+++ src/core/component/const/validators.ts
@@ -6,6 +6,8 @@
  * https://github.com/V4Fire/Client/blob/master/LICENSE
  */
 
+import { isWebComponent } from 'core/component/const/wc-validators';
+
 const componentPrefixes = new Set([
 	'b-',
 	'g-',
@@ -28,6 +30,6 @@ export const isComponent = {
 			return false;
 		}
 
-		return componentPrefixes.has(name.slice(0, 2)) && !name.includes(' ', 2);
+		return componentPrefixes.has(name.slice(0, 2)) && !name.includes(' ', 2) && !isWebComponent.test(name);
 	}
 };
---
+++ src/core/component/const/wc-validators.ts
@@ -0,0 +1,16 @@
+/*!
+ * V4Fire Client Core
+ * https://github.com/V4Fire/Client
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Client/blob/master/LICENSE
+ */
+
+/**
+ * A RegExp to check if the given string is the name of a web-component
+ */
+export const isWebComponent = {
+	test(_name: Nullable<string>): boolean {
+		return false;
+	}
+};
